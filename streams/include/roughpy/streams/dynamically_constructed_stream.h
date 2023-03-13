fe2560f49849f9c2d67c4ba3227bd7b4c8ea780a
Sam Morley <sam@inakleinbottle.com>
2023-03-13 11:14:14 +0000

    Oops missed some paths

---
+++ streams/include/roughpy/streams/dynamically_constructed_stream.h
@@ -0,0 +1,14 @@
+#ifndef ROUGHPY_STREAMS_DYNAMICALLY_CONSTRUCTED_STREAM_H_
+#define ROUGHPY_STREAMS_DYNAMICALLY_CONSTRUCTED_STREAM_H_
+
+#include "stream_base.h"
+
+
+namespace rpy {
+namespace streams {
+
+
+}// namespace streams
+}// namespace rpy
+
+#endif// ROUGHPY_STREAMS_DYNAMICALLY_CONSTRUCTED_STREAM_H_
