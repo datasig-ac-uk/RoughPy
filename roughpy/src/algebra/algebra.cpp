360e2f9eaced9a2b8d3c22971368f1d3b05cf4e9
inakleinbottle <admin@inakleinbottle.com>
2023-03-14 15:22:38 +0000

    Init all algebra components

--- roughpy/src/algebra/algebra.cpp
+++ roughpy/src/algebra/algebra.cpp
@@ -4,5 +4,32 @@
 
 #include "algebra.h"
 
+#include "tensor_key.h"
+#include "lie_key.h"
+#include "free_tensor.h"
+#include "shuffle_tensor.h"
+#include "lie.h"
+#include "lie_key_iterator.h"
+#include "tensor_key_iterator.h"
+
+
+
 void rpy::python::init_algebra(pybind11::module_ &m) {
+
+    py::enum_<algebra::VectorType>(m, "VectorType")
+        .value("DenseVector", algebra::VectorType::Dense)
+        .value("SparseVector", algebra::VectorType::Sparse)
+        .export_values();
+
+    init_py_tensor_key(m);
+    init_py_lie_key(m);
+    init_tensor_key_iterator(m);
+    init_lie_key_iterator(m);
+    init_context(m);
+
+    init_free_tensor(m);
+    init_shuffle_tensor(m);
+    init_lie(m);
+
+
 }
