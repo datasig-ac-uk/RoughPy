d6d40a6466043f205edb4f2345ae70b3200dec8a
inakleinbottle <admin@inakleinbottle.com>
2023-06-12 10:05:33 +0100

    expand out type defs

--- roughpy/src/scalars/scalar_type.cpp
+++ roughpy/src/scalars/scalar_type.cpp
@@ -43,33 +43,98 @@ PyObject *PyScalarMetaType_call(PyObject *, PyObject *, PyObject *) {
     PyErr_SetString(PyExc_AssertionError, "doh");
     return nullptr;
 }
-
 static PyTypeObject PyScalarMetaType_type = {
     PyVarObject_HEAD_INIT(&PyType_Type, 0)
-        .tp_name = "_roughpy.ScalarMeta",
-    .tp_basicsize = sizeof(python::PyScalarMetaType),
-    .tp_itemsize = 0,
-    .tp_call = PyScalarMetaType_call,
-    .tp_flags = Py_TPFLAGS_TYPE_SUBCLASS,
-    .tp_doc = PyDoc_STR("Scalar meta class"),
-    .tp_methods = PyScalarMetaType_methods};
+    "_roughpy.ScalarMeta", /* tp_name */
+    sizeof(python::PyScalarTypeBase),                                /* tp_basicsize */
+    0,                                               /* tp_itemsize */
+    0,                       /* tp_dealloc */
+    0,                                               /* tp_vectorcall_offset */
+    0,                                               /* tp_getattr */
+    0,                                               /* tp_setattr */
+    0,                                               /* tp_as_async */
+    0,                            /* tp_repr */
+    0,                                               /* tp_as_number */
+    0,                                               /* tp_as_sequence */
+    0,                                               /* tp_as_mapping */
+    0,                                               /* tp_hash */
+    PyScalarMetaType_call,                                               /* tp_call */
+    0,                                               /* tp_str */
+    0,                                               /* tp_getattro */
+    0,                                               /* tp_setattro */
+    0,                                               /* tp_as_buffer */
+    Py_TPFLAGS_TYPE_SUBCLASS,                                               /* tp_flags */
+    PyDoc_STR("Scalar meta class"),                         /* tp_doc */
+    0,                                               /* tp_traverse */
+    0,                                               /* tp_clear */
+    0,                                               /* tp_richcompare */
+    0,                                               /* tp_weaklistoffset */
+    0,                                               /* tp_iter */
+    0,                                               /* tp_iternext */
+    PyScalarMetaType_methods,                                               /* tp_methods */
+    0,                                               /* tp_members */
+    0,                                               /* tp_getset */
+    0,                                               /* tp_base */
+    0,                                               /* tp_dict */
+    0,                                               /* tp_descr_get */
+    0,                                               /* tp_descr_set */
+    0,                                               /* tp_dictoffset */
+    0,                                               /* tp_init */
+    0,                                               /* tp_alloc */
+    NULL,                                       /* tp_new */
+};
+
+
 
 static PyMethodDef PyScalarTypeBase_methods[] = {
     {nullptr, nullptr, 0, nullptr}};
 
 static PyTypeObject PyScalarTypeBase_type = {
-    PyVarObject_HEAD_INIT(nullptr, 0)
-        .tp_name = "_roughpy.ScalarTypeBase",
-    .tp_basicsize = sizeof(python::PyScalarTypeBase),
-    .tp_itemsize = 0,
-    .tp_flags = Py_TPFLAGS_DEFAULT
-                | Py_TPFLAGS_BASETYPE
+    PyVarObject_HEAD_INIT(NULL, 0)
+    "_roughpy.ScalarTypeBase", /* tp_name */
+    sizeof(python::PyScalarTypeBase),                                /* tp_basicsize */
+    0,                                               /* tp_itemsize */
+    0,                       /* tp_dealloc */
+    0,                                               /* tp_vectorcall_offset */
+    0,                                               /* tp_getattr */
+    0,                                               /* tp_setattr */
+    0,                                               /* tp_as_async */
+    0,                            /* tp_repr */
+    0,                                               /* tp_as_number */
+    0,                                               /* tp_as_sequence */
+    0,                                               /* tp_as_mapping */
+    0,                                               /* tp_hash */
+    0,                                               /* tp_call */
+    0,                                               /* tp_str */
+    0,                                               /* tp_getattro */
+    0,                                               /* tp_setattro */
+    0,                                               /* tp_as_buffer */
+    Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE
 #if PY_VERSION_HEX >= 0x030A0000
-                | Py_TPFLAGS_DISALLOW_INSTANTIATION
+        | Py_TPFLAGS_DISALLOW_INSTANTIATION
 #endif
-    ,
-    .tp_doc = PyDoc_STR("Base class for scalar type"),
-    .tp_methods = PyScalarTypeBase_methods};
+    ,                                               /* tp_flags */
+    PyDoc_STR("Base class for scalar type"),                         /* tp_doc */
+    0,                                               /* tp_traverse */
+    0,                                               /* tp_clear */
+    0,                                               /* tp_richcompare */
+    0,                                               /* tp_weaklistoffset */
+    0,                                               /* tp_iter */
+    0,                                               /* tp_iternext */
+    PyScalarTypeBase_methods,                                               /* tp_methods */
+    0,                                               /* tp_members */
+    0,                                               /* tp_getset */
+    0,                                               /* tp_base */
+    0,                                               /* tp_dict */
+    0,                                               /* tp_descr_get */
+    0,                                               /* tp_descr_set */
+    0,                                               /* tp_dictoffset */
+    0,                                               /* tp_init */
+    0,                                               /* tp_alloc */
+    myobj_new,                                       /* tp_new */
+};
+
+
 
 pybind11::handle python::get_scalar_metaclass() {
     RPY_CHECK(PyType_Ready(&PyScalarMetaType_type) == 0);
