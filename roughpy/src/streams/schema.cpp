4941ae6039c28e042fbdf575c92a0e9de20c2530
inakleinbottle <admin@inakleinbottle.com>
2023-06-26 11:52:29 +0100

    implemented schema from_data using parser

--- roughpy/src/streams/schema.cpp
+++ roughpy/src/streams/schema.cpp
@@ -33,6 +33,8 @@
 #include "args/parse_schema.h"
 #include <roughpy/streams/schema.h>
 
+#include "r_py_tick_construction_helper.h"
+
 using namespace rpy;
 using namespace rpy::python;
 using namespace rpy::streams;
@@ -59,6 +61,24 @@ inline void init_channel_item(py::module_ &m) {
     py::class_<StreamChannel> cls(m, "StreamChannel");
 }
 
+std::shared_ptr<StreamSchema> schema_from_data(const py::object &data, const py::kwargs &kwargs) {
+
+    py::object parser;
+
+    auto schema = std::make_shared<StreamSchema>();
+
+    if (kwargs.contains("parser")) {
+        parser = kwargs["parser"](schema, true);
+    } else {
+        auto tick_helpers_mod = py::module_::import("roughpy.streams.tick_stream");
+        parser = tick_helpers_mod.attr("StandardTickDataParser")(schema, true);
+    }
+
+    parser.attr("parse_data")(data);
+    auto &helper = parser.attr("helper").cast<python::RPyTickConstructionHelper &>();
+    return helper.schema();
+}
+
 }// namespace
 
 void rpy::python::init_schema(py::module_ &m) {
@@ -68,7 +88,7 @@ void rpy::python::init_schema(py::module_ &m) {
     py::class_<StreamSchema, std::shared_ptr<StreamSchema>>
         cls(m, "StreamSchema");
 
-    cls.def_static("from_data", &parse_schema_from_data, "data"_a);
+    cls.def_static("from_data", &schema_from_data, "data"_a);
     cls.def_static("parse", &parse_schema, "schema"_a);
 
     cls.def("width", &StreamSchema::width);
@@ -100,7 +120,8 @@ void rpy::python::init_schema(py::module_ &m) {
                 case streams::ChannelType::Increment:
                     PyList_SET_ITEM(plist, i++, PyUnicode_FromString(item.first.c_str()));
                     break;
-                case streams::ChannelType::Value: if (item.second.is_lead_lag()) {
+                case streams::ChannelType::Value:
+                    if (item.second.is_lead_lag()) {
                         PyList_SET_ITEM(plist, i++, PyUnicode_FromString((item.first + item.second.label_suffix(0)).c_str()));
                         PyList_SET_ITEM(plist, i++, PyUnicode_FromString((item.first + item.second.label_suffix(1)).c_str()));
                     } else {
