f93c72703218fbcecd5c7be277cbbe981cd9a17f
inakleinbottle <admin@inakleinbottle.com>
2023-06-30 19:54:27 +0100

    Revert "revert to intrusive"
    This reverts commit 0d06607a764533d95b84bd50bc4da20e096f4bcf.

--- core/include/roughpy/core/macros.h
+++ core/include/roughpy/core/macros.h
@@ -229,9 +229,9 @@
 #if defined(RPY_PLATFORM_WINDOWS)
 #ifdef RPY_BUILDING_LIBRARY
 #define RPY_EXPORT_TEMPLATE(TYPE, TMPL, ...) \
-    extern template TYPE TMPL<__VA_ARGS__>
-#define RPY_INSTANTIATE_TEMPLATE(TYPE, TMPL, ...) \
     template TYPE RPY_EXPORT TMPL<__VA_ARGS__>
+#define RPY_INSTANTIATE_TEMPLATE(TYPE, TMPL, ...) \
+    template TYPE TMPL<__VA_ARGS__>
 #else
 #define RPY_EXPORT_TEMPLATE(TYPE, TMPL, ...) \
     template TYPE RPY_EXPORT TMPL<__VA_ARGS__>
@@ -240,9 +240,9 @@
 #endif
 #else
 #define RPY_EXPORT_TEMPLATE(TYPE, TMPL, ...) \
-    extern template TYPE TMPL<__VA_ARGS__>
+    extern template TYPE RPY_EXPORT TMPL<__VA_ARGS__>
 #define RPY_INSTANTIATE_TEMPLATE(TYPE, TMPL, ...) \
-    template TYPE RPY_EXPORT TMPL<__VA_ARGS__>
+    template TYPE TMPL<__VA_ARGS__>
 #endif
 
 
