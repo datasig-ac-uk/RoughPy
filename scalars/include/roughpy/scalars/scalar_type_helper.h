a7925e5df3d4d88e7f54ced9cc0556acdd8252d4
Sam Morley <admin@inakleinbottle.com>
2023-08-03 11:54:41 +0100

    no threading for small numbers of args.
    There is some detail to work out here about how to handle exceptions that are thrown by threads.

--- scalars/include/roughpy/scalars/scalar_type_helper.h
+++ scalars/include/roughpy/scalars/scalar_type_helper.h
@@ -1,7 +1,7 @@
 // Copyright (c) 2023 the RoughPy Developers. All rights reserved.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
 //
 // 1. Redistributions of source code must retain the above copyright notice,
 // this list of conditions and the following disclaimer.
@@ -18,13 +18,12 @@
 // AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 // ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #ifndef ROUGHPY_SCALARS_SCALAR_TYPE_HELPER_H_
 #define ROUGHPY_SCALARS_SCALAR_TYPE_HELPER_H_
@@ -724,7 +723,7 @@ protected:
 
 #if defined(RPY_ALLOW_THREADING)
             auto state = platform::get_thread_state();
-            if (state.is_enabled) {
+            if (state.is_enabled && count > 1) {
                 binary_into_buffer_optimised_threads(dptr, lptr, rptr, count, block_count, mask, std::forward<F>(func));
             } else {
                 binary_into_buffer_optimised_seq(
@@ -743,7 +742,7 @@ protected:
 
 #if defined(RPY_ALLOW_THREADING)
         auto state = platform::get_thread_state();
-        if (state.is_enabled) {
+        if (state.is_enabled && count > 1) {
             binary_into_buffer_fallback_threads(
                     dst, lhs, rhs, count, mask, std::forward<F>(func)
                     );
