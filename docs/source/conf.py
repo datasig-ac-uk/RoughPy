3eacbdf28f617c0ba70ab6110ba96a1753dfa908
Sam Morley <admin@inakleinbottle.com>
2023-08-18 14:15:31 +0100

    started working on docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,17 +16,8 @@ release = '0.0.2'
 
 extensions = [
     "sphinx.ext.intersphinx",
-    "sphinx.ext.autodoc",
-    "sphinx_design",
-    "sphinx.ext.todo",
-    "nbsphinx",
-    'myst_parser',
-    'IPython.sphinxext.ipython_console_highlighting',
-    'sphinxcontrib.bibtex',
-    "sphinxcontrib.video",
+    "breathe",
 ]
-
-bibtex_bibfiles = ['references.bib']
 
 source_suffix = {
     '.rst': 'restructuredtext',
@@ -35,18 +26,17 @@ source_suffix = {
 }
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', '**.ipynb_checkpoints']
+exclude_patterns = []
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
 
 
-intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
-
-# Display todos by setting to True
-todo_include_todos = False
+intersphinx_mapping = {
+    "python": ("https://python.org/3", None)
+}
