288fd871ba4e3c65f9cdb200d57d9da99a2ad33a
Sam Morley <admin@inakleinbottle.com>
2023-08-21 15:07:17 +0100

    Added transposed parameter to copy_strided function

--- roughpy/src/args/strided_copy.cpp
+++ roughpy/src/args/strided_copy.cpp
@@ -1,3 +1,30 @@
+// Copyright (c) 2023 the RoughPy Developers. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 //
 // Created by sam on 09/08/23.
 //
@@ -9,7 +36,8 @@ void rpy::python::stride_copy(
         void* dst, const void* src, const py::ssize_t itemsize,
         const py::ssize_t ndim, const py::ssize_t* shape_in,
         const py::ssize_t* strides_in,
-        const py::ssize_t* strides_out
+        const py::ssize_t* strides_out,
+        bool transpose
 ) noexcept
 {
     RPY_DBG_ASSERT(ndim == 1 || ndim == 2);
@@ -23,12 +51,22 @@ void rpy::python::stride_copy(
                         sptr + i*strides_in[0],
                         itemsize);
         }
+    } else if (transpose) {
+         for (py::ssize_t i=0; i<shape_in[0]; ++i) {
+            for (py::ssize_t j=0; j<shape_in[1]; ++j) {
+                std::memcpy(
+                        dptr + j*strides_out[0] + i*strides_out[1],
+                        sptr + i*strides_in[0] + j*strides_in[1],
+                        itemsize
+                        );
+            }
+        }
     } else {
         for (py::ssize_t i=0; i<shape_in[0]; ++i) {
             for (py::ssize_t j=0; j<shape_in[1]; ++j) {
                 std::memcpy(
-                        dptr + i*strides_in[0] + j*strides_in[1],
                         dptr + i*strides_out[0] + j*strides_out[1],
+                        sptr + i*strides_in[0] + j*strides_in[1],
                         itemsize
                         );
             }
