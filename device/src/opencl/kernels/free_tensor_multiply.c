35863b769ea90ac124cea304d9635a7cfed9ec39
Sam Morley <admin@inakleinbottle.com>
2023-09-05 14:51:10 +0100

    more work on kernels...

---
+++ device/src/opencl/kernels/free_tensor_multiply.c
@@ -0,0 +1,52 @@
+// Copyright (c) 2023 the RoughPy Developers. All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+// this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+// this list of conditions and the following disclaimer in the documentation
+// and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its contributors
+// may be used to endorse or promote products derived from this software without
+// specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+// USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+//
+// Created by user on 05/09/23.
+//
+
+#include "kernel_types.h"
+
+inline float identity_float(float x) { return x; }
+
+#define TYPE float
+#define OP identity_float
+
+
+
+RPY_KERNEL void free_tensor_multiply_tiled_level(
+        RPY_ADDR_GLOBL TYPE* dst,
+        RPY_ADDR_GLOBL const TYPE* lhs_src,
+        RPY_ADDR_GLOBL const TYPE* rhs_src,
+        RPY_ADDR_CONST const size_t* levels,
+        int width,
+        int depth,
+        int tile_letters
+        )
+{
+
+}
