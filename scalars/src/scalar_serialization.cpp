92c03073511cf7e3ce21767e01e74b8d486f2a1e
Sam Morley <admin@inakleinbottle.com>
2023-10-02 14:09:44 +0100

    added instantiations of the archive types

---
+++ scalars/src/scalar_serialization.cpp
@@ -0,0 +1,29 @@
+//
+// Created by user on 02/10/23.
+//
+
+#include <roughpy/scalars/scalars_fwd.h>
+
+using namespace rpy;
+using namespace rpy::scalars;
+
+
+#define RPY_SERIAL_IMPL_CLASSNAME rpy::scalars::half
+#define RPY_SERIAL_EXTERNAL cereal
+#define RPY_SERIAL_DO_SPLIT
+#include <roughpy/platform/serialization_instantiations.inl>
+
+#define RPY_SERIAL_IMPL_CLASSNAME rpy::scalars::bfloat16
+#define RPY_SERIAL_EXTERNAL cereal
+#define RPY_SERIAL_DO_SPLIT
+#include <roughpy/platform/serialization_instantiations.inl>
+
+#define RPY_SERIAL_IMPL_CLASSNAME rpy::scalars::indeterminate_type
+#define RPY_SERIAL_EXTERNAL cereal
+#define RPY_SERIAL_DO_SPLIT
+#include <roughpy/platform/serialization_instantiations.inl>
+
+#define RPY_SERIAL_IMPL_CLASSNAME rpy::scalars::monomial
+#define RPY_SERIAL_EXTERNAL cereal
+#define RPY_SERIAL_DO_SPLIT
+#include <roughpy/platform/serialization_instantiations.inl>
