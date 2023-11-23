0ef012daaa1d231166f58983b5704abc0a5c4be9
Sam Morley <admin@inakleinbottle.com>
2023-11-23 14:52:07 +0000

    formatting

--- device/src/core.cpp
+++ device/src/core.cpp
@@ -7,47 +7,33 @@
 std::ostream& rpy::devices::operator<<(std::ostream& os, const TypeInfo& info)
 {
     switch (info.code) {
-        case TypeCode::Int:
-            os << "int" << info.bytes * CHAR_BIT;
+        case TypeCode::Int: os << "int" << info.bytes * CHAR_BIT;
             break;
-        case TypeCode::UInt:
-            os << "uint" << info.bytes * CHAR_BIT;
+        case TypeCode::UInt: os << "uint" << info.bytes * CHAR_BIT;
             break;
-        case TypeCode::Float:
-            os << "float" << info.bytes * CHAR_BIT;
+        case TypeCode::Float: os << "float" << info.bytes * CHAR_BIT;
             break;
-        case TypeCode::OpaqueHandle:
-            os << "opaque";
+        case TypeCode::OpaqueHandle: os << "opaque";
             break;
-        case TypeCode::BFloat:
-            os << "bfloat" << info.bytes*CHAR_BIT;
+        case TypeCode::BFloat: os << "bfloat" << info.bytes * CHAR_BIT;
             break;
-        case TypeCode::Complex:
-            os << "complex" << info.bytes*CHAR_BIT;
-        break;
-        case TypeCode::Bool:
-            os << "bool";
+        case TypeCode::Complex: os << "complex" << info.bytes * CHAR_BIT;
             break;
-        case TypeCode::Rational:
-            os << "Rational";
+        case TypeCode::Bool: os << "bool";
             break;
-        case TypeCode::ArbitraryPrecisionInt:
-            os << "mp_int";
+        case TypeCode::Rational: os << "Rational";
             break;
-        case TypeCode::ArbitraryPrecisionUInt:
-            os << "mp_uint";
+        case TypeCode::ArbitraryPrecisionInt: os << "mp_int";
             break;
-        case TypeCode::ArbitraryPrecisionFloat:
-            os << "mp_float";
+        case TypeCode::ArbitraryPrecisionUInt: os << "mp_uint";
             break;
-        case TypeCode::ArbitraryPrecisionComplex:
-            os << "mp_complex";
+        case TypeCode::ArbitraryPrecisionFloat: os << "mp_float";
             break;
-        case TypeCode::ArbitraryPrecisionRational:
-            os << "Rational";
+        case TypeCode::ArbitraryPrecisionComplex: os << "mp_complex";
             break;
-        case TypeCode::APRationalPolynomial:
-            os << "PolyRational";
+        case TypeCode::ArbitraryPrecisionRational: os << "Rational";
+            break;
+        case TypeCode::APRationalPolynomial: os << "PolyRational";
             break;
     }
 
