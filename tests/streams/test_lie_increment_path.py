a5fef5c0b2128e7f302d6896f7023453c7958d4f
Sam Morley <admin@inakleinbottle.com>
2023-12-01 14:26:10 +0000

    Use default resolution

--- tests/streams/test_lie_increment_path.py
+++ tests/streams/test_lie_increment_path.py
@@ -198,7 +198,7 @@ def test_tick_sig_deriv_width_3_depth_2_let_2_perturb():
     perturbation = Lie(np.array([0.0, 1.0, 0.0]), width=3, depth=2)
     interval = RealInterval(0.0, 1.0)
 
-    d = p.signature_derivative(interval, perturbation, 1)
+    d = p.signature_derivative(interval, perturbation)
 
     expected = FreeTensor(np.array([0.0, 0.0, 1.0, 0.0,
                                     0.0, 0.1, 0.0,
@@ -215,7 +215,7 @@ def test_tick_sig_deriv_width_3_depth_2_let_2_perturb_with_context():
     perturbation = Lie(np.array([0.0, 1.0, 0.0]), width=3, depth=2)
     interval = RealInterval(0.0, 1.0)
 
-    d = p.signature_derivative(interval, perturbation, 1, depth=2)
+    d = p.signature_derivative(interval, perturbation, depth=2)
 
     expected = FreeTensor(np.array([0.0, 0.0, 1.0, 0.0,
                                     0.0, 0.1, 0.0,
