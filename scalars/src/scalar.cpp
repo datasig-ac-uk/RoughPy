16d931e797e4167dd9fc7f7463506fc68a06c830
Sam Morley <admin@inakleinbottle.com>
2024-01-16 11:56:18 +0000

    formatting

--- scalars/src/scalar.cpp
+++ scalars/src/scalar.cpp
@@ -57,7 +57,7 @@ void Scalar::allocate_data()
     RPY_DBG_ASSERT(p_type_and_content_type.is_pointer());
     opaque_pointer = p_type_and_content_type->allocate_single();
     p_type_and_content_type.update_enumeration(
-            dtl::ScalarContentType::OwnedPointer
+        dtl::ScalarContentType::OwnedPointer
     );
 }
 
@@ -73,7 +73,7 @@ Scalar::Scalar(Scalar::type_pointer type) : integer_for_convenience(0)
             mode = dtl::content_type_of(info);
             if (mode == dtl::ScalarContentType::OwnedPointer) {
                 p_type_and_content_type
-                        = type_pointer(*scalar_type_of(info), mode);
+                    = type_pointer(*scalar_type_of(info), mode);
             } else {
                 p_type_and_content_type = type_pointer(info, mode);
             }
@@ -94,6 +94,7 @@ Scalar::Scalar(const ScalarType* type)
         opaque_pointer = type->allocate_single();
     }
 }
+
 Scalar::Scalar(devices::TypeInfo info)
     : p_type_and_content_type(info, dtl::content_type_of(info)),
       integer_for_convenience(0)
@@ -104,60 +105,63 @@ Scalar::Scalar(devices::TypeInfo info)
         opaque_pointer = p_type_and_content_type->allocate_single();
     }
 }
+
 Scalar::Scalar() : p_type_and_content_type(), integer_for_convenience(0) {}
 
 Scalar::Scalar(const ScalarType* type, void* ptr)
     : p_type_and_content_type(type, dtl::ScalarContentType::OpaquePointer),
-      opaque_pointer(ptr)
-{}
+      opaque_pointer(ptr) {}
+
 Scalar::Scalar(const ScalarType* type, const void* ptr)
     : p_type_and_content_type(type, dtl::ScalarContentType::ConstOpaquePointer),
-      opaque_pointer(const_cast<void*>(ptr))
-{}
+      opaque_pointer(const_cast<void*>(ptr)) {}
 
 Scalar::Scalar(devices::TypeInfo info, void* ptr)
     : p_type_and_content_type(info, dtl::ScalarContentType::OpaquePointer),
-      opaque_pointer(ptr)
-{}
+      opaque_pointer(ptr) {}
+
 Scalar::Scalar(devices::TypeInfo info, const void* ptr)
     : p_type_and_content_type(info, dtl::ScalarContentType::ConstOpaquePointer),
-      opaque_pointer(const_cast<void*>(ptr))
-{}
+      opaque_pointer(const_cast<void*>(ptr)) {}
+
 Scalar::Scalar(const ScalarType* type, int64_t num, int64_t denom)
 {
     auto info = type->type_info();
     if (traits::is_arithmetic(info) && info.bytes <= sizeof(void*)) {
         p_type_and_content_type
-                = type_pointer(type, dtl::ScalarContentType::TrivialBytes);
+            = type_pointer(type, dtl::ScalarContentType::TrivialBytes);
         dtl::scalar_assign_rational(trivial_bytes, info, num, denom);
     } else {
         p_type_and_content_type
-                = type_pointer(type, dtl::ScalarContentType::OwnedPointer);
+            = type_pointer(type, dtl::ScalarContentType::OwnedPointer);
         opaque_pointer = type->allocate_single();
         dtl::scalar_assign_rational(opaque_pointer, info, num, denom);
     }
 }
 
-void Scalar::copy_from_opaque_pointer(devices::TypeInfo info, const void *src) {
+void Scalar::copy_from_opaque_pointer(devices::TypeInfo info, const void* src)
+{
     if (p_type_and_content_type.is_null()) {
-        p_type_and_content_type = type_pointer(info, dtl::ScalarContentType::TrivialBytes);
+        p_type_and_content_type =
+            type_pointer(info, dtl::ScalarContentType::TrivialBytes);
     }
 
     const auto dst_info = type_info();
-    if (traits::is_fundamental(dst_info) && dst_info.bytes <= sizeof(void *)) {
+    if (traits::is_fundamental(dst_info) && dst_info.bytes <= sizeof(void*)) {
         if (info == dst_info) {
             std::memcpy(trivial_bytes, src, info.bytes);
         } else {
-            auto successful = dtl::scalar_convert_copy(trivial_bytes, dst_info, src, info);
+            auto successful =
+                dtl::scalar_convert_copy(trivial_bytes, dst_info, src, info);
             RPY_DBG_ASSERT(successful);
         }
         p_type_and_content_type.update_enumeration(
-                dtl::ScalarContentType::TrivialBytes
+            dtl::ScalarContentType::TrivialBytes
         );
     } else {
         allocate_data();
         auto successful
-                = dtl::scalar_convert_copy(opaque_pointer, info, src, info);
+            = dtl::scalar_convert_copy(opaque_pointer, dst_info, src, info);
         RPY_DBG_ASSERT(successful);
     }
 }
@@ -170,7 +174,9 @@ Scalar::Scalar(const Scalar& other) : integer_for_convenience(0)
         switch (p_type_and_content_type.get_enumeration()) {
             case dtl::ScalarContentType::TrivialBytes:
             case dtl::ScalarContentType::ConstTrivialBytes:
-                std::memcpy(trivial_bytes, other.trivial_bytes, sizeof(void*));
+                std::memcpy(trivial_bytes,
+                            other.trivial_bytes,
+                            sizeof(void*));
                 break;
             case dtl::ScalarContentType::OpaquePointer:
             case dtl::ScalarContentType::ConstOpaquePointer:
@@ -186,17 +192,18 @@ Scalar::Scalar(const Scalar& other) : integer_for_convenience(0)
                 // constructor/destructor.
                 allocate_data();
                 auto successful = dtl::scalar_convert_copy(
-                        opaque_pointer,
-                        info,
-                        other.opaque_pointer,
-                        info,
-                        1
+                    opaque_pointer,
+                    info,
+                    other.opaque_pointer,
+                    info,
+                    1
                 );
                 RPY_DBG_ASSERT(successful);
                 break;
         }
     }
 }
+
 Scalar::Scalar(Scalar&& other) noexcept
     : p_type_and_content_type(other.p_type_and_content_type),
       integer_for_convenience(0)
@@ -232,11 +239,13 @@ Scalar::~Scalar()
             }
             break;
         case dtl::ScalarContentType::Interface:
-        case dtl::ScalarContentType::OwnedInterface: interface.~unique_ptr();
+        case dtl::ScalarContentType::OwnedInterface:
+            interface.~unique_ptr();
         case dtl::ScalarContentType::TrivialBytes:
         case dtl::ScalarContentType::OpaquePointer:
         case dtl::ScalarContentType::ConstTrivialBytes:
-        case dtl::ScalarContentType::ConstOpaquePointer: break;
+        case dtl::ScalarContentType::ConstOpaquePointer:
+            break;
     }
 };
 
@@ -251,9 +260,9 @@ Scalar& Scalar::operator=(const Scalar& other)
                 case dtl::ScalarContentType::TrivialBytes:
                 case dtl::ScalarContentType::ConstTrivialBytes:
                     std::memcpy(
-                            trivial_bytes,
-                            other.trivial_bytes,
-                            sizeof(interface_pointer_t)
+                        trivial_bytes,
+                        other.trivial_bytes,
+                        sizeof(interface_pointer_t)
                     );
                     break;
                 case dtl::ScalarContentType::OpaquePointer:
@@ -273,7 +282,8 @@ Scalar& Scalar::operator=(const Scalar& other)
                 }
                     break;
                 case dtl::ScalarContentType::Interface:
-                case dtl::ScalarContentType::OwnedInterface:copy_from_opaque_pointer(info, other.pointer());
+                case dtl::ScalarContentType::OwnedInterface:
+                    copy_from_opaque_pointer(info, other.pointer());
                     // Copying of interface pointers is disallowed.
 //                    RPY_THROW(
 //                        std::runtime_error,
@@ -301,9 +311,9 @@ Scalar& Scalar::operator=(Scalar&& other) noexcept
             case dtl::ScalarContentType::TrivialBytes:
             case dtl::ScalarContentType::ConstTrivialBytes:
                 std::memcpy(
-                        trivial_bytes,
-                        other.trivial_bytes,
-                        sizeof(interface_pointer_t)
+                    trivial_bytes,
+                    other.trivial_bytes,
+                    sizeof(interface_pointer_t)
                 );
                 break;
             case dtl::ScalarContentType::OpaquePointer:
@@ -337,8 +347,8 @@ bool Scalar::is_zero() const noexcept
         case dtl::ScalarContentType::Interface:
         case dtl::ScalarContentType::OwnedInterface:
             return is_pointer_zero(
-                    interface->pointer(),
-                    p_type_and_content_type
+                interface->pointer(),
+                p_type_and_content_type
             );
     }
 
@@ -351,11 +361,13 @@ bool Scalar::is_reference() const noexcept
     switch (p_type_and_content_type.get_enumeration()) {
         case dtl::ScalarContentType::TrivialBytes:
         case dtl::ScalarContentType::ConstTrivialBytes:
-        case dtl::ScalarContentType::OwnedPointer: return false;
+        case dtl::ScalarContentType::OwnedPointer:
+            return false;
         case dtl::ScalarContentType::OpaquePointer:
         case dtl::ScalarContentType::ConstOpaquePointer:
         case dtl::ScalarContentType::Interface:
-        case dtl::ScalarContentType::OwnedInterface: return true;
+        case dtl::ScalarContentType::OwnedInterface:
+            return true;
     }
     RPY_UNREACHABLE_RETURN(false);
 }
@@ -364,13 +376,19 @@ bool Scalar::is_const() const noexcept
 {
     if (fast_is_zero()) { return true; }
     switch (p_type_and_content_type.get_enumeration()) {
-        case dtl::ScalarContentType::TrivialBytes: return false;
-        case dtl::ScalarContentType::ConstTrivialBytes: return true;
-        case dtl::ScalarContentType::OpaquePointer: return false;
-        case dtl::ScalarContentType::ConstOpaquePointer: return true;
+        case dtl::ScalarContentType::TrivialBytes:
+            return false;
+        case dtl::ScalarContentType::ConstTrivialBytes:
+            return true;
+        case dtl::ScalarContentType::OpaquePointer:
+            return false;
+        case dtl::ScalarContentType::ConstOpaquePointer:
+            return true;
         case dtl::ScalarContentType::Interface:
-        case dtl::ScalarContentType::OwnedInterface: return false;
-        case dtl::ScalarContentType::OwnedPointer: return false;
+        case dtl::ScalarContentType::OwnedInterface:
+            return false;
+        case dtl::ScalarContentType::OwnedPointer:
+            return false;
     }
     RPY_UNREACHABLE_RETURN(false);
 }
@@ -379,46 +397,50 @@ const void* Scalar::pointer() const noexcept
 {
     switch (p_type_and_content_type.get_enumeration()) {
         case dtl::ScalarContentType::TrivialBytes:
-        case dtl::ScalarContentType::ConstTrivialBytes: return trivial_bytes;
+        case dtl::ScalarContentType::ConstTrivialBytes:
+            return trivial_bytes;
         case dtl::ScalarContentType::OpaquePointer:
         case dtl::ScalarContentType::ConstOpaquePointer:
-        case dtl::ScalarContentType::OwnedPointer: return opaque_pointer;
+        case dtl::ScalarContentType::OwnedPointer:
+            return opaque_pointer;
         case dtl::ScalarContentType::Interface:
         case dtl::ScalarContentType::OwnedInterface:
             return interface->pointer();
     }
     RPY_UNREACHABLE_RETURN(nullptr);
 }
+
 void* Scalar::mut_pointer()
 {
     if (p_type_and_content_type.is_null()) {
         RPY_THROW(
-                std::runtime_error,
-                "cannot get mutable pointer to constant value zero"
+            std::runtime_error,
+            "cannot get mutable pointer to constant value zero"
         );
     }
     switch (p_type_and_content_type.get_enumeration()) {
-        case dtl::ScalarContentType::TrivialBytes: return trivial_bytes;
+        case dtl::ScalarContentType::TrivialBytes:
+            return trivial_bytes;
         case dtl::ScalarContentType::OpaquePointer:
         case dtl::ScalarContentType::OwnedPointer:
             if (opaque_pointer == nullptr) {
                 RPY_THROW(
-                        std::runtime_error,
-                        "cannot get mutable pointer to constant value zero"
+                    std::runtime_error,
+                    "cannot get mutable pointer to constant value zero"
                 );
             }
             return opaque_pointer;
         case dtl::ScalarContentType::ConstTrivialBytes:
         case dtl::ScalarContentType::ConstOpaquePointer:
             RPY_THROW(
-                    std::runtime_error,
-                    "cannot get mutable pointer to constant value"
+                std::runtime_error,
+                "cannot get mutable pointer to constant value"
             );
         case dtl::ScalarContentType::Interface:
         case dtl::ScalarContentType::OwnedInterface:
             RPY_THROW(
-                    std::runtime_error,
-                    "cannot get mutable pointer to special scalar references"
+                std::runtime_error,
+                "cannot get mutable pointer to special scalar references"
             );
     }
 
@@ -428,8 +450,8 @@ void* Scalar::mut_pointer()
 bool Scalar::is_mutable() const noexcept
 {
     return !static_cast<bool>(
-            static_cast<uint8_t>(p_type_and_content_type.get_enumeration())
-            & static_cast<uint8_t>(dtl::ScalarContentType::IsConst)
+        static_cast<uint8_t>(p_type_and_content_type.get_enumeration())
+        & static_cast<uint8_t>(dtl::ScalarContentType::IsConst)
     );
 }
 
@@ -442,6 +464,7 @@ optional<const ScalarType*> Scalar::type() const noexcept
     auto info = p_type_and_content_type.get_type_info();
     return scalar_type_of(info);
 }
+
 devices::TypeInfo Scalar::type_info() const noexcept
 {
     if (p_type_and_content_type.is_pointer()) {
@@ -451,18 +474,16 @@ devices::TypeInfo Scalar::type_info() const noexcept
 }
 
 static void print_trivial_bytes(
-        std::ostream& os,
-        const byte* bytes,
-        PackedScalarTypePointer<scalars::dtl::ScalarContentType> p_type
-)
-{}
+    std::ostream& os,
+    const byte* bytes,
+    PackedScalarTypePointer<scalars::dtl::ScalarContentType> p_type
+) {}
 
 static void print_opaque_pointer(
-        std::ostream& os,
-        const void* opaque,
-        PackedScalarTypePointer<scalars::dtl::ScalarContentType> p_type
-)
-{}
+    std::ostream& os,
+    const void* opaque,
+    PackedScalarTypePointer<scalars::dtl::ScalarContentType> p_type
+) {}
 
 std::ostream& rpy::scalars::operator<<(std::ostream& os, const Scalar& value)
 {
@@ -480,17 +501,18 @@ std::vector<byte> Scalar::to_raw_bytes() const
 {
     return dtl::to_raw_bytes(pointer(), 1, type_info());
 }
-void Scalar::from_raw_bytes(devices::TypeInfo info, Slice<byte> bytes)
+
+void Scalar::from_raw_bytes(devices::TypeInfo info, Slice <byte> bytes)
 {
     // Make sure it is clear
     this->~Scalar();
     auto tp_o = scalar_type_of(info);
     if (tp_o) {
         p_type_and_content_type
-                = type_pointer(*tp_o, dtl::ScalarContentType::TrivialBytes);
+            = type_pointer(*tp_o, dtl::ScalarContentType::TrivialBytes);
     } else {
         p_type_and_content_type
-                = type_pointer(info, dtl::ScalarContentType::TrivialBytes);
+            = type_pointer(info, dtl::ScalarContentType::TrivialBytes);
     }
 
     void* ptr = nullptr;
