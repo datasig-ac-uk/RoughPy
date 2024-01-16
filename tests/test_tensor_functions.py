27a837edc089af7fcf5ffc081c030d1ee1d976cb
Sam Morley <admin@inakleinbottle.com>
2024-01-16 11:36:15 +0000

    Add test for Log

--- tests/test_tensor_functions.py
+++ tests/test_tensor_functions.py
@@ -2,10 +2,6 @@
 import roughpy as rp
 from roughpy import tensor_functions
 
-import pytest
-
-
-
 
 def test_adjoint_of_free_tensor():
 
@@ -13,3 +9,11 @@ def test_adjoint_of_free_tensor():
     ad_tensor = tensor_functions._adjoint_of_shuffle(tensor)
 
     print(ad_tensor)
+
+
+def test_Log_on_grouplike():
+    x = rp.FreeTensor([0., 1., 2., 3.], width=3, depth=2, dtype=rp.Rational).exp()
+
+    result = tensor_functions.Log(x)
+    expected = x.log()
+    assert result == expected, f"{result} != {expected}"
