806695a3454c49bdfc4785b714f877134e6b4028
Sam Morley <admin@inakleinbottle.com>
2024-01-22 09:44:01 +0000

    formatting

--- scalars/src/scalar/arithmetic.cpp
+++ scalars/src/scalar/arithmetic.cpp
@@ -33,13 +33,14 @@
 #include "arithmetic.h"
 #include "casts.h"
 #include "do_macro.h"
+#include "type_promotion.h"
 
+#include "traits.h"
 #include <roughpy/core/alloc.h>
 #include <roughpy/platform/devices/host_device.h>
 #include <roughpy/scalars/scalar.h>
-#include <roughpy/scalars/scalar_types.h>
 #include <roughpy/scalars/scalar_type.h>
-#include "traits.h"
+#include <roughpy/scalars/scalar_types.h>
 
 using namespace rpy;
 using namespace rpy::scalars;
@@ -47,7 +48,8 @@ using namespace rpy::scalars;
 using rpy::scalars::dtl::ScalarContentType;
 
 static inline void
-uminus_impl(void* dst, const void* ptr, const devices::TypeInfo& info) {}
+uminus_impl(void* dst, const void* ptr, const devices::TypeInfo& info)
+{}
 
 Scalar Scalar::operator-() const
 {
@@ -57,30 +59,31 @@ Scalar Scalar::operator-() const
     if (!fast_is_zero()) {
         switch (p_type_and_content_type.get_enumeration()) {
             case ScalarContentType::TrivialBytes:
-            case ScalarContentType::ConstTrivialBytes: result.
-                        p_type_and_content_type = type_pointer(
-                            info,
-                            dtl::ScalarContentType::TrivialBytes
-                        );
+            case ScalarContentType::ConstTrivialBytes:
+                result.p_type_and_content_type = type_pointer(
+                        info,
+                        dtl::ScalarContentType::TrivialBytes
+                );
                 uminus_impl(result.trivial_bytes, trivial_bytes, info);
                 break;
             case ScalarContentType::OpaquePointer:
             case ScalarContentType::ConstOpaquePointer:
-            case ScalarContentType::OwnedPointer: stype = type();
+            case ScalarContentType::OwnedPointer:
+                stype = type();
                 RPY_CHECK(stype);
                 result.p_type_and_content_type = type_pointer(
-                    *stype,
-                    dtl::ScalarContentType::OwnedPointer
+                        *stype,
+                        dtl::ScalarContentType::OwnedPointer
                 );
                 result.allocate_data();
                 uminus_impl(result.opaque_pointer, opaque_pointer, info);
                 break;
             case ScalarContentType::Interface:
-            case ScalarContentType::OwnedInterface: result.
-                        p_type_and_content_type = type_pointer(
-                            *stype,
-                            dtl::ScalarContentType::OwnedPointer
-                        );
+            case ScalarContentType::OwnedInterface:
+                result.p_type_and_content_type = type_pointer(
+                        *stype,
+                        dtl::ScalarContentType::OwnedPointer
+                );
                 result.allocate_data();
                 uminus_impl(result.opaque_pointer, interface->pointer(), info);
                 break;
@@ -91,36 +94,44 @@ Scalar Scalar::operator-() const
 
 namespace {
 
-struct RPY_LOCAL AddInplace
-{
+struct RPY_LOCAL AddInplace {
     template <typename T>
-    constexpr void operator()(T& lhs, const T& rhs) noexcept { lhs += rhs; }
+    constexpr void operator()(T& lhs, const T& rhs) noexcept
+    {
+        lhs += rhs;
+    }
 };
 
-struct RPY_LOCAL SubInplace
-{
+struct RPY_LOCAL SubInplace {
     template <typename T>
-    constexpr void operator()(T& lhs, const T& rhs) noexcept { lhs -= rhs; }
+    constexpr void operator()(T& lhs, const T& rhs) noexcept
+    {
+        lhs -= rhs;
+    }
 };
 
-struct RPY_LOCAL MulInplace
-{
+struct RPY_LOCAL MulInplace {
     template <typename T>
-    constexpr void operator()(T& lhs, const T& rhs) noexcept { lhs *= rhs; }
+    constexpr void operator()(T& lhs, const T& rhs) noexcept
+    {
+        lhs *= rhs;
+    }
 };
 
-struct RPY_LOCAL DivInplace
-{
+struct RPY_LOCAL DivInplace {
     template <typename T, typename R>
     constexpr enable_if_t<
-        !is_same<T, rational_poly_scalar>::value &&
-        is_same<
-        decltype(std::declval<T&>() /= std::declval<const R&>()), T&>::value>
-    operator()(T& lhs, const R& rhs) noexcept { lhs /= rhs; }
+            !is_same<T, rational_poly_scalar>::value
+            && is_same<
+                    decltype(std::declval<T&>() /= std::declval<const R&>()),
+                    T&>::value>
+    operator()(T& lhs, const R& rhs) noexcept
+    {
+        lhs /= rhs;
+    }
 
     void operator()(...) { RPY_THROW(std::domain_error, "invalid division"); }
 };
-
 
 template <typename Op>
 static inline void
@@ -134,11 +145,11 @@ do_op(void* dst, const void* src, devices::TypeInfo type, Op&& op)
 
 template <typename Op>
 inline void scalar_inplace_arithmetic(
-    void* dst,
-    PackedScalarTypePointer<ScalarContentType> dst_type,
-    const void* src,
-    PackedScalarTypePointer<ScalarContentType> src_type,
-    Op&& op
+        void* dst,
+        PackedScalarTypePointer<ScalarContentType> dst_type,
+        const void* src,
+        PackedScalarTypePointer<ScalarContentType> src_type,
+        Op&& op
 )
 {
     auto src_info = type_info_from(src_type);
@@ -220,14 +231,16 @@ Scalar& Scalar::operator*=(const Scalar& other)
  * to eliminate these as problems.
  */
 template <typename T, typename R>
-static inline
-enable_if_t<is_same<decltype(std::declval<T&>() /= std::declval<const R&>()), T&>::value>
+static inline enable_if_t<
+        is_same<decltype(std::declval<T&>() /= std::declval<const R&>()),
+                T&>::value>
 do_divide_impl(T* dst, const R* divisor)
 {
     *dst /= *divisor;
 }
 
-static inline void do_divide_impl(rational_poly_scalar* dst, const rational_scalar_type* divisor)
+static inline void
+do_divide_impl(rational_poly_scalar* dst, const rational_scalar_type* divisor)
 {
     *dst /= *divisor;
 }
@@ -243,17 +256,13 @@ static inline void do_divide_impl(...)
     RPY_THROW(std::domain_error, "invalid division");
 }
 
-
 template <typename T>
-static inline void do_divide(T* dst,
-                             const void* divisor,
-                             const devices::TypeInfo& info)
-{
+static inline void
+do_divide(T* dst, const void* divisor, const devices::TypeInfo& info){
 #define X(TP) return do_divide_impl(dst, (const TP*) divisor)
-    DO_FOR_EACH_X(info)
+        DO_FOR_EACH_X(info)
 #undef X
 }
-
 
 Scalar& Scalar::operator/=(const Scalar& other)
 {
@@ -272,22 +281,24 @@ Scalar& Scalar::operator/=(const Scalar& other)
 
     type_pointer tmp_type;
     if (other.p_type_and_content_type.is_pointer()) {
-        tmp_type = type_pointer(other.p_type_and_content_type->rational_type(),
-                                other.p_type_and_content_type.
-                                      get_enumeration());
+        tmp_type = type_pointer(
+                other.p_type_and_content_type->rational_type(),
+                other.p_type_and_content_type.get_enumeration()
+        );
     } else {
         tmp_type = type_pointer(
-            traits::rational_type_of(
-                other.p_type_and_content_type.get_type_info()),
-            other.p_type_and_content_type.get_enumeration());
+                traits::rational_type_of(
+                        other.p_type_and_content_type.get_type_info()
+                ),
+                other.p_type_and_content_type.get_enumeration()
+        );
     }
 
     const auto num_type = type_info();
     if (tmp_type == other.p_type_and_content_type) {
-#define X(TP) do_divide((TP*) mut_pointer(),\
-        other.pointer(),\
-        tmp_type.get_type_info());\
-        break
+#define X(TP)                                                                  \
+    do_divide((TP*) mut_pointer(), other.pointer(), tmp_type.get_type_info()); \
+    break
 
         DO_FOR_EACH_X(num_type)
 #undef X
@@ -295,14 +306,12 @@ Scalar& Scalar::operator/=(const Scalar& other)
         Scalar tmp(tmp_type);
         // hopefully converts
         tmp = other;
-#define X(TP) do_divide((TP*) mut_pointer(),\
-        tmp.pointer(),\
-        tmp_type.get_type_info());\
-        break
+#define X(TP)                                                                  \
+    do_divide((TP*) mut_pointer(), tmp.pointer(), tmp_type.get_type_info());   \
+    break
 
         DO_FOR_EACH_X(num_type)
 #undef X
-
     }
 
     return *this;
