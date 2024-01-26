2adef12604a999f573251be2acaf8b128c6c07b1
Sam Morley <admin@inakleinbottle.com>
2024-01-26 14:35:35 +0000

    odd duplication

--- streams/include/roughpy/streams/dyadic_caching_layer.h
+++ streams/include/roughpy/streams/dyadic_caching_layer.h
@@ -111,16 +111,6 @@ protected:
     void dump_cache() const;
 
 };
-RPY_SERIAL_EXTERN_LOAD_CLS(DyadicCachingLayer)
-RPY_SERIAL_EXTERN_SAVE_CLS(DyadicCachingLayer)
-
-#ifdef RPY_COMPILING_STREAMS
-RPY_SERIAL_EXTERN_LOAD_CLS_BUILD(DyadicCachingLayer)
-RPY_SERIAL_EXTERN_SAVE_CLS_BUILD(DyadicCachingLayer)
-#else
-RPY_SERIAL_EXTERN_LOAD_CLS_IMP(DyadicCachingLayer)
-RPY_SERIAL_EXTERN_SAVE_CLS_IMP(DyadicCachingLayer)
-#endif
 
 #ifdef RPY_COMPILING_STREAMS
 RPY_SERIAL_EXTERN_LOAD_CLS_BUILD(DyadicCachingLayer)
