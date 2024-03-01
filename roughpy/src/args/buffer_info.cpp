b5ee1b2b54497c0a11c1990ef92d3364311727dc
Sam Morley <admin@inakleinbottle.com>
2024-03-01 09:32:31 +0000

    Buffer info struct, because the pybind one is too expensive

---
+++ roughpy/src/args/buffer_info.cpp
@@ -0,0 +1,22 @@
+//
+// Created by sam on 2/29/24.
+//
+
+#include "buffer_info.h"
+
+#include "roughpy_module.h"
+#include <roughpy/platform/errors.h>
+
+using namespace rpy;
+using namespace python;
+
+BufferInfo::BufferInfo(PyObject* object)
+{
+    if (PyObject_GetBuffer(object, &m_view, PyBUF_FULL_RO) != 0) {
+        RPY_THROW(py::buffer_error, "invalid buffer object");
+    }
+}
+
+BufferInfo::~BufferInfo() { PyBuffer_Release(&m_view); }
+
+BufferFormat BufferInfo::format() const { BufferFormat fmt; }
---
+++ roughpy/src/args/buffer_info.h
@@ -0,0 +1,86 @@
+//
+// Created by sam on 2/29/24.
+//
+
+#ifndef ROUGHPY_BUFFER_INFO_H
+#define ROUGHPY_BUFFER_INFO_H
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+#include <roughpy/core/macros.h>
+#include <roughpy/core/types.h>
+#include <roughpy/platform/devices/core.h>
+
+#include <boost/container/small_vector.hpp>
+
+namespace rpy {
+namespace python {
+
+struct BufferFormat {
+    boost::container::small_vector<devices::TypeInfo, 1> types;
+};
+
+class BufferInfo
+{
+    Py_buffer m_view;
+
+public:
+    explicit BufferInfo(PyObject* object);
+    ~BufferInfo();
+
+    RPY_NO_DISCARD const byte* data() const noexcept
+    {
+        return reinterpret_cast<const byte*>(m_view.buf);
+    }
+
+    RPY_NO_DISCARD Py_ssize_t size() const noexcept { return m_view.len; }
+
+    RPY_NO_DISCARD bool is_contiguous() const noexcept
+    {
+        return static_cast<bool>(PyBuffer_IsContiguous(&m_view, 'A'));
+    }
+
+    RPY_NO_DISCARD idimn_t itemsize() const noexcept
+    {
+        return static_cast<idimn_t>(m_view.itemsize);
+    }
+    RPY_NO_DISCARD BufferFormat format() const;
+
+    RPY_NO_DISCARD idimn_t ndim() const noexcept
+    {
+        return static_cast<idimn_t>(m_view.itemsize);
+    }
+
+    RPY_NO_DISCARD const Py_ssize_t* shape() const noexcept
+    {
+        return m_view.shape;
+    }
+
+    RPY_NO_DISCARD const Py_ssize_t* strides() const noexcept
+    {
+        return m_view.strides;
+    }
+
+    RPY_NO_DISCARD const Py_ssize_t* suboffsets() const noexcept
+    {
+        return m_view.suboffsets;
+    }
+
+    RPY_NO_DISCARD const byte* ptr(Py_ssize_t* index) const noexcept
+    {
+        return reinterpret_cast<const byte*>(PyBuffer_GetPointer(&m_view, index)
+        );
+    }
+
+    RPY_NO_DISCARD boost::container::small_vector<Py_ssize_t, 2>
+    new_index() const
+    {
+        return boost::container::small_vector<Py_ssize_t, 2>(m_view.ndim);
+    }
+};
+
+}// namespace python
+}// namespace rpy
+
+#endif// ROUGHPY_BUFFER_INFO_H
