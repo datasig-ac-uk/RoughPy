711dd19cd79000e78b2da5b27fac810c29dcbceb
Sam Morley <admin@inakleinbottle.com>
2024-03-05 09:38:44 +0000

    add test for constructing from list of Lies

--- tests/streams/test_lie_increment_path.py
+++ tests/streams/test_lie_increment_path.py
@@ -396,3 +396,18 @@ def test_signature_multiplication():
     full = stream.signature(rp.RealInterval(0.0, 2.0))
 
     assert left * right == full, f"{left}*{right}={left * right} != {full}"
+
+
+def test_construct_sequence_of_lies():
+    ctx = rp.get_context(width=3, depth=3, coeffs=rp.DPReal)
+    seq = [
+        rp.Lie([1., 2., 3.], ctx=ctx),
+        rp.Lie([4., 5., 6.], ctx=ctx),
+        rp.Lie([7., 8., 9.], ctx=ctx)
+    ]
+
+    stream = LieIncrementStream.from_increments(seq, ctx=ctx)
+
+    lsig = stream.log_signature(rp.RealInterval(0, 1))
+
+    assert lsig == seq[0], f"{lsig} != {seq[0]}"
