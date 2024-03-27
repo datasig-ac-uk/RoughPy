f512a13dc0f93cd92a747c7e559aa9a659cafbf0
Sam Morley <admin@inakleinbottle.com>
2024-03-27 22:33:48 +0000

    formatting

--- algebra/include/roughpy/algebra/key_scalar_array.h
+++ algebra/include/roughpy/algebra/key_scalar_array.h
@@ -5,8 +5,8 @@
 #ifndef ROUGPY_ALGEBRA_KEY_SCALAR_ARRAY_H
 #define ROUGPY_ALGEBRA_KEY_SCALAR_ARRAY_H
 
-#include <roughpy/core/types.h>
 #include <roughpy/core/macros.h>
+#include <roughpy/core/types.h>
 
 #include <roughpy/scalars/devices/core.h>
 #include <roughpy/scalars/scalar_array.h>
@@ -14,21 +14,15 @@
 #include "basis_key.h"
 #include "key_array.h"
 
-
-namespace rpy { namespace algebra {
-
+namespace rpy {
+namespace algebra {
 
 class ROUGHPY_ALGEBRA_EXPORT KeyScalarArray : public scalars::ScalarArray
 {
     KeyArray m_keys;
-
-
-
-
 };
 
+}// namespace algebra
+}// namespace rpy
 
-}}
-
-
-#endif //ROUGPY_ALGEBRA_KEY_SCALAR_ARRAY_H
+#endif// ROUGPY_ALGEBRA_KEY_SCALAR_ARRAY_H
--- algebra/include/roughpy/algebra/key_scalar_stream.h
+++ algebra/include/roughpy/algebra/key_scalar_stream.h
@@ -5,22 +5,21 @@
 #ifndef ROUGHPY_ALGEBRA_KEY_SCALAR_STREAM_H
 #define ROUGHPY_ALGEBRA_KEY_SCALAR_STREAM_H
 
-#include <roughpy/core/types.h>
 #include <roughpy/core/macros.h>
+#include <roughpy/core/types.h>
 
 #include <roughpy/scalars/scalar_stream.h>
 
 #include "key_scalar_array.h"
 
-namespace rpy { namespace algebra {
-
+namespace rpy {
+namespace algebra {
 
 class ROUGHPY_ALGEBRA_EXPORT KeyScalarStream
 {
-
 };
 
+}// namespace algebra
+}// namespace rpy
 
-}}
-
-#endif //ROUGHPY_ALGEBRA_KEY_SCALAR_STREAM_H
+#endif// ROUGHPY_ALGEBRA_KEY_SCALAR_STREAM_H
