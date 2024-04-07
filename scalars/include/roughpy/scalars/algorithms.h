75b8b875a54fd69b6be901279d7c8dec43eff385
Sam Morley <admin@inakleinbottle.com>
2024-04-07 10:32:31 +0100

    fix calls and add more algorithms

--- scalars/include/roughpy/scalars/algorithms.h
+++ scalars/include/roughpy/scalars/algorithms.h
@@ -18,27 +18,62 @@
 namespace rpy {
 namespace scalars {
 namespace algorithms {
-namespace drivers = devices::algorithms::drivers;
+namespace drivers = devices::algorithms;
 
-inline const devices::Type* packed_to_type(const PackedScalarType& type)
+RPY_NO_DISCARD inline optional<dimn_t>
+find(const ScalarArray& range, const Scalar& value)
 {
-    return type.is_pointer() ? type->as_type() : get_type(type.get_type_info());
+    return drivers::find(range.buffer(), devices::Reference(value.pointer()));
 }
 
 template <typename T>
 RPY_NO_DISCARD optional<dimn_t> find(const ScalarArray& range, const T& value)
 {
-    return drivers::find(
-            packed_to_type(range.type()),
-            range.buffer(),
-            devices::TypedReference(value)
-    );
+    return find(range.buffer(), Scalar(range.type(), &value));;
 }
+
+
+
 RPY_NO_DISCARD inline optional<dimn_t>
-find(const ScalarArray& range, const Scalar& value)
+lower_bound(const ScalarArray& range, const Scalar& value)
 {
-    return drivers::find(
-            packed_to_type(range.type()),
+    return drivers::lower_bound(range.buffer(), devices::Reference(value.pointer()));
+}
+
+template <typename T>
+RPY_NO_DISCARD optional<dimn_t> lower_bound(const ScalarArray& range, const T& value)
+{
+    return lower_bound(range.buffer(), Scalar(range.type(), &value));
+}
+
+RPY_NO_DISCARD inline optional<dimn_t>
+upper_bound(const ScalarArray& range, const Scalar& value)
+{
+    return drivers::upper_bound(range.buffer(), devices::Reference(value.pointer()));
+}
+
+template <typename T>
+RPY_NO_DISCARD optional<dimn_t> upper_bound(const ScalarArray& range, const T& value)
+{
+    return upper_bound(range.buffer(), Scalar(range.type(), &value));
+}
+
+RPY_NO_DISCARD inline dimn_t
+count(const ScalarArray& range, const Scalar& value)
+{
+    return drivers::count(range.buffer(), devices::Reference(value.pointer()));
+}
+
+template <typename T>
+RPY_NO_DISCARD dimn_t count(const ScalarArray& range, const T& value)
+{
+    return count(range.buffer(), Scalar(range.type(), &value));
+}
+
+RPY_NO_DISCARD inline bool
+contains(const ScalarArray& range, const Scalar& value)
+{
+    return drivers::contains(
             range.buffer(),
             devices::Reference(value.pointer())
     );
@@ -47,89 +82,95 @@ find(const ScalarArray& range, const Scalar& value)
 template <typename T>
 RPY_NO_DISCARD bool contains(const ScalarArray& range, const T& value)
 {
-    return drivers::contains(
-            packed_to_type(range.type()),
-            range.buffer(),
-            devices::TypedReference(value)
-    );
+    return contains(range.buffer(), Scalar(range.type(), &value));
 }
 
-RPY_NO_DISCARD inline bool
-contains(const ScalarArray& range, const Scalar& value)
-{
-    return drivers::contains(
-            packed_to_type(range.type()),
-            range.buffer(),
-            devices::Reference(value.pointer())
-    );
-}
-
-template <typename T>
-RPY_NO_DISCARD dimn_t count(const ScalarArray& range, const T& value)
-{
-    return drivers::count(
-            packed_to_type(range.type()),
-            range.buffer(),
-            devices::TypedReference(value)
-    );
-}
-
-RPY_NO_DISCARD inline dimn_t
-count(const ScalarArray& range, const Scalar& value)
-{
-    return drivers::count(
-            packed_to_type(range.type()),
-            range.buffer(),
-            devices::Reference(value.pointer())
-    );
-}
 
 RPY_NO_DISCARD inline optional<dimn_t>
 mismatch(const ScalarArray& left, const ScalarArray& right)
 {
-    return drivers::mismatch(
-            left.type(),
-            right.buffer(),
-            left.type(),
-            right.buffer()
-    );
+    return drivers::mismatch(left.buffer(), right.buffer());
 }
 
 RPY_NO_DISCARD inline bool
 equal(const ScalarArray& left, const ScalarArray& right)
 {
-    return drivers::equal(
-            left.type(),
-            left.buffer(),
-            right.type(),
-            right.buffer()
-    );
+    return drivers::equal(left.buffer(), right.buffer());
 }
 
-inline void swap(ScalarArray& left, ScalarArray& right)
+
+
+
+inline void swap_ranges(ScalarArray& left, ScalarArray& right)
 {
     RPY_CHECK(left.type() == right.type());
-    drivers::swap(left.type(), left.mut_buffer(), right.mut_buffer());
+    drivers::swap_ranges(left.mut_buffer(), right.mut_buffer());
 }
 
 inline void copy(ScalarArray& dst, const ScalarArray& src)
 {
-    drivers::copy(dst.type(), dst.mut_buffer(), src.buffer());
+    drivers::copy(dst.mut_buffer(), src.buffer());
+}
+
+
+
+
+
+inline void reverse(ScalarArray& arr)
+{
+    drivers::reverse(arr.mut_buffer());
+}
+
+inline void shift_left(ScalarArray& arr, dimn_t count)
+{
+    (void) count;
+    drivers::shift_left(arr.mut_buffer());
+}
+
+inline void shift_right(ScalarArray& arr, dimn_t count)
+{
+    (void) count;
+    drivers::shift_right(arr.mut_buffer());
+}
+
+
+inline void fill(ScalarArray& dst, const Scalar& value)
+{
+    drivers::fill(dst.mut_buffer(), devices::Reference(value.pointer()));
 }
 
 template <typename T>
 inline void fill(ScalarArray& dst, const T& value)
 {
-    drivers::fill(dst.type(), dst.mut_buffer(), devices::TypedReference(value));
+    fill(dst, Scalar(dst.type(), &value));
 }
 
-inline void fill(ScalarArray& dst, const Scalar& value)
+
+
+
+
+
+
+RPY_NO_DISCARD inline bool
+lexicographical_compare(const ScalarArray& left, const ScalarArray& right)
 {
-    drivers::fill(
-            dst.type(),
-            dst.mut_buffer(),
-            devices::Reference(value.pointer())
-    );
+    return drivers::lexicographical_compare(left.buffer(), right.buffer());
+}
+
+
+RPY_NO_DISCARD inline Scalar min(const ScalarArray& range)
+{
+    Scalar result(range.type());
+    drivers::min(range.buffer(), devices::Reference(result.mut_pointer()));
+    return result;
+}
+
+
+RPY_NO_DISCARD inline Scalar max(const ScalarArray& range)
+{
+    Scalar result(range.type());
+    drivers::max(range.buffer(), devices::Reference(result.mut_pointer()));
+    return result;
 }
 
 }// namespace algorithms
