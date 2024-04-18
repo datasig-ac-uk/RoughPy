ea7e6033f0ea4cf5f2e1bc9804bd785dbe594a36
Sam Morley <admin@inakleinbottle.com>
2024-04-18 15:38:09 +0100

    Static pointer rather than element.

--- scalars/src/devices/algorithm_drivers.cpp
+++ scalars/src/devices/algorithm_drivers.cpp
@@ -3,18 +3,15 @@
 //
 
 #include "algorithm_drivers.h"
-
-
+#include <boost/smart_ptr/intrusive_ptr.hpp>
 
 using namespace rpy;
 using namespace rpy::devices;
 
-
-AlgorithmDriversPtr algorithms::get_builtin_algorithms() noexcept {
-    static const AlgorithmDrivers algos;
-    return &algos;
+AlgorithmDriversPtr algorithms::get_builtin_algorithms() noexcept
+{
+    static AlgorithmDriversPtr algos(new AlgorithmDrivers);
+    return algos;
 }
-
-
 
 AlgorithmDrivers::~AlgorithmDrivers() = default;
