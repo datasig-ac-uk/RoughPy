422755bf72095465a5407bb81b09f6646a0ceb56
Sam Morley <admin@inakleinbottle.com>
2024-04-19 14:49:49 +0100

    Import boost intrusive under alias

---
+++ core/include/roughpy/core/smart_ptr.h
@@ -0,0 +1,21 @@
+//
+// Created by sam on 4/19/24.
+//
+
+#ifndef ROUGHPY_CORE_SMART_PTR_H
+#define ROUGHPY_CORE_SMART_PTR_H
+
+#include <boost/smart_ptr/intrusive_ptr.hpp>
+#include <boost/smart_ptr/intrusive_ref_counter.hpp>
+
+namespace rpy {
+
+template <typename T>
+using Rc = boost::intrusive_ptr<T>;
+
+template <typename Derived, typename Policy = boost::thread_safe_counter>
+using RcBase = boost::intrusive_ref_counter;
+
+}// namespace rpy
+
+#endif// ROUGHPY_CORE_SMART_PTR_H
