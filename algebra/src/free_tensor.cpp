c436fdd2cf4470b9e93b03128f92d9697dc0948e
Sam Morley <admin@inakleinbottle.com>
2024-04-20 19:11:07 +0100

    Expand tensor operations

--- algebra/src/free_tensor.cpp
+++ algebra/src/free_tensor.cpp
@@ -2,12 +2,29 @@
 // Created by sam on 16/02/24.
 //
 
-
 #include "free_tensor.h"
 
 using namespace rpy;
 using namespace rpy::algebra;
 
-FreeTensor FreeTensor::exp() const { return FreeTensor(); }
-FreeTensor FreeTensor::log() const { return FreeTensor(); }
-FreeTensor FreeTensor::antipode() const { return FreeTensor(); }
+FreeTensor FreeTensor::exp() const
+{
+    return FreeTensor(basis(), scalar_type());
+}
+FreeTensor FreeTensor::log() const
+{
+    return FreeTensor(basis(), scalar_type());
+}
+FreeTensor FreeTensor::antipode() const
+{
+    return FreeTensor(basis(), scalar_type());
+}
+
+FreeTensor FreeTensor::fused_multiply_exp(const FreeTensor& other) const
+{
+    return FreeTensor(this->basis(), scalar_type());
+}
+FreeTensor& FreeTensor::fused_multiply_exp_inplcae(const FreeTensor& other)
+{
+    return *this;
+}
