a3a14de3598c4e7d4a4d75c1c79e11079f467cf6
Sam Morley <admin@inakleinbottle.com>
2024-06-24 15:28:41 +0100

    Add BFloat16 scalar type implementation
    New scalar type BFloat16 has been added to the "scalars" namespace, alongside its type-specific methods. Additionally, necessary changes have been made to the CMakeLists.txt file for proper compilation and linking.

---
+++ algebra/src/scalars/scalar_types/bfloat16/bfloat_16_type.cpp
@@ -0,0 +1,64 @@
+//
+// Created by sam on 24/06/24.
+//
+
+#include "bfloat_16_type.h"
+
+#include <roughpy/devices/buffer.h>
+
+
+#include "bfloat16.h"
+
+
+namespace rpy {
+namespace scalars {
+namespace implementations {
+BFloat16Type::BFloat16Type()
+    : Type("bf16",
+           "BFloat16",
+           {devices::TypeCode::BFloat, 2, 2, 1},
+           devices::traits_of<BFloat16>())
+{}
+
+devices::Buffer
+BFloat16Type::allocate(devices::Device device, dimn_t count) const
+{
+    return Type::allocate(device, count);
+}
+void* BFloat16Type::allocate_single() const { return Type::allocate_single(); }
+void BFloat16Type::free_single(void* ptr) const { Type::free_single(ptr); }
+bool BFloat16Type::supports_device(const devices::Device& device) const noexcept
+{
+    return Type::supports_device(device);
+}
+bool BFloat16Type::convertible_to(const Type& dest_type) const noexcept
+{
+    return Type::convertible_to(dest_type);
+}
+bool BFloat16Type::convertible_from(const Type& src_type) const noexcept
+{
+    return Type::convertible_from(src_type);
+}
+devices::TypeComparison BFloat16Type::compare_with(const Type& other
+) const noexcept
+{
+    return Type::compare_with(other);
+}
+void BFloat16Type::copy(void* dst, const void* src, dimn_t count) const
+{
+    Type::copy(dst, src, count);
+}
+void BFloat16Type::move(void* dst, void* src, dimn_t count) const
+{
+    Type::move(dst, src, count);
+}
+void BFloat16Type::display(std::ostream& os, const void* ptr) const
+{
+    Type::display(os, ptr);
+}
+devices::ConstReference BFloat16Type::zero() const { return Type::zero(); }
+devices::ConstReference BFloat16Type::one() const { return Type::one(); }
+devices::ConstReference BFloat16Type::mone() const { return Type::mone(); }
+}// namespace implementations
+}// namespace scalars
+}// namespace rpy
---
+++ algebra/src/scalars/scalar_types/bfloat16/bfloat_16_type.h
@@ -0,0 +1,42 @@
+//
+// Created by sam on 24/06/24.
+//
+
+#ifndef ROUGHPY_SCALAR_IMPLEMENTATIONS_BFLOAT_16_TYPE_H
+#define ROUGHPY_SCALAR_IMPLEMENTATIONS_BFLOAT_16_TYPE_H
+
+#include <roughpy/devices/type.h>
+
+
+namespace rpy {
+namespace scalars {
+namespace implementations {
+
+class BFloat16Type : public devices::Type{
+    BFloat16Type();
+public:
+    RPY_NO_DISCARD devices::Buffer
+    allocate(devices::Device device, dimn_t count) const override;
+    RPY_NO_DISCARD void* allocate_single() const override;
+    void free_single(void* ptr) const override;
+    RPY_NO_DISCARD bool supports_device(const devices::Device& device
+    ) const noexcept override;
+    RPY_NO_DISCARD bool convertible_to(const Type& dest_type
+    ) const noexcept override;
+    RPY_NO_DISCARD bool convertible_from(const Type& src_type
+    ) const noexcept override;
+    RPY_NO_DISCARD devices::TypeComparison compare_with(const Type& other
+    ) const noexcept override;
+    void copy(void* dst, const void* src, dimn_t count) const override;
+    void move(void* dst, void* src, dimn_t count) const override;
+    void display(std::ostream& os, const void* ptr) const override;
+    RPY_NO_DISCARD devices::ConstReference zero() const override;
+    RPY_NO_DISCARD devices::ConstReference one() const override;
+    RPY_NO_DISCARD devices::ConstReference mone() const override;
+};
+
+} // implementations
+} // scalars
+} // rpy
+
+#endif //ROUGHPY_SCALAR_IMPLEMENTATIONS_BFLOAT_16_TYPE_H
