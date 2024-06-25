b15d1a73d8fdbf149fb78aba75f5a22044838ca8
Sam Morley <admin@inakleinbottle.com>
2024-06-25 16:45:34 +0100

    Remove redundant scalar header files
    Deleted unnecessary files: algorithms.h, random.h, and scalar.h, which were no longer in use or needed. Cleanup enhances codebase maintainability and reduces clutter.

--- scalars/src/random/random_impl.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by sam on 12/05/23.
-//
-
-#include "random_impl.h"
-
-const char* rpy::scalars::dtl::rng_type_getter<std::mt19937_64>::name
-        = "mt19937";
-const char* rpy::scalars::dtl::rng_type_getter<pcg64>::name = "pcg";
