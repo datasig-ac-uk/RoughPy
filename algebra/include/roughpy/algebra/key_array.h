24b43d96e6558b643d37219eebd4b44e3bceb90c
Sam Morley <admin@inakleinbottle.com>
2024-08-21 11:01:45 +0100

    Refactor `KeyArray` class to inherit from `Buffer`
    Refactor the `KeyArray` class to directly inherit from `Buffer` instead of containing a `Buffer` member. This change simplifies the class structure and removes redundant member functions, improving maintainability and clarity.

--- algebra/include/roughpy/algebra/key_array.h
+++ algebra/include/roughpy/algebra/key_array.h
@@ -13,7 +13,6 @@
 
 #include <roughpy/devices/buffer.h>
 
-
 #include "basis_keys.h"
 
 namespace rpy {
@@ -165,81 +164,24 @@ public:
  *
  * @tparam BasisKey The type of the elements stored in the KeyArray.
  */
-class ROUGHPY_ALGEBRA_EXPORT KeyArray
+class ROUGHPY_ALGEBRA_EXPORT KeyArray : public devices::Buffer
 {
-    devices::Buffer m_buffer;
-
 public:
     using value_type = BasisKey;
     using iterator = dtl::KeyArrayIterator;
     using const_iterator = dtl::KeyArrayIterator;
-    using reference = BasisKey&;
-    using const_reference = const BasisKey&;
+    using reference = BasisKeyRef;
+    using const_reference = BasisKeyCRef;
+    using Buffer::Buffer;
 
-    KeyArray();
-    KeyArray(const KeyArray&);
-    KeyArray(KeyArray&&) noexcept;
-
-    explicit KeyArray(devices::Buffer&& data) : m_buffer(std::move(data)) {}
-
-    explicit KeyArray(Slice<BasisKey> keys);
-
-    explicit KeyArray(dimn_t size);
-
-    explicit KeyArray(devices::Device device, dimn_t size);
-
-    ~KeyArray();
-
-    /**
-     * @brief Checks if the buffer is empty.
-     *
-     * The empty method checks whether the buffer is empty or not. It returns a
-     * boolean value indicating whether the buffer is empty.
-     *
-     * @return A boolean value. True if the buffer is empty, false otherwise.
-     * @note This method does not throw any exceptions and is guaranteed to be
-     * noexcept.
-     */
-    RPY_NO_DISCARD bool empty() const noexcept { return m_buffer.empty(); }
-
-    /**
-     * @brief Gets the size of the buffer.
-     *
-     * The size method returns the size of the buffer. It returns a dimn_t value
-     * indicating the size of the buffer in elements.
-     *
-     * @return The size of the buffer.
-     * @note This method does not throw any exceptions and is guaranteed to be
-     * noexcept.
-     */
-    RPY_NO_DISCARD dimn_t size() const noexcept { return m_buffer.size(); }
+    explicit KeyArray(Buffer&& buffer) noexcept : Buffer(std::move(buffer)) {}
 
     KeyArray& operator=(const KeyArray&);
     KeyArray& operator=(KeyArray&&) noexcept;
 
-    /**
-     * @brief Returns a range of BasisKey objects in the KeyArray.
-     *
-     * The as_range method returns a dtl::KeyArrayRange object representing a
-     * range of BasisKey objects in the KeyArray. The range starts at the given
-     * offset and ends at the given end_offset (or the end of the KeyArray if
-     * end_offset is not specified). The method checks that the offsets are
-     * within the bounds of the KeyArray buffer before creating the range. If
-     * the offsets are invalid, an exception will be thrown.
-     *
-     * @param offset The starting offset of the range. Defaults to 0.
-     * @param end_offset The ending offset of the range. Defaults to 0, which
-     * means it will be set to the size of the KeyArray buffer.
-     * @return A dtl::KeyArrayRange object representing the range of BasisKey
-     * objects.
-     * @throws std::runtime_error if the offset or end_offset is out of bounds.
-     */
-    dtl::KeyArrayRange as_range(dimn_t offset = 0, dimn_t end_offset = 0) const
+    RPY_NO_DISCARD bool is_const() const noexcept
     {
-        RPY_CHECK(offset <= m_buffer.size() && end_offset <= m_buffer.size());
-        if (end_offset == 0) { end_offset = m_buffer.size(); }
-        RPY_CHECK(end_offset >= offset);
-        return dtl::KeyArrayRange(m_buffer.map(offset, end_offset - offset));
+        return mode() == devices::BufferMode::Read;
     }
 
     BasisKey operator[](dimn_t index) const;
@@ -249,22 +191,12 @@ public:
     KeyArray operator[](SliceIndex index);
     KeyArray operator[](SliceIndex index) const;
 
-    devices::Buffer& mut_buffer() noexcept { return m_buffer; }
-    const devices::Buffer& buffer() const noexcept { return m_buffer; }
-
-    /**
-     * @brief Returns the underlying device associated with this device.
-     *
-     * The device() method returns the underlying devices::Device object
-     * associated with this device. This method is const and noexcept.
-     *
-     * @return The underlying devices::Device object associated with this
-     * device.
-     */
-    RPY_NO_DISCARD devices::Device device() const noexcept
+    devices::Buffer& mut_buffer() noexcept
     {
-        return m_buffer.device();
+        RPY_CHECK(!is_const());
+        return *this;
     }
+    const devices::Buffer& buffer() const noexcept { return *this; }
 
     /**
      * @brief Returns a constant slice of BasisKey objects from the KeyArray.
@@ -319,7 +251,7 @@ public:
      * @see KeyArray
      * @see Buffer::map()
      */
-    RPY_NO_DISCARD KeyArray view() const { return KeyArray(m_buffer.map()); }
+    RPY_NO_DISCARD KeyArray view() const { return KeyArray(map()); }
 
     /**
      * @brief Creates a mutable view of the KeyArray object.
@@ -341,7 +273,7 @@ public:
      * @return A new KeyArray object that provides a mutable view of the data
      * stored in the original KeyArray object.
      */
-    RPY_NO_DISCARD KeyArray mut_view() { return KeyArray(m_buffer.map()); }
+    RPY_NO_DISCARD KeyArray mut_view() { return KeyArray(map()); }
 
     /**
      * @brief Copies the KeyArray to the specified device.
