bcf03390a32c0b2f09ccd2a32704e443950e573b
Sam Morley <admin@inakleinbottle.com>
2024-08-29 17:39:18 +0100

    Use std::addressof to resolve iterator to pointer

--- platform/src/devices/algorithms/shift_left.cpp
+++ platform/src/devices/algorithms/shift_left.cpp
@@ -1,34 +1,29 @@
 
-#include "devices/algorithms.h"
 #include "algorithm_impl.h"
+#include "devices/algorithms.h"
 #include <roughpy/core/ranges.h>
 
 using namespace rpy;
 using namespace rpy::devices;
 
-
 namespace {
 
 template <typename T>
-struct ShiftLeftFunctor
-{
+struct ShiftLeftFunctor {
     void operator()(Buffer& buffer) const
     {
         auto slice = buffer.as_mut_slice<T>();
         auto dst = slice.end();
         auto begin = slice.begin();
-        auto src = dst - 1;;
+        auto src = dst - 1;
+        ;
 
-        while (src != begin) {
-            *(--dst) = std::move(*(--src));
-        }
+        while (src != begin) { *(--dst) = std::move(*(--src)); }
 
-        for (; dst != begin; --dst) {
-            construct_inplace(dst);
-        }
+        for (; dst != begin; --dst) { construct_inplace(std::addressof(dst)); }
     }
 };
-}
+}// namespace
 
 void AlgorithmDrivers::shift_left(Buffer& buffer) const
 {
