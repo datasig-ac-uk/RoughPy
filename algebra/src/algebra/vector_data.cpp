c4b63de356c1dc68a7ebfa2cc678e7b8be91217c
Sam Morley <admin@inakleinbottle.com>
2024-09-05 10:50:10 +0100

    Remove redundant methods in VectorData and DenseVectorData
    Eliminated unnecessary methods such as empty_like, is_sparse, and various vector operations from VectorData and DenseVectorData classes. This reduces code duplication and simplifies the class interfaces, improving maintainability.

--- algebra/src/algebra/vector_data.cpp
+++ algebra/src/algebra/vector_data.cpp
@@ -6,19 +6,3 @@
 
 using namespace rpy;
 using namespace rpy::algebra;
-
-VectorData::~VectorData() = default;
-
-Rc<VectorData> VectorData::empty_like() const noexcept
-{
-    return make_default(basis());
-}
-
-bool VectorData::is_sparse() const noexcept { return false; }
-bool VectorData::is_contiguous() const noexcept { return true; }
-
-Rc<VectorData> VectorData::make_default(BasisPointer basis) noexcept
-{
-
-    return nullptr;
-}
