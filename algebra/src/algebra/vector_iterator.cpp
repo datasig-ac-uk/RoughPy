985e2a414d91e66003f6908de28a74d6817385f4
Sam Morley <admin@inakleinbottle.com>
2024-09-05 11:51:41 +0100

    Comment out VectorIterator methods
    Temporarily disable VectorIterator's member functions and namespace to prevent usage. This includes overloading operators and utility methods within the file.

--- algebra/src/algebra/vector_iterator.cpp
+++ algebra/src/algebra/vector_iterator.cpp
@@ -10,64 +10,64 @@ using namespace rpy;
 using namespace rpy::algebra;
 
 using rpy::scalars::Scalar;
-
-VectorIterator& VectorIterator::operator++()
-{
-    ++m_index;
-    return *this;
-}
-
-const VectorIterator VectorIterator::operator++(int)
-{
-    VectorIterator prev(*this);
-    ++m_index;
-    return prev;
-}
-
-namespace {
-
-BasisKey clone_key(const void* kptr)
-{
-    return *static_cast<const BasisKey*>(kptr);
-}
-
-}// namespace
-
-VectorIterator::reference VectorIterator::operator*()
-{
-    if (m_key_view.empty()) {
-        return {BasisKey(m_index),
-                std::move(static_cast<const scalars::ScalarArray&>(m_scalar_view
-                )[m_index])};
-    }
-    auto* ptr
-            = static_cast<const BasisKey*>(m_key_view.buffer().ptr()) + m_index;
-    return {clone_key(ptr),
-            static_cast<const scalars::ScalarArray&>(m_scalar_view)[m_index]};
-}
-
-VectorIterator::pointer VectorIterator::operator->()
-{
-    if (m_key_view.empty()) {
-        return {BasisKey(m_index), m_scalar_view[m_index]};
-    }
-    auto* ptr
-            = static_cast<const BasisKey*>(m_key_view.buffer().ptr()) + m_index;
-    return {clone_key(ptr), m_scalar_view[m_index]};
-}
-
-bool VectorIterator::operator==(const VectorIterator& other) const noexcept
-{
-    return (m_scalar_view.buffer() == other.m_scalar_view.buffer()
-            && m_key_view.buffer() == other.m_key_view.buffer()
-            && m_index == other.m_index)
-            || m_index >= m_scalar_view.size();
-}
-
-bool VectorIterator::operator!=(const VectorIterator& other) const noexcept
-{
-    return ((m_scalar_view.buffer() == other.m_scalar_view.buffer())
-            && (m_key_view.buffer() == other.m_key_view.buffer())
-            && m_index != other.m_index)
-            || m_index < m_scalar_view.size();
-}
+//
+// VectorIterator& VectorIterator::operator++()
+// {
+//     ++m_index;
+//     return *this;
+// }
+//
+// const VectorIterator VectorIterator::operator++(int)
+// {
+//     VectorIterator prev(*this);
+//     ++m_index;
+//     return prev;
+// }
+//
+// namespace {
+//
+// BasisKey clone_key(const void* kptr)
+// {
+//     return *static_cast<const BasisKey*>(kptr);
+// }
+//
+// }// namespace
+//
+// VectorIterator::reference VectorIterator::operator*()
+// {
+//     if (m_key_view.empty()) {
+//         return {BasisKey(m_index),
+//                 std::move(static_cast<const scalars::ScalarArray&>(m_scalar_view
+//                 )[m_index])};
+//     }
+//     auto* ptr
+//             = static_cast<const BasisKey*>(m_key_view.buffer().ptr()) + m_index;
+//     return {clone_key(ptr),
+//             static_cast<const scalars::ScalarArray&>(m_scalar_view)[m_index]};
+// }
+//
+// VectorIterator::pointer VectorIterator::operator->()
+// {
+//     if (m_key_view.empty()) {
+//         return {BasisKey(m_index), m_scalar_view[m_index]};
+//     }
+//     auto* ptr
+//             = static_cast<const BasisKey*>(m_key_view.buffer().ptr()) + m_index;
+//     return {clone_key(ptr), m_scalar_view[m_index]};
+// }
+//
+// bool VectorIterator::operator==(const VectorIterator& other) const noexcept
+// {
+//     return (m_scalar_view.buffer() == other.m_scalar_view.buffer()
+//             && m_key_view.buffer() == other.m_key_view.buffer()
+//             && m_index == other.m_index)
+//             || m_index >= m_scalar_view.size();
+// }
+//
+// bool VectorIterator::operator!=(const VectorIterator& other) const noexcept
+// {
+//     return ((m_scalar_view.buffer() == other.m_scalar_view.buffer())
+//             && (m_key_view.buffer() == other.m_key_view.buffer())
+//             && m_index != other.m_index)
+//             || m_index < m_scalar_view.size();
+// }
