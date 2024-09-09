bd499e998dd9f11b5f8567734f1119dceef611aa
Sam Morley <admin@inakleinbottle.com>
2024-09-09 10:25:48 +0100

    Add FreeTensor member functions for construction and operations
    Implemented new constructors and member functions in FreeTensor to support cloning, initialization from a Vector, and in-place operations such as fused multiply-add. These enhancements aim to extend the functionality and flexibility of the FreeTensor class for various algebraic operations.

--- algebra/src/algebra/free_tensor.cpp
+++ algebra/src/algebra/free_tensor.cpp
@@ -7,6 +7,35 @@
 using namespace rpy;
 using namespace rpy::algebra;
 
+FreeTensor FreeTensor::new_like(const FreeTensor& arg) noexcept
+{
+    return {};
+}
+FreeTensor FreeTensor::clone(const FreeTensor& arg) noexcept
+{
+    return {};
+}
+FreeTensor FreeTensor::from(Vector&& arg) noexcept
+{
+    return {};
+}
+FreeTensor::FreeTensor()
+    : AlgebraBase(Vector())
+{}
+FreeTensor&
+FreeTensor::fma(const Vector& lhs, const Vector& rhs, const ops::Operator& op)
+{
+    return *this;
+}
+FreeTensor& FreeTensor::multiply_inplace(
+        const Vector& lhs,
+        const Vector& rhs,
+        const ops::Operator& op
+)
+{
+    return *this;
+}
+
 FreeTensor FreeTensor::exp() const
 {
     return FreeTensor();
