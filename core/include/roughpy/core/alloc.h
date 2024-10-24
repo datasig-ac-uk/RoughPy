66482659ab1b9c93d1458d37d95cee877a905558
Sam Morley <admin@inakleinbottle.com>
2024-10-24 11:08:36 +0100

    Add templated construct_inplace functions
    Introduce two templated `construct_inplace` functions in alloc.h to facilitate object construction directly in allocated memory. These functions enhance flexibility by supporting both pointer and reference types as the destination for in-place constructions.

--- core/include/roughpy/core/alloc.h
+++ core/include/roughpy/core/alloc.h
@@ -26,6 +26,20 @@ constexpr void construct_inplace(T* dst, Args&&... args)
     ::new (static_cast<void*>(dst)) T(std::forward<Args>(args)...);
 }
 
+template <typename T, typename... Args>
+constexpr void construct_inplace(void* dst, Args&&... args)
+    noexcept(is_nothrow_constructible<T, Args...>::value)
+{
+   ::new (dst) T(std::forward<Args>(args)...);
+}
+
+template <typename T, typename... Args>
+constexpr void construct_inplace(T& dst, Args&&... args)
+    noexcept(is_nothrow_constructible<T, Args...>::value)
+{
+    :: new(static_cast<void*>(std::addressof(dst))) T(std::forward<Args>(args)...);
+}
+
 
 }// namespace rpy
 
