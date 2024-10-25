d0ce57538ed84ddb74fa3d0dfd3ccee53187dbc5
Sam Morley <admin@inakleinbottle.com>
2024-10-25 17:53:01 +0100

    Remove unused RPY_UNUSED macro
    The RPY_UNUSED macro in the compare_same function declaration was unnecessary and has been removed. This change enhances code readability and ensures the removal of redundant code elements.

--- scalars/src/scalar/comparison.cpp
+++ scalars/src/scalar/comparison.cpp
@@ -68,7 +68,6 @@ constexpr bool compare_impl(const S& lhs, const T& rhs) noexcept
     return lhs == rhs;
 }
 
-RPY_UNUSED
 inline bool
 compare_same(const void* lhs, const void* rhs, TypeInfo info) noexcept
 {
