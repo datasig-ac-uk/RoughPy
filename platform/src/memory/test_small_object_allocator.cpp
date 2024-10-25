de1dacd88453449171006825dbf6c1fb6aced940
Sam Morley <admin@inakleinbottle.com>
2024-10-25 15:15:52 +0100

    Add tests for SmallObjectAllocator in memory module
    Included new test file `test_small_object_allocator.cpp` in the memory module's CMake configuration. This test ensures that the upstream allocator of the SmallObjectAllocator is set correctly.

---
+++ platform/src/memory/test_small_object_allocator.cpp
@@ -0,0 +1,20 @@
+//
+// Created by sam on 25/10/24.
+//
+
+#include <gtest/gtest.h>
+
+#include <roughpy/platform/memory.h>
+
+
+using namespace rpy;
+
+TEST(SmalLObjectAllocatorTest, TestUpstreamAllocatorSetCorrectly)
+{
+    auto* small_object_mr = get_small_object_memory_resource();
+
+    auto* pool_mr = dynamic_cast<std::pmr::synchronized_pool_resource*>(small_object_mr);
+    ASSERT_NE(pool_mr, nullptr);
+
+    ASSERT_EQ(pool_mr->upstream_resource(), get_small_object_memory_resource());
+}
