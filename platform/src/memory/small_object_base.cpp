27f055c3dcfb9fb66ef08b085f9d2737d755c9b1
Sam Morley <admin@inakleinbottle.com>
2024-10-29 21:15:31 +0000

    Add null check and throw bad_alloc in new operator
    This change ensures that the memory allocation does not proceed with a null pointer by adding a check for nullptr. If the allocation fails, the code now throws std::bad_alloc to handle the error properly.

--- platform/src/memory/small_object_base.cpp
+++ platform/src/memory/small_object_base.cpp
@@ -10,7 +10,11 @@ using namespace rpy;
 
 
 void* mem::SmallObjectBase::operator new(dimn_t size) {
-    return small::small_object_alloc(size, small::small_alloc_chunk_size);
+    void* ptr = small::small_object_alloc(size, small::small_alloc_chunk_size);
+    if (RPY_UNLIKELY(ptr == nullptr)) {
+        throw std::bad_alloc();
+    }
+    return ptr;
 }
 
 void mem::SmallObjectBase::operator delete(void* p, dimn_t size) {
