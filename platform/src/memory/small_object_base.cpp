dafd3120460137e0d05283bc38ad174d2e13798a
Sam Morley <admin@inakleinbottle.com>
2024-10-30 11:35:04 +0000

    Update memory allocation to use pool memory
    Changed memory allocation and deallocation in SmallObjectBase to use pool memory functions. Added checks for null pointers and assertions to ensure pool memory availability.

--- platform/src/memory/small_object_base.cpp
+++ platform/src/memory/small_object_base.cpp
@@ -4,13 +4,18 @@
 
 
 #include "memory.h"
+#include "small_object_memory.h"
 
 using namespace rpy;
 
 
 
 void* mem::SmallObjectBase::operator new(dimn_t size) {
-    void* ptr = small::small_object_alloc(size, small::small_alloc_chunk_size);
+    if (RPY_UNLIKELY(size == 0)) { return nullptr; }
+    auto* pool = get_pool_memory();
+    RPY_DBG_ASSERT(pool != nullptr);
+
+    void* ptr = pool->allocate(size, alignof(std::max_align_t));
     if (RPY_UNLIKELY(ptr == nullptr)) {
         throw std::bad_alloc();
     }
@@ -18,6 +23,8 @@ void* mem::SmallObjectBase::operator new(dimn_t size) {
 }
 
 void mem::SmallObjectBase::operator delete(void* p, dimn_t size) {
-    small::small_object_free(p, size);
+    auto* pool = get_pool_memory();
+    RPY_DBG_ASSERT(pool != nullptr);
+    pool->deallocate(p, size);
 }
 
