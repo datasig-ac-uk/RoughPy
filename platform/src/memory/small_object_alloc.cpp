f683ac13d13b02574c478bc721ff66dcf20cea64
Sam Morley <admin@inakleinbottle.com>
2024-10-30 09:16:57 +0000

    Ensure pool memory is not null before allocation/deallocation.
    Added debug assertions to verify that pool memory is not null before trying to allocate or deallocate memory in small_object_alloc and small_object_free functions. This increases the safety and robustness of memory handling by catching potential null pointer issues early.

--- platform/src/memory/small_object_alloc.cpp
+++ platform/src/memory/small_object_alloc.cpp
@@ -11,10 +11,14 @@ using namespace rpy;
 void* mem::small::small_object_alloc(dimn_t alignment, dimn_t size) noexcept
 {
     if (RPY_UNLIKELY(size == 0)) { return nullptr; }
-    return get_pool_memory()->allocate(size, alignment);
+    auto* pool = get_pool_memory();
+    RPY_DBG_ASSERT(pool != nullptr);
+    return pool->allocate(size, alignment);
 }
 
 void mem::small::small_object_free(void* ptr, dimn_t size) noexcept
 {
-    get_pool_memory()->deallocate(ptr, size);
+    auto* pool = get_pool_memory();
+    RPY_DBG_ASSERT(pool != nullptr);
+    pool->deallocate(ptr, size);
 }
