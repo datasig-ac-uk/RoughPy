11d3588ba55aaa5e52a988401d41dca066e57336
Sam Morley <admin@inakleinbottle.com>
2024-11-02 11:31:58 +0000

    Refactor small object memory allocation
    Replaced raw pointer with smart pointer for small object memory resource. This ensures better memory management and prevents potential memory leaks.

--- platform/src/memory/small_object_memory.cpp
+++ platform/src/memory/small_object_memory.cpp
@@ -10,14 +10,14 @@ using namespace rpy;
 
 mem::small::PoolMemory* mem::get_pool_memory() noexcept
 {
-    static mem::small::PoolMemory memory_resource(
+    static auto memory_resource = std::make_unique<small::PoolMemory>(
             std::pmr::pool_options{
                     small::small_alloc_max_chunks,
                     small::small_alloc_chunk_size
             },
             mem::AlignedMemory::get()
     );
-    return &memory_resource;
+    return memory_resource.get();
 }
 
 mem::small::PoolMemory* mem::small::get_small_object_memory_resource() noexcept
