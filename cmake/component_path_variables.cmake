d0e55fdb57daff0a4793e8291f046408ae00fe52
Sam Morley <admin@inakleinbottle.com>
2024-11-05 10:38:04 +0000

    Refactor variable setting to use a macro.
    Introduced a `_do_set_variable` macro to streamline setting component path variables. This change improves code readability and reduces redundancy, as messages and cache descriptions are now encapsulated within the macro.

--- cmake/component_path_variables.cmake
+++ cmake/component_path_variables.cmake
@@ -3,7 +3,10 @@
 include_guard()
 
 
-
+macro(_do_set_variable _var_name _value _cache_descr)
+    set(ROUGHPY_${_name}_${_var_name} ${_value} CACHE FILEPATH "${_cache_descr}")
+    message(DEBUG "    ROUGHPY_${_name}_${_var_name}=${ROUGHPY_${_name}_${_var_name}}")
+endmacro()
 
 function(set_component_path_variables _component)
     if (NOT _component)
@@ -13,7 +16,13 @@ function(set_component_path_variables _component)
     string(TOUPPER ${_component} _name)
     string(TOLOWER ${_component} _lower_name)
 
-    set(ROUGHPY_${_name}_ROOT_DIR ${CMAKE_CURRENT_LIST_DIR} PARENT_SCOPE)
-    set(ROUGHPY_${_name}_INCLUDE_DIR ${ROUGHPY_${_name}_ROOT_DIR}/include/roughpy PARENT_SCOPE)
-    set(ROUGHPY_${_name}_${_name}_INCLUDE ${ROUGHPY_${_name}_INCLUDE_DIR}/${_lower_name} PARENT_SCOPE)
+    message(DEBUG "Setting variables for component ${_component}")
+    _do_set_variable(ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}"
+            "Root directory of the ${_component} component")
+    _do_set_variable(INCLUDE_DIR "${ROUGHPY_${_name}_ROOT_DIR}/include/roughpy/${_lower_name}"
+            "Include directory for the ${_component} component")
+    _do_set_variable(INCLUDE "${ROUGHPY_${_name}_INCLUDE_DIR}"
+            "Include directory for the ${_component} component")
+
+
 endfunction()
