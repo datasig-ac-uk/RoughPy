8a5a31bc18bbc470765ce92eaa2b9b294c12b30d
Sam Morley <admin@inakleinbottle.com>
2024-11-10 19:10:28 +0000

    Refactor trait includes and update Ordering enums
    Reorganized header inclusion for traits to specific trait headers for better modularity and maintainability. Simplified enum naming in ordering_trait.h for improved readability. This change enhances clarity and ensures consistency across the codebase.

--- platform/src/generics/ordering_trait.cpp
+++ platform/src/generics/ordering_trait.cpp
@@ -3,7 +3,7 @@
 //
 
 
-#include "roughpy/generics/traits.h"
+#include "roughpy/generics/traits/ordering_trait.h"
 #include "roughpy/generics/const_reference.h"
 
 
@@ -13,28 +13,28 @@ using namespace rpy::generics;
 
 bool Ordering::comparable(ConstReference lhs, ConstReference rhs) const noexcept
 {
-    return this->compare(std::move(lhs), std::move(rhs)) != OrderingResult::Incomparable;
+    return this->compare(std::move(lhs), std::move(rhs)) != Incomparable;
 }
 
 bool Ordering::less(ConstReference lhs, ConstReference rhs) const noexcept
 {
-    return this->compare(std::move(lhs), std::move(rhs)) == OrderingResult::LessThan;
+    return this->compare(std::move(lhs), std::move(rhs)) == LessThan;
 }
 
 bool Ordering::less_equal(ConstReference lhs, ConstReference rhs) const noexcept
 {
     auto result = this->compare(std::move(lhs), std::move(rhs));
-    return result == OrderingResult::LessThan || result == OrderingResult::Equal;
+    return result == LessThan || result == Equal;
 }
 
 bool Ordering::greater(ConstReference lhs, ConstReference rhs) const noexcept
 {
-    return this->compare(std::move(lhs), std::move(rhs)) == OrderingResult::GreaterThan;
+    return this->compare(std::move(lhs), std::move(rhs)) == GreaterThan;
 }
 
 bool Ordering::greater_equal(ConstReference lhs, ConstReference rhs) const noexcept
 {
     auto result = this->compare(std::move(lhs), std::move(rhs));
-    return result == OrderingResult::GreaterThan || result == OrderingResult::Equal;
+    return result == GreaterThan || result == Equal;
 }
 
