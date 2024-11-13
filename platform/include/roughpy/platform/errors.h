83a855ad2c3bbcd7346451d230871f65db78d88a
Sam Morley <admin@inakleinbottle.com>
2024-11-13 13:35:09 +0000

    Refactor error handling in errors.cpp and errors.h
    Moved throw_exception template specializations from header to source file to improve compilation units' separation. This also helps reduce header file complexity and potential redundancy.

--- platform/include/roughpy/platform/errors.h
+++ platform/include/roughpy/platform/errors.h
@@ -17,38 +17,38 @@
 namespace rpy {
 namespace errors {
 
-
-template <>
-ROUGHPY_PLATFORM_EXPORT void throw_exception<std::runtime_error>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func
-);
-
-template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::system_error>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func);
-
-template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::logic_error>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func);
-
-template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::invalid_argument>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func);
-
-template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::domain_error>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func);
-
-template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::out_of_range>(std::string_view user_msg,
-  const char* filename,
-  int lineno,
-  const char* func);
+//
+// template
+// ROUGHPY_PLATFORM_EXPORT void throw_exception<std::runtime_error>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func
+// );
+//
+// template ROUGHPY_PLATFORM_EXPORT void throw_exception<std::system_error>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func);
+//
+// template ROUGHPY_PLATFORM_EXPORT void throw_exception<std::logic_error>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func);
+//
+// template ROUGHPY_PLATFORM_EXPORT void throw_exception<std::invalid_argument>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func);
+//
+// template ROUGHPY_PLATFORM_EXPORT void throw_exception<std::domain_error>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func);
+//
+// template ROUGHPY_PLATFORM_EXPORT void throw_exception<std::out_of_range>(std::string_view user_msg,
+//   const char* filename,
+//   int lineno,
+//   const char* func);
 
 
 
--- platform/src/errors.cpp
+++ platform/src/errors.cpp
@@ -3,21 +3,50 @@
 //
 
 #include "errors.h"
-#include <boost/stacktrace.hpp>
 
-using namespace rpy;
+#include <stdexcept>
+#include <string_view>
 
-string errors::format_error_message(
-        string_view user_message,
-        const char* filename,
-        int lineno,
-        const char* func,
-        const boost::stacktrace::stacktrace& st
-)
-{
-    std::stringstream ss;
-    ss << user_message << " at lineno " << lineno << " in " << filename
-       << " in function " << func << '\n'
-       << st << '\n';
-    return ss.str();
+#include <roughpy/core/check.h>
+
+
+
+namespace rpy::errors {
+
+
+template
+ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::runtime_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func
+);
+
+template ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::system_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::logic_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::invalid_argument>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::domain_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template ROUGHPY_PLATFORM_EXPORT RPY_NO_RETURN void throw_exception<std::out_of_range>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+
+
 }
+
