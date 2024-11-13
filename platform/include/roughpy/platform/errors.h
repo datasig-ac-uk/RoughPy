5e96eebf218b54b85acc609a4368ae0a89f85d68
Sam Morley <admin@inakleinbottle.com>
2024-11-13 13:24:25 +0000

    Add specialized throw_exception functions for common errors
    Introduced specialized `throw_exception` templates for common exceptions such as `std::runtime_error`, `std::system_error`, `std::logic_error`, `std::invalid_argument`, `std::domain_error`, and `std::out_of_range`. This addition allows for more precise error handling and clearer debug information.

--- platform/include/roughpy/platform/errors.h
+++ platform/include/roughpy/platform/errors.h
@@ -5,31 +5,56 @@
 #ifndef ROUGHPY_PLATFORM_ERRORS_H
 #define ROUGHPY_PLATFORM_ERRORS_H
 
+
+#include <roughpy/core/check.h>
 #include <roughpy/core/macros.h>
 #include <roughpy/core/types.h>
 
+
 #include "roughpy_platform_export.h"
-#include <boost/stacktrace.hpp>
 
 
 namespace rpy {
 namespace errors {
 
-ROUGHPY_PLATFORM_EXPORT
-string format_error_message(string_view user_message, const char* filename, int lineno, const char* func, const boost::stacktrace::stacktrace& st);
+
+template <>
+ROUGHPY_PLATFORM_EXPORT void throw_exception<std::runtime_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func
+);
+
+template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::system_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::logic_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::invalid_argument>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::domain_error>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
+
+template <> ROUGHPY_PLATFORM_EXPORT void throw_exception<std::out_of_range>(std::string_view user_msg,
+  const char* filename,
+  int lineno,
+  const char* func);
 
 
 
 }// namespace errors
 }// namespace rpy
 
-#ifndef __CLION_IDE__
 
-#else
-
-#  define RPY_CHECK(...)
-#  define RPY_THROW(...)
-
-#endif
 
 #endif// ROUGHPY_PLATFORM_ERRORS_H
