92a4f9e22662c6735aa3ca049a7f53ebf46817ca
Sam Morley <admin@inakleinbottle.com>
2024-11-13 13:42:56 +0000

    Refactor string manipulation functions to use std::string
    Replaced custom string type with std::string in string_append, string_cat, and string_join functions. Added utility functions for recursive string joining to simplify code and improve maintainability.

--- core/include/roughpy/core/strings.h
+++ core/include/roughpy/core/strings.h
@@ -8,42 +8,43 @@
 
 #include <string>
 #include <string_view>
+#include <utility>
 
 namespace rpy {
 
 namespace dtl {
 
 
-inline void string_append(string& result, string&& other)
+inline void string_append(std::string& result, std::string&& other)
 {
     result.append(std::move(other));
 }
 
-inline void string_append(string& result, string_view other)
+inline void string_append(std::string& result, std::string_view other)
 {
     result.append(other);
 }
 
-inline void string_append(string& result, const char& other)
+inline void string_append(std::string& result, const char& other)
 {
     result.push_back(other);
 }
 
 template <size_t N>
-void string_append(string& result, const char (&other)[N])
+void string_append(std::string& result, const char (&other)[N])
 {
     result.append(other);
 }
 template <typename T>
-void string_append(string& result, const T& other)
+void string_append(std::string& result, const T& other)
 {
     result.append(std::to_string(other));
 }
 
-inline void string_append_all(string& result) {}
+inline void string_append_all(std::string& result) {}
 
 template <typename Arg, typename... Args>
-void string_append_all(string& result, Arg&& arg, Args&&... args)
+void string_append_all(std::string& result, Arg&& arg, Args&&... args)
 {
     string_append(result, std::forward<Arg>(arg));
     string_append_all(result, std::forward<Args>(args)...);
@@ -67,12 +68,32 @@ void string_append_all(string& result, Arg&& arg, Args&&... args)
  * @see std::string
  */
 template <typename... Args>
-string string_cat(Args&&... args)
+std::string string_cat(Args&&... args)
 {
-    string result;
+    std::string result;
     dtl::string_append_all(result, std::forward<Args>(args)...);
     return result;
 }
+
+
+namespace dtl {
+
+template <typename Joiner, typename Arg>
+void join_all(Joiner&& join_fn, Arg&& arg)
+{
+    join_fn(arg);
+}
+
+template <typename Joiner, typename Arg, typename... Args>
+void join_all(Joiner&& join_fn, Arg&& arg, Args&&... args)
+{
+    join_fn(arg);
+    join_all(std::forward<Joiner>(join_fn), std::forward<Args>(args)...);
+}
+
+
+}
+
 
 /**
  * @brief Concatenates multiple strings with a joiner.
@@ -82,16 +103,16 @@ string string_cat(Args&&... args)
  * @return The joined string.
  */
 template <typename S, typename... Args>
-string string_join(const S& joiner, Args&&... args)
+std::string string_join(const S& joiner, Args&&... args)
 {
-    string result;
+    std::string result;
     bool first = true;
     auto append_arg = [&](const auto& arg) {
         if (!first) { dtl::string_append(result, joiner); }
         dtl::string_append(result, arg);
         first = false;
     };
-    (append_arg(std::forward<Args>(args)), ...);
+    dtl::join_all(append_arg, std::forward<Args>(args)...);
     return result;
 }
 
