8408a9eefe29a8b7783ba5e48c141bb49804a8b8
Sam Morley <admin@inakleinbottle.com>
2024-11-13 11:31:40 +0000

    Add debug assertions and comparison helpers
    Created new headers for debug assertions and comparison helper functions. These enhancements improve code maintainability and readability by isolating the assertion logic and comparison utility functions.

---
+++ core/include/roughpy/core/check_helpers.h
@@ -0,0 +1,137 @@
+//
+// Created by sam on 13/11/24.
+//
+
+#ifndef ROUGHPY_CORE_CHECK_HELPERS_H
+#define ROUGHPY_CORE_CHECK_HELPERS_H
+
+// Rather than using the traits header from RoughPy, we're using the type_traits
+// standard header directly, because we don't want to have any cyclic
+// dependencies
+#include <type_traits>
+#include <utility>
+
+namespace rpy {
+
+// C++20 adds helpful functions for comparing integers, but we don't necessarily
+// have access to those, so reimplement them here. These are just the possible
+// implementations lifted directly from cppreference.com.
+// We don't use the boost safe_numerics because it depends on mp11, which we
+// we absolutely want to avoid.
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+compare_equal(T t, U u) noexcept {
+    if constexpr (std::is_signed_v<T> == std::is_signed_v<U>) {
+        return t == u;
+    } else if constexpr (std::is_signed_v<T>) {
+        return t >= 0 && std::make_unsigned_t<T>(t) == u;
+    } else {
+        return u >= 0 && std::make_unsigned_t<U>(u) == t;
+    }
+}
+
+
+
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+compare_less(T t, U u) noexcept {
+    if constexpr (std::is_signed_v<T> == std::is_signed_v<U>) {
+        return t < u;
+    } else if constexpr (std::is_signed_v<T>) {
+        return t < 0 || std::make_unsigned_t<T>(t) < u;
+    } else {
+        return u >= 0 && t < std::make_unsigned_t<U>(u);
+    }
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+compare_less_equal(T t, U u) noexcept {
+    if constexpr (std::is_signed_v<T> == std::is_signed_v<U>) {
+        return t <= u;
+    } else if constexpr (std::is_signed_v<T>) {
+        return t < 0 || std::make_unsigned_t<T>(t) <= u;
+    } else {
+        return u >= 0 && t <= std::make_unsigned_t<U>(u);
+    }
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+compare_greater(T t, U u) noexcept {
+    return !compare_less_equal(t, u);
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<std::is_integral_v<T> && std::is_integral_v<U>, bool>
+compare_greater_equal(T t, U u) noexcept {
+    return !compare_less(t, u);
+}
+
+
+
+
+// If we want to use these safe functions for integers, we need "safe" functions
+// for other comparisons too
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<!std::is_integral_v<T> || !std::is_integral_v<U>, bool>
+compare_equal(const T& t, const U& u) noexcept(noexcept(t == u))
+{
+    return t == u;
+}
+
+// Compare less and compare greater might carry different semantic meaning for
+// some non-trvial types
+template <typename T, typename U>
+constexpr
+std::enable_if_t<!std::is_integral_v<T> || !std::is_integral_v<U>, bool>
+compare_less(const T& t, const U& u) noexcept(noexcept(t < u))
+{
+    return t < u;
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<!std::is_integral_v<T> || !std::is_integral_v<U>, bool>
+compare_less_equal(const T& t, const U& u) noexcept(noexcept(t <= u))
+{
+    return t <= u;
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<!std::is_integral_v<T> || !std::is_integral_v<U>, bool>
+compare_greater(const T& t, const U& u) noexcept(noexcept(t > u))
+{
+    return t > u;
+}
+
+template <typename T, typename U>
+constexpr
+std::enable_if_t<!std::is_integral_v<T> || !std::is_integral_v<U>, bool>
+compare_greater_equal(const T& t, const U& u) noexcept(noexcept(t >= u))
+{
+    return t >= u;
+}
+
+// It's always a safe assumption that "is not equal" is synonymous with not
+// "is equal"
+template <typename T, typename U>
+constexpr bool
+compare_not_equal(const T& t, const U& u) noexcept {
+    return !compare_equal(t, u);
+}
+
+}
+
+
+#endif //ROUGHPY_CORE_DETAIL_CHECK_HELPERS_H
---
+++ core/include/roughpy/core/debug_assertion.h
@@ -0,0 +1,47 @@
+//
+// Created by sam on 13/11/24.
+//
+
+#ifndef ROUGHPY_CORE_DEBUG_ASSERTION_H
+#define ROUGHPY_CORE_DEBUG_ASSERTION_H
+
+#include <cassert>
+
+#if (defined(_DEBUG) || !defined(NDEBUG) || !defined(__OPTIMIZE__))            \
+        && !defined(RPY_DEBUG)
+#  define RPY_DEBUG
+#elif !defined(RPY_DEBUG)
+#  undef RPY_DEBUG
+#endif
+
+
+#ifdef RPY_DEBUG
+#  include "check_helpers.h"
+#endif
+
+
+#ifdef RPY_DEBUG
+#  if defined(RPY_GCC) || defined(RPY_CLANG)
+#    define RPY_DBG_ASSERT(ARG) assert(ARG)
+#  else
+#    define RPY_DBG_ASSERT(ARG) assert(ARG)
+#  endif
+#else
+#  define RPY_DBG_ASSERT(ARG) (void) 0
+#endif
+
+
+#define RPY_DBG_ASSERT_EQ(a, b) RPY_DBG_ASSERT(::rpy::check_equal((a), (b)))
+#define RPY_DBG_ASSERT_NE(a, b) RPY_DBG_ASSERT(::rpy::check_not_equal((a), (b)))
+#define RPY_DBG_ASSERT_LT(a, b) RPY_DBUG_ASSERT(::rpy::check_less((a), (b)))
+#define RPY_DBG_ASSERT_GT(a, b) RPY_DBG_ASSERT(::rpy::check_greater((a), (b)))
+#define RPY_DBG_ASSERT_LE(a, b) RPY_DBG_ASSERT(::rpy::check_less_equal((a), (b)))
+#define RPY_DBG_ASSERT_GE(a, b) RPY_DBG_ASSERT(::rpy::check_greater_equal((a), (b)))
+
+
+
+
+
+
+
+#endif //ROUGHPY_CORE_DEBUG_ASSERTION_H
