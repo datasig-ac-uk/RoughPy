b5f4cdc31012bdd4cd863792e922501d8ad1cba0
Sam Morley <admin@inakleinbottle.com>
2024-11-13 13:14:11 +0000

    Add error checking macros and exception handling
    Introduced a new header file `check.h` defining macros for error checking and exception handling. Updated CMakeLists.txt to include this new file in the RoughPy_Core library.

---
+++ core/include/roughpy/core/check.h
@@ -0,0 +1,115 @@
+//
+// Created by sam on 13/11/24.
+//
+
+#ifndef ROUGHPY_CORE_CHECK_H
+#define ROUGHPY_CORE_CHECK_H
+
+#include <string>
+#include <string_view>
+
+#include "check_helpers.h"
+#include "macros.h"
+#include "strings.h"
+
+
+#if defined(RPY_GCC)
+#  define RPY_FUNC_NAME __PRETTY_FUNCTION__
+#elif defined(RPY_CLANG)
+#  define RPY_FUNC_NAME __builtin_FUNCTION()
+#elif defined(RPY_MSVC)
+#  define RPY_FUNC_NAME __FUNCTION__
+#else
+#  define RPY_FUNC_NAME static_cast<const char*>(0)
+#endif
+
+
+
+namespace rpy::errors {
+
+template <typename E>
+RPY_NO_RETURN void throw_exception(
+        std::string_view user_msg, const char* filename, int lineno, const char* func
+)
+{
+    throw E(string_cat(user_msg, filename, lineno, func));
+//    throw E(format_error_message(msg, filename, lineno, func, boost::stacktrace::stacktrace()));
+}
+
+}
+
+#define RPY_THROW_2(EXC_TYPE, MSG)                                             \
+    ::rpy::errors::throw_exception<EXC_TYPE>(                                  \
+            MSG, __FILE__, __LINE__, RPY_FUNC_NAME                             \
+    )
+#define RPY_THROW_1(MSG) RPY_THROW_2(std::runtime_error, MSG)
+
+#define RPY_THROW_SEL(NUM) RPY_JOIN(RPY_THROW_, NUM)
+#define RPY_THROW_CNT_IMPL(_1, _2, COUNT, ...) COUNT
+#define RPY_THROW_CNT(...) RPY_THROW_CNT_IMPL(__VA_ARGS__, 2, 1, 0)
+#define RPY_THROW(...)                                                         \
+    RPY_INVOKE_VA(RPY_THROW_SEL(RPY_COUNT_ARGS(__VA_ARGS__)), (__VA_ARGS__))
+
+
+
+
+
+/*
+ * Check macro definition.
+ *
+ * This macro checks that the given expression evaluates to true (under the
+ * assumption that it will usually be true), and throws an error if this
+ * evaluates to false.
+ *
+ * Optionally, one can provide a message string literal that will be used
+ * instead of the default, and an optional error type. The default error type
+ * is a std::runtime_error.
+ */
+
+
+
+// Dispatch the check macro on the number of arguments
+// See: https://stackoverflow.com/a/16683147/9225581
+#define RPY_CHECK_3(EXPR, MSG, TYPE)                                           \
+    do {                                                                       \
+        if (RPY_UNLIKELY(!(EXPR))) {                                           \
+            ::rpy::errors::throw_exception<TYPE>(                              \
+                    MSG, RPY_FILE_NAME, __LINE__, RPY_FUNC_NAME                \
+            );                                                                 \
+        }                                                                      \
+    } while (0)
+
+#define RPY_CHECK_2(EXPR, MSG) RPY_CHECK_3(EXPR, MSG, std::runtime_error)
+
+#define RPY_CHECK_1(EXPR) RPY_CHECK_2(EXPR, "failed check \"" #EXPR "\"")
+
+#define RPY_CHECK_CNT_IMPL(_1, _2, _3, COUNT, ...) COUNT
+// Always pass one more argument than expected, so clang doesn't complain about
+// empty parameter packs.
+#define RPY_CHECK_CNT(...) RPY_CHECK_CNT_IMPL(__VA_ARGS__, 3, 2, 1, 0)
+#define RPY_CHECK_SEL(NUM) RPY_JOIN(RPY_CHECK_, NUM)
+
+#define RPY_CHECK(...)                                                         \
+    RPY_INVOKE_VA(RPY_CHECK_SEL(RPY_COUNT_ARGS(__VA_ARGS__)), (__VA_ARGS__))
+
+
+
+
+
+#define RPY_CHECK_EQ(a, b, ...)                                                \
+    RPY_CHECK(::rpy::check_equal((a), (b)), __VA_ARGS__)
+#define RPY_CHECK_NE(a, b, ...) \
+    RPY_CHECK(::rpy::check_not_equal((a), (b)), __VA_ARGS__)
+#define RPY_CHECK_LT(a, b, ...) \
+    RPY_CHECK(::rpy::check_less((a), (b)), __VA_ARGS__)
+#define RPY_CHECK_LE(a, b, ...) \
+    RPY_CHECK(::rpy::check_less_equal((a), (b)), __VA_ARGS__)
+#define RPY_CHECK_GT(a, b, ...) \
+    RPY_CHECK(::rpy::check_greater((a), (b)), __VA_ARGS__)
+#define RPY_CHECK_GE(a, b, ...) \
+    RPY_CHECK(::rpy::check_greater_equal((a), (b)), __VA_ARGS__)
+
+
+
+
+#endif //ROUGHPY_CORE_CHECK_H
