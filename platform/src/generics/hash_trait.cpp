40a554cbd94b31d59b9e722f29eb69e9cad5c9ea
Sam Morley <admin@inakleinbottle.com>
2024-11-14 19:47:45 +0000

    Add HashTrait class with implementation
    Introduce the HashTrait class and its implementation in the generics module. This includes defining the HashTrait interface and the HashTraitImpl template for hashing values, and integrating the new hash_trait.cpp file for function definitions.

---
+++ platform/src/generics/hash_trait.cpp
@@ -0,0 +1,26 @@
+//
+// Created by sam on 14/11/24.
+//
+
+#include "roughpy/generics/hash_trait.h"
+
+#include "roughpy/core/debug_assertion.h"
+#include "roughpy/core/types.h"
+
+#include "roughpy/generics/values.h"
+
+using namespace rpy;
+using namespace rpy::generics;
+
+
+ HashTrait::~HashTrait() = default;
+
+
+hash_t HashTrait::hash(ConstRef value) const
+{
+    if (value.fast_is_zero()) {
+        return static_cast<hash_t>(0);
+    }
+    return unsafe_hash(value.data());
+}
+
