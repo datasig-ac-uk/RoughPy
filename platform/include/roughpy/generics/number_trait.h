a231bacd60b2931001dd2434a892fe1748b4f0aa
Sam Morley <admin@inakleinbottle.com>
2024-11-14 19:34:10 +0000

    Add initial trait header files for generics
    These header files include placeholders for arithmetic, number, hash, and ordering traits. They set up the structure for future development and ensure proper include guards are in place for each trait.

---
+++ platform/include/roughpy/generics/number_trait.h
@@ -0,0 +1,12 @@
+//
+// Created by sam on 14/11/24.
+//
+
+#ifndef ROUGHPY_GENERICS_NUMBER_TRAIT_H
+#define ROUGHPY_GENERICS_NUMBER_TRAIT_H
+
+#include "builtin_trait.h"
+
+
+
+#endif //ROUGHPY_GENERICS_NUMBER_TRAIT_H
