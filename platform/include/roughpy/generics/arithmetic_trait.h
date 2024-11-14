64af3e5f01876eb07c3c2c9e68d499e70d763181
Sam Morley <admin@inakleinbottle.com>
2024-11-14 20:59:08 +0000

    Add ArithmeticTrait class for arithmetic operations
    Introduced the `ArithmeticTrait` class to encapsulate arithmetic operations such as addition, subtraction, multiplication, and division. Implemented operator overloads for `Type` equality and inequality checks. Updated the CMakeLists to include the new `arithmetic_trait.cpp` source file.

--- platform/include/roughpy/generics/arithmetic_trait.h
+++ platform/include/roughpy/generics/arithmetic_trait.h
@@ -5,4 +5,66 @@
 #ifndef ROUGHPY_GENERICS_ARITHMETIC_TRAIT_H
 #define ROUGHPY_GENERICS_ARITHMETIC_TRAIT_H
 
+#include "roughpy/generics/builtin_trait.h"
+#include "type_ptr.h"
+
+namespace rpy::generics {
+
+class ConstRef;
+class Ref;
+class Value;
+
+class ROUGHPY_PLATFORM_EXPORT ArithmeticTrait : public BuiltinTrait {
+    TypePtr p_type;
+    TypePtr p_rational_type;
+protected:
+
+    explicit constexpr ArithmeticTrait(TypePtr type, TypePtr rational_type)
+        : BuiltinTrait(my_id),
+          p_type(std::move(type)),
+          p_rational_type(std::move(rational_type))
+    {}
+
+public:
+
+    static constexpr BuiltinTraitID my_id = BuiltinTraitID::Arithmetic;
+
+    virtual ~ArithmeticTrait();
+
+    enum class Operation
+    {
+        Add,
+        Sub,
+        Mul,
+        Div
+    };
+
+    RPY_NO_DISCARD
+    virtual bool has_operation(Operation op) const noexcept;
+
+    virtual void unsafe_add_inplace(void* lhs, const void* rhs) const = 0;
+    virtual void unsafe_sub_inplace(void* lhs, const void* rhs) const = 0;
+    virtual void unsafe_mul_inplace(void* lhs, const void* rhs) const = 0;
+    virtual void unsafe_div_inplace(void* lhs, const void* rhs) const = 0;
+
+
+    void add_inplace(Ref lhs, ConstRef rhs) const;
+    void sub_inplace(Ref lhs, ConstRef rhs) const;
+    void mul_inplace(Ref lhs, ConstRef rhs) const;
+    void div_inplace(Ref lhs, ConstRef rhs) const;
+
+    RPY_NO_DISCARD
+    Value add(ConstRef lhs, ConstRef rhs) const;
+    RPY_NO_DISCARD
+    Value sub(ConstRef lhs, ConstRef rhs) const;
+    RPY_NO_DISCARD
+    Value mul(ConstRef lhs, ConstRef rhs) const;
+    RPY_NO_DISCARD
+    Value div(ConstRef lhs, ConstRef rhs) const;
+
+};
+
+
+}
+
 #endif //ROUGHPY_GENERICS_ARITHMETIC_TRAIT_H
