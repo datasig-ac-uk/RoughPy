9224d6a0effc923bab8fe83019bf91a7ea0e9c82
Sam Morley <admin@inakleinbottle.com>
2024-11-15 20:35:05 +0000

    Add backup_display implementation
    Introduced `backup_display.cpp` to the project and updated the `CMakeLists.txt` file to include it. The function `backup_display` outputs a default value to the provided stream.

---
+++ platform/src/generics/backup_display.cpp
@@ -0,0 +1,13 @@
+//
+// Created by sam on 15/11/24.
+//
+
+
+#include "roughpy/generics/values.h"
+
+#include <ostream>
+
+
+void rpy::generics::dtl::backup_display(std::ostream& os) {
+    os << '0';
+}
