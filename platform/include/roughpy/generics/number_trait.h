4cf921f1ea8c3b84bb2c47b1dac94166730ec1f8
Sam Morley <admin@inakleinbottle.com>
2024-11-15 17:16:21 +0000

    Add NumberTraits class for numeric operations
    Introduced the `NumberTraits` class with methods to handle numeric traits like real, imaginary, abs, sqrt, exp, log, and rational conversions. Updated CMakeLists.txt to include the new number_trait.cpp file.

--- platform/include/roughpy/generics/number_trait.h
+++ platform/include/roughpy/generics/number_trait.h
@@ -5,8 +5,87 @@
 #ifndef ROUGHPY_GENERICS_NUMBER_TRAIT_H
 #define ROUGHPY_GENERICS_NUMBER_TRAIT_H
 
+
+#include <roughpy/core/debug_assertion.h>
+
 #include "builtin_trait.h"
+#include "type_ptr.h"
+
+namespace rpy::generics {
+
+class ConstRef;
+class Ref;
+class Value;
+
+class ROUGHPY_PLATFORM_EXPORT NumberTraits : public BuiltinTrait
+{
+    TypePtr p_type;
+    TypePtr p_real_type;
+
+protected:
+
+    NumberTraits(TypePtr type, TypePtr real_type=nullptr)
+        : BuiltinTrait(my_id),
+          p_type(std::move(type)),
+          p_real_type(std::move(real_type))
+    {}
+
+public:
+
+    static constexpr auto my_id = BuiltinTraitID::Number;
 
 
 
-#endif //ROUGHPY_GENERICS_NUMBER_TRAIT_H
+    virtual ~NumberTraits();
+
+    virtual void unsafe_real(void* dst, const void* src) const;
+    virtual void unsafe_imaginary(void* dst, const void* src) const;
+
+    virtual void unsafe_abs(void* dst, const void* src) const noexcept = 0;
+
+
+    virtual void unsafe_sqrt(void* dst, const void* src) const;
+    virtual void unsafe_pow(void* dst, const void* base, int64_t exponent) const;
+
+    virtual void unsafe_exp(void* dst, const void* src) const;
+    virtual void unsafe_log(void* dst, const void* src) const;
+
+    virtual void unsafe_from_rational(void* dst, int64_t numerator,
+        int64_t denominator) const = 0;
+
+
+    void real(Ref dst, ConstRef src) const;
+    void imaginary(Ref dst, ConstRef src) const;
+    void abs(Ref dst, ConstRef src) const;
+    void sqrt(Ref dst, ConstRef src) const;
+    void exp(Ref dst, ConstRef src) const;
+    void log(Ref dst, ConstRef src) const;
+
+    void from_rational(Ref dst, int64_t numerator, int64_t denominator) const;
+
+    RPY_NO_DISCARD Value real(ConstRef value) const;
+    RPY_NO_DISCARD Value imaginary(ConstRef value) const;
+    RPY_NO_DISCARD Value abs(ConstRef value) const;
+    RPY_NO_DISCARD Value sqrt(ConstRef value) const;
+    RPY_NO_DISCARD Value pow(ConstRef value, int64_t power) const;
+    RPY_NO_DISCARD Value exp(ConstRef value) const;
+    RPY_NO_DISCARD Value log(ConstRef value) const;
+
+    RPY_NO_DISCARD Value
+    from_rational(int64_t numerator, int64_t denominator) const;
+
+    RPY_NO_DISCARD
+    const TypePtr& real_type() const noexcept
+    {
+        return p_real_type ? p_real_type : p_type;
+    }
+
+    RPY_NO_DISCARD const TypePtr& imaginary_type() const noexcept
+    {
+        return p_real_type;
+    }
+};
+
+}// namespace rpy::generics
+
+#endif// ROUGHPY_GENERICS_NUMBER_TRAIT_H
---
+++ platform/src/generics/number_trait.cpp
@@ -0,0 +1,198 @@
+//
+// Created by sammorley on 15/11/24.
+//
+
+
+#include "roughpy/generics/number_trait.h"
+
+#include <stdexcept>
+
+#include "roughpy/core/check.h"
+#include "roughpy/core/debug_assertion.h"
+
+#include "roughpy/generics/type.h"
+#include "roughpy/generics/values.h"
+
+
+using namespace rpy;
+using namespace rpy::generics;
+
+NumberTraits::~NumberTraits() = default;
+
+void NumberTraits::unsafe_real(void* dst, const void* src) const noexcept
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    p_type->copy(dst, src, 1, true);
+}
+void NumberTraits::unsafe_imaginary(void* dst, const void* src) const noexcept
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    p_type->copy(dst, nullptr, 1, false);
+}
+void NumberTraits::unsafe_sqrt(void* dst, const void* src) const
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    RPY_THROW(std::runtime_error, "this operation is not implemented");
+}
+void NumberTraits::unsafe_pow(void* dst, const void* base, int64_t exponent)
+        const
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    RPY_THROW(std::runtime_error, "this operation is not implemented");
+}
+void NumberTraits::unsafe_exp(void* dst, const void* src) const
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    RPY_THROW(std::runtime_error, "this operation is not implemented");
+}
+void NumberTraits::unsafe_log(void* dst, const void* src) const
+{
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    RPY_THROW(std::runtime_error, "this operation is not implemented");
+}
+
+void NumberTraits::real(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *real_type());
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_real(dst.data(), src.data());
+}
+void NumberTraits::imaginary(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(imaginary_type(), "this type is not complex");
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *real_type());
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_imaginary(dst.data(), src.data());
+}
+void NumberTraits::abs(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *real_type());
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_abs(dst.data(), src.data());
+}
+void NumberTraits::sqrt(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *real_type());
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_abs(dst.data(), src.data());
+}
+void NumberTraits::exp(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *p_type);
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_exp(dst.data(), src.data());
+}
+void NumberTraits::log(Ref dst, ConstRef src) const
+{
+    RPY_CHECK(dst.is_valid() && src.is_valid());
+    RPY_CHECK_EQ(dst.type(), *p_type);
+    RPY_CHECK_EQ(src.type(), *p_type);
+
+    unsafe_log(dst.data(), src.data());
+}
+void NumberTraits::from_rational(
+        Ref dst,
+        int64_t numerator,
+        int64_t denominator
+) const
+{
+    RPY_CHECK_NE(denominator, 0, std::domain_error);
+
+    RPY_CHECK(!dst.fast_is_zero());
+    RPY_CHECK_EQ(dst.type(), *p_type);
+
+    unsafe_from_rational(dst.data(), numerator, denominator);
+}
+Value NumberTraits::real(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    unsafe_real(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::imaginary(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    unsafe_real(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::abs(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(real_type());
+    unsafe_abs(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::sqrt(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    unsafe_sqrt(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::pow(ConstRef value, int64_t power) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    if (power != 1) {
+        unsafe_pow(result.data(), value.data(), power);
+    } else {
+        p_type->copy(result.data(), value.data(), 1, true);
+    }
+
+    return result;
+}
+Value NumberTraits::exp(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    unsafe_exp(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::log(ConstRef value) const
+{
+    RPY_CHECK(value.is_valid());
+    RPY_CHECK_EQ(value.type(), *p_type);
+
+    Value result(p_type);
+    unsafe_exp(result.data(), value.data());
+
+    return result;
+}
+Value NumberTraits::from_rational(int64_t numerator, int64_t denominator) const
+{
+    RPY_CHECK_NE(denominator, 0, std::domain_error);
+
+    Value result(p_type);
+    unsafe_from_rational(result.data(), numerator, denominator);
+
+    return result;
+}
