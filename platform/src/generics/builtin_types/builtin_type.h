d145feda69fc85bcc1edfd533ad4a304db8c91d0
Sam Morley <admin@inakleinbottle.com>
2024-11-17 23:41:48 +0000

    Remove extraneous blank lines in builtin_type.h
    Cleaned up the builtin_type.h file by removing unnecessary blank lines. This helps improve readability and maintainability of the code. No functional changes were made.

--- platform/src/generics/builtin_types/builtin_type.h
+++ platform/src/generics/builtin_types/builtin_type.h
@@ -34,7 +34,6 @@ class BuiltinTypeBase : public Type
     ComparisonTraitImpl<T> m_comparison_trait;
     NumberTraitImpl<T> m_number_trait;
 
-
     // std::unordered_map<string_view, std::unique_ptr<const Trait>> m_traits;
 
     hash_t hash_with_type(const Type& other_type) const noexcept
@@ -46,7 +45,6 @@ class BuiltinTypeBase : public Type
     }
 
 protected:
-
     BuiltinTypeBase()
         : Type(&typeid(T), sizeof(T), basic_properties_of<T>()),
           m_arithmetic_trait(this, this),
@@ -58,8 +56,6 @@ protected:
     bool dec_ref() const noexcept override;
     void* allocate_object() const override;
     void free_object(void*) const override;
-
-
 
 public:
     void copy_or_move(void* dst, const void* src, size_t count, bool move)
@@ -75,8 +71,9 @@ public:
     const std::ostream&
     display(std::ostream& os, const void* value) const override;
 
-
+    hash_t hash_of(const void* value) const noexcept override;
 };
+
 
 
 }// namespace rpy::generics
