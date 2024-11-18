e03566a481add972ca4860e78695dd8889d46cf8
Sam Morley <admin@inakleinbottle.com>
2024-11-18 13:27:15 +0000

    Add float type support in built-in types
    Updated the get_builtin_types function to include the float type. This ensures that float type can now be utilized in addition to the double type, expanding the type handling capabilities of the module.

--- platform/src/generics/type_builtin.cpp
+++ platform/src/generics/type_builtin.cpp
@@ -7,7 +7,7 @@
 #include "roughpy/generics/type.h"
 
 #include "builtin_types/double_type.h"
-
+#include "builtin_types/float_type.h"
 
 using namespace rpy;
 using namespace rpy::generics;
@@ -16,7 +16,7 @@ using namespace rpy::generics;
 const BuiltinTypes& rpy::generics::get_builtin_types() noexcept
 {
     static BuiltinTypes builtins {
-        nullptr,
+        FloatType::get(),
         DoubleType::get(),
 
         nullptr,
