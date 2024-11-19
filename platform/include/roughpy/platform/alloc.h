5043a4e7365b5fe3f12c6a04926ae2a2117da00b
Sam Morley <admin@inakleinbottle.com>
2024-11-19 22:18:36 +0000

    Enhance memory management with small object allocator
    Added support for efficient small object allocation and deallocation using a pool resource. Introduced new classes and constants to manage small memory chunks. Optimized aligned allocation and free functions with noexcept guarantees and size parameters.

--- platform/include/roughpy/platform/alloc.h
+++ platform/include/roughpy/platform/alloc.h
@@ -11,12 +11,32 @@
 #include "roughpy/platform/roughpy_platform_export.h"
 
 namespace rpy::mem {
-RPY_NO_DISCARD ROUGHPY_PLATFORM_EXPORT void* aligned_alloc(size_t alignment, size_t size);
-
-ROUGHPY_PLATFORM_EXPORT void aligned_free(void* ptr);
 
 
+// 4K is a typical page size most operating systems
+inline constexpr size_t small_chunk_size = 4096;
+inline constexpr size_t small_block_size = 64;
+inline constexpr size_t small_blocks_per_chunk = small_chunk_size / small_block_size;
 
-}
+RPY_NO_DISCARD ROUGHPY_PLATFORM_EXPORT void*
+aligned_alloc(size_t alignment, size_t size) noexcept;
 
-#endif //ALLOC_H
+ROUGHPY_PLATFORM_EXPORT void aligned_free(void* ptr, size_t size=0) noexcept;
+
+RPY_NO_DISCARD ROUGHPY_PLATFORM_EXPORT
+void* small_object_alloc(size_t size);
+
+ROUGHPY_PLATFORM_EXPORT
+void small_object_free(void* ptr, size_t size);
+
+
+class ROUGHPY_PLATFORM_EXPORT SmallObjectBase
+{
+public:
+    void* operator new(size_t size);
+    void operator delete(void* object, size_t size);
+};
+
+}// namespace rpy::mem
+
+#endif// ALLOC_H
