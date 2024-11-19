6c30ec3c979c04a8b69c91eeca2098c6f4c3a17a
Sam Morley <admin@inakleinbottle.com>
2024-11-19 14:29:35 +0000

    Add pragma once and refactor macros.h; introduce config.h
    Added `#pragma once` to `macros.h` for better include guard management and refactored various sections for improved readability. Created a new `config.h` file to centralize platform, architecture, endianness, and compiler detection logic. Updated CMakeLists.txt to include the new `config.h` file.

---
+++ core/include/roughpy/core/detail/config.h
@@ -0,0 +1,107 @@
+//
+// Created by sam on 19/11/24.
+//
+#pragma once
+
+#ifndef ROUGHPY_CORE_DETAIL_CONFIG_H
+#  define ROUGHPY_CORE_DETAIL_CONFIG_H
+
+// Detect operating system
+#  if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
+#    define RPY_PLATFORM_WINDOWS 1
+#  elif defined(__unix__) || defined(__unix)
+#    define RPY_PLATFORM_UNIX 1
+#    if defined(__APPLE__) || defined(__MACH__)
+#      define RPY_PLATFORM_MACOS 1
+#    elif defined(__linux__)
+#      define RPY_PLATFORM_LINUX 1
+#    elif defined(__FreeBSD__)
+#      define RPY_PLATFORM_FREEBSD 1
+#    endif
+#  elif defined(__APPLE__) || defined(__MACH__)
+#    define RPY_PLATFORM_MACOS 1
+#  else
+#    define RPY_PLATFORM_UNKNOWN 0
+#  endif
+
+// Detect Architecture
+#  if defined(_M_X64) || defined(__amd64__) || defined(__x86_64__)             \
+          || defined(_M_AMD64)
+#    define RPY_ARCH_X64
+#  elif defined(_M_IX86) || defined(__i386__) || defined(_X86_)
+#    define RPY_ARCH_X86
+#  elif defined(__aarch64__) || defined(_M_ARM64)
+#    define RPY_ARCH_ARM64
+#  elif defined(__arm__) || defined(_M_ARM)
+#    define RPY_ARCH_ARM
+#  elif defined(__powerpc) || defined(_M_PPC) || defined(__ppc)                \
+          || defined(_ARCH_PPC)
+#    define RPY_ARCH_PPC
+#  elif defined(__riscv)
+#    define RPY_ARCH_RISCV
+#  else
+#    define RPY_ARCH_UNKNOWN
+#  endif
+
+// Detect Endianness
+#if defined(__BYTE_ORDER__) && defined(__ORDER_LITTLE_ENDIAN__) && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#  define RPY_IS_LITTLE_ENDIAN 1
+#elif defined(__BYTE_ORDER__) && defined(__ORDER_BIG_ENDIAN__) && __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
+#  define RPY_IS_BIG_ENDIAN 1
+#elif RPY_PLATFORM_WINDOWS
+// Abseil sets
+#  define RPY_IS_LITTLE_ENDIAN 1
+#else
+#  error "Cannot detect the endianness of this platform
+#endif
+
+
+// Detect Compiler and Version
+#  if defined(_MSC_VER)
+#    define RPY_COMPILER_MSVC _MSC_VER
+#  elif defined(__clang__)
+#    define RPY_COMPILER_CLANG                                                 \
+        (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__)
+#  elif defined(__GNUC__) || defined(__GNUG__)
+#    define RPY_COMPILER_GCC                                                   \
+        (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__)
+#  elif defined(__INTEL_COMPILER)
+#    define RPY_COMPILER_INTEL __INTEL_COMPILER
+#  elif defined(__NVCC__)
+#    define RPY_COMPILER_NVCC                                                  \
+        (__CUDACC_VER_MAJOR__ * 10000 + __CUDACC_VER_MINOR__ * 100             \
+         + __CUDACC_VER_PATCHLEVEL__)
+#  else
+#    define RPY_COMPILER_UNKNOWN 0
+#  endif
+
+// Detect the C++ standard
+#  if defined(__cplusplus)
+// At some point we might have non-c++ parts of the library.
+#    if defined(_MSVC_LANG)
+// MSVC does not correctly set __cplusplus, so we have to use _MSVC_LANG to get
+// the version of the C++ standard supported by the compiler. (Abseil has a
+// comment that even this is not always valid.)
+#      define RPY_CPP_VERSION _MSVC_LANG
+#    else
+#      define RPY_CPP_VERSION __cplusplus
+#    endif
+#  endif
+
+#  if RPY_CPP_VERSION >= 201103L
+#    define RPY_CPP_11 1
+#  endif
+#  if RPY_CPP_VERSION >= 201403L
+#    define RPY_CPP_14 1
+#  endif
+#  if RPY_CPP_VERSION >= 201703L
+#    define RPY_CPP_17 1
+#  endif
+#  if RPY_CPP_VERSION >= 202002L
+#    define RPY_CPP_20 1
+#  endif
+#  if RPY_CPP_VERSION >= 202303L
+#    define RPY_CPP_23 1
+#  endif
+
+#endif// ROUGHPY_CORE_DETAIL_CONFIG_H
--- core/include/roughpy/core/macros.h
+++ core/include/roughpy/core/macros.h
@@ -30,35 +30,36 @@
 // Created by user on 04/03/23.
 //
 
+#pragma once
+
 #ifndef ROUGHPY_CORE_MACROS_H
-#define ROUGHPY_CORE_MACROS_H
+#  define ROUGHPY_CORE_MACROS_H
 
+#  include "detail/config.h"
 
+#  ifdef __has_builtin
+#    define RPY_HAS_BUILTIN(x) __has_builtin(x)
+#  else
+#    define RPY_HAS_BUILTIN(x) 0
+#  endif
 
+#  ifdef __has_feature
+#    define RPY_HAS_FEATURE(FEAT) __has_feature(FEAT)
+#  else
+#    define RPY_HAS_FEATURE(FEAT) 0
+#  endif
 
-#ifdef __has_builtin
-#  define RPY_HAS_BUILTIN(x) __has_builtin(x)
-#else
-#  define RPY_HAS_BUILTIN(x) 0
-#endif
+#  ifdef __has_cpp_attribute
+#    define RPY_HAS_CPP_ATTRIBUTE(x) __has_cpp_attribute(x)
+#  else
+#    define RPY_HAS_CPP_ATTRIBUTE(x) 0
+#  endif
 
-#ifdef __has_feature
-#  define RPY_HAS_FEATURE(FEAT) __has_feature(FEAT)
-#else
-#  define RPY_HAS_FEATURE(FEAT) 0
-#endif
-
-#ifdef __has_cpp_attribute
-#  define RPY_HAS_CPP_ATTRIBUTE(x) __has_cpp_attribute(x)
-#else
-#  define RPY_HAS_CPP_ATTRIBUTE(x) 0
-#endif
-
-#ifdef __has_include
-#  define RPY_HAS_INCLUDE(x) __has_include(x)
-#else
-#  define RPY_HAS_INCLUDE(x) 0
-#endif
+#  ifdef __has_include
+#    define RPY_HAS_INCLUDE(x) __has_include(x)
+#  else
+#    define RPY_HAS_INCLUDE(x) 0
+#  endif
 
 /*
  * The C++ preprocessor is very basic, but one can accomplish fairly advanced
@@ -67,20 +68,21 @@
  * to do. One of main consumers of this set of utilities are the RPY_CHECK and
  * RPY_DBG_ASSERT macros in Core.
  *
- * See https://github.com/pfultz2/Cloak/wiki/C-Preprocessor-tricks,-tips,-and-idioms
+ * See
+ * https://github.com/pfultz2/Cloak/wiki/C-Preprocessor-tricks,-tips,-and-idioms
  * and the C++template metaprogramming book by David Abrahams and
  * Aleksey Gurtovoy
  */
-#define RPY_STRINGIFY_IMPL(ARG) #ARG
-#define RPY_STRINGIFY(ARG) RPY_STRINGIFY_IMPL(ARG)
+#  define RPY_STRINGIFY_IMPL(ARG) #ARG
+#  define RPY_STRINGIFY(ARG) RPY_STRINGIFY_IMPL(ARG)
 
-#define RPY_JOIN_IMPL_IMPL(LHS, RHS) LHS##RHS
-#define RPY_JOIN_IMPL(LHS, RHS) RPY_JOIN_IMPL_IMPL(LHS, RHS)
-#define RPY_JOIN(LHS, RHS) RPY_JOIN_IMPL(LHS, RHS)
+#  define RPY_JOIN_IMPL_IMPL(LHS, RHS) LHS##RHS
+#  define RPY_JOIN_IMPL(LHS, RHS) RPY_JOIN_IMPL_IMPL(LHS, RHS)
+#  define RPY_JOIN(LHS, RHS) RPY_JOIN_IMPL(LHS, RHS)
 
-#define RPY_IDENTITY(ARG) ARG
-#define RPY_EMPTY(...)
-#define RPY_DEFER(...) __VA_ARGS__
+#  define RPY_IDENTITY(ARG) ARG
+#  define RPY_EMPTY(...)
+#  define RPY_DEFER(...) __VA_ARGS__
 
 /*
  * MSVC has a bug where it treats __VA_ARGS__ as a single token in argument
@@ -88,267 +90,190 @@
  * the __VA_ARGS__ in the token stream so that proper expansion occurs.
  * See: https://stackoverflow.com/a/9338429
  */
-#define RPY_INVOKE_VA(X, Y) X Y
+#  define RPY_INVOKE_VA(X, Y) X Y
 
-#define RPY_COUNT_ARGS_1(_4, _3, _2, _1, count, ...) count
-#define RPY_COUNT_ARGS(...)                                                    \
-    RPY_INVOKE_VA(RPY_COUNT_ARGS_1, (__VA_ARGS__, 4, 3, 2, 1, 0))
+#  define RPY_COUNT_ARGS_1(_4, _3, _2, _1, count, ...) count
+#  define RPY_COUNT_ARGS(...)                                                  \
+      RPY_INVOKE_VA(RPY_COUNT_ARGS_1, (__VA_ARGS__, 4, 3, 2, 1, 0))
 
-
-
-#if defined(_MSC_VER) && defined(_MSVC_LANG)
-#  define RPY_CPP_VERSION _MSVC_LANG
-#  define RPY_MSVC _MSC_VER
-#else
-#  define RPY_CPP_VERSION __cplusplus
-#  undef RPY_MSVC
-#endif
-
-#if defined(__GNUC__) && !defined(__clang__)
-#  define RPY_GCC __GNUC__
-#else
-#  undef RPY_GCC
-#endif
-
-#ifdef __clang__
-#  define RPY_CLANG __clang__
-#else
-#  undef RPY_CLANG
-#endif
-
-#if defined(__linux__)
-#  define RPY_PLATFORM_LINUX 1
-#elif defined(__APPLE__) && defined(__MACH__)
-#  define RPY_PLATFORM_MACOS 1
-#elif defined(_WIN32) || defined(WIN32)
-#  define RPY_PLATFORM_WINDOWS 1
-#endif
-
-#if defined(RPY_PLATFORM_WINDOWS) || defined(__CYGWIN__)
-#  define RPY_COMPILING_DLL
-#  define RPY_DLL_EXPORT __declspec(dllexport)
-#  define RPY_DLL_IMPORT __declspec(dllimport)
-#else
-#  define RPY_DLL_EXPORT
-#  define RPY_DLL_IMPORT
-#endif
-
-#ifndef RPY_DISABLE_EXPORTS
-#  ifdef RPY_COMPILING_DLL
-#    if RPY_BUILDING_LIBRARY
-#      if defined(RPY_MSVC)
-#        define RPY_EXPORT __declspec(dllexport)
-#        define RPY_LOCAL
-#      else
-#        define RPY_EXPORT __attribute__((dllexport))
-#        define RPY_LOCAL
-#      endif
-#    else
-#      if defined(RPY_MSVC)
-#        define RPY_EXPORT __declspec(dllimport)
-#        define RPY_LOCAL
-#      else
-#        define RPY_EXPORT __attribute__((dllimport))
-#      endif
-#    endif
+#  if defined(RPY_PLATFORM_WINDOWS)
+#    define RPY_COMPILING_DLL
+#    define RPY_DLL_EXPORT __declspec(dllexport)
+#    define RPY_DLL_IMPORT __declspec(dllimport)
 #  else
-#    if (defined(RPY_GCC) && RPY_GCC >= 4) || defined(RPY_CLANG)
-#      if defined(RPY_BUILDING_LIBRARY)
-#        define RPY_LOCAL __attribute__((visibility("hidden")))
-#      else
-#        define RPY_LOCAL __attribute__((visibility("hidden")))
-#      endif
-#    else
-#      define RPY_LOCAL
-#    endif
+#    define RPY_DLL_EXPORT
+#    define RPY_DLL_IMPORT
 #  endif
-#else
-#  define RPY_LOCAL
-#endif
+
+#  if defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_LOCAL __attribute__((visibility("hidden")))
+#  else
+#    define RPY_LOCAL
+#  endif
 
 /*
- * MSVC pre-defines min and max macros. undef them because this is insane
+ * MSVC pre-defines min and max macros, undef them because this is insane and
+ * disable this behaviour
  */
-#ifdef RPY_PLATFORM_WINDOWS
-#  undef min
-#  undef max
-#endif
+#  ifdef RPY_PLATFORM_WINDOWS
+#    define NOMINMAX 1
+#    undef min
+#    undef max
+#  endif
 
-
-#if RPY_CPP_VERSION >= 201403L
-#  define RPY_CPP_14
-#else
-#  undef RPY_CPP_14
-#endif
-
-#if RPY_CPP_VERSION >= 201703L
-#  define RPY_CPP_17
-#else
-#  undef RPY_CPP_17
-#endif
-
-#ifdef RPY_CPP_17
-#  define RPY_UNUSED [[maybe_unused]]
+#  define RPY_MAYBE_UNUSED [[maybe_unused]]
 #  define RPY_NO_DISCARD [[nodiscard]]
 #  define RPY_NO_RETURN [[noreturn]]
 #  define RPY_IF_CONSTEXPR constexpr
-#else
-#  define RPY_UNUSED
-#  define RPY_NO_DISCARD
-#  define RPY_IF_CONSTEXPR
-#  define RPY_NO_RETURN
-#endif
 
-#if defined(RPY_GCC) || defined(RPY_CLANG)
-#  define RPY_UNUSED_VAR __attribute__((unused))
-#else
-#  define RPY_UNUSED_VAR
-#endif
+#  if defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_UNUSED_VAR __attribute__((unused))
+#  else
+#    define RPY_UNUSED_VAR
+#  endif
 
-#if defined(RPY_GCC) || defined(RPY_CLANG)
-#  define RPY_UNREACHABLE() (__builtin_unreachable())
-#  define RPY_UNREACHABLE_RETURN(...) RPY_UNREACHABLE()
-#elif defined(RPY_MSVC)
-#  define RPY_UNREACHABLE() (__assume(false))
-#  define RPY_UNREACHABLE_RETURN(...)                                          \
-      RPY_UNREACHABLE();                                                       \
-      return __VA_ARGS__
-#else
-#  define RPY_UNREACHABLE() abort()
-#  define RPY_UNREACHABLE_RETURN(...)                                          \
-      RPY_UNREACHABLE();                                                       \
-      return __VA_ARGS__
-#endif
+#  if defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_UNREACHABLE() (__builtin_unreachable())
+#    define RPY_UNREACHABLE_RETURN(...) RPY_UNREACHABLE()
+#  elif defined(RPY_COMPILER_MSVC)
+#    define RPY_UNREACHABLE() (__assume(false))
+#    define RPY_UNREACHABLE_RETURN(...)                                        \
+        RPY_UNREACHABLE();                                                     \
+        return __VA_ARGS__
+#  else
+#    define RPY_UNREACHABLE() abort()
+#    define RPY_UNREACHABLE_RETURN(...)                                        \
+        RPY_UNREACHABLE();                                                     \
+        return __VA_ARGS__
+#  endif
 
-#ifdef RPY_MSVC
-#  define RPY_PRAGMA(ARG) __pragma(ARG)
-#else
-#  define RPY_PRAGMA(ARG) _Pragma(RPY_STRINGIFY(ARG))
-#endif
+#  ifdef RPY_COMPILER_MSVC
+#    define RPY_PRAGMA(ARG) __pragma(ARG)
+#  else
+#    define RPY_PRAGMA(ARG) _Pragma(RPY_STRINGIFY(ARG))
+#  endif
 
 // Macros that control optimisations
-
-#if defined(__OPTIMIZE__) || !defined(RPY_DEBUG)
-#  if defined(_WIN32) || defined(_WIN64)
-#    define RPY_INLINE_ALWAYS __forceinline
-#  elif defined(__GNUC__) || defined(__clang__)
-#    define RPY_INLINE_ALWAYS inline __attribute__((always_inline))
+#  if defined(__OPTIMIZE__) || !defined(RPY_DEBUG)
+#    if defined(_WIN32) || defined(_WIN64)
+#      define RPY_INLINE_ALWAYS __forceinline
+#    elif defined(__GNUC__) || defined(__clang__)
+#      define RPY_INLINE_ALWAYS inline __attribute__((always_inline))
+#    else
+#      define RPY_INLINE_ALWAYS inline
+#    endif
 #  else
 #    define RPY_INLINE_ALWAYS inline
 #  endif
-#else
-#  define RPY_INLINE_ALWAYS inline
-#endif
 
-#ifdef RPY_MSVC
-#  define RPY_INLINE_NEVER __declspec(noinline)
-#elif defined(RPY_GCC) || defined(RPY_CLANG)
-#  define RPY_INLINE_NEVER __attribute__((never_inline))
-#else
-#  define RPY_INLINE_NEVER
-#endif
-
-#if defined(RPY_GCC) || defined(RPY_CLANG)
-#  define RPY_RESTRICT __restrict__
-#elif defined(RPY_MSVC)
-#  define RPY_RESTRICT __restrict
-#else
-#  define RPY_RESTRICT
-#endif
-
-#if defined(RPY_GCC) || defined(RPY_CLANG)
-#  define RPY_LIKELY(COND) (__builtin_expect(static_cast<bool>(COND), 1))
-#  define RPY_UNLIKELY(COND) (__builtin_expect(static_cast<bool>(COND), 0))
-#else
-#  define RPY_LIKELY(COND) (COND)
-#  define RPY_UNLIKELY(COND) (COND)
-#endif
-
-#define RPY_FALLTHROUGH (void) 0
-
-#if defined(RPY_PLATFORM_WINDOWS)
-#  if RPY_BUILDING_LIBRARY
-#    define RPY_TEMPLATE_EXTERN
-#    define RPY_EXPORT_TEMPLATE
-#    define RPY_EXPORT_INSTANTIATION RPY_EXPORT
+#  ifdef RPY_COMPILER_MSVC
+#    define RPY_INLINE_NEVER __declspec(noinline)
+#  elif defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_INLINE_NEVER __attribute__((never_inline))
 #  else
-#    define RPY_TEMPLATE_EXTERN
+#    define RPY_INLINE_NEVER
+#  endif
+
+#  if defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_RESTRICT __restrict__
+#  elif defined(RPY_COMPILER_MSVC)
+#    define RPY_RESTRICT __restrict
+#  else
+#    define RPY_RESTRICT
+#  endif
+
+#  if defined(RPY_COMPILER_GCC) || defined(RPY_COMPILER_CLANG)
+#    define RPY_LIKELY(COND) (__builtin_expect(static_cast<bool>(COND), 1))
+#    define RPY_UNLIKELY(COND) (__builtin_expect(static_cast<bool>(COND), 0))
+#  else
+#    define RPY_LIKELY(COND) (COND)
+#    define RPY_UNLIKELY(COND) (COND)
+#  endif
+
+#  define RPY_FALLTHROUGH (void) 0
+
+#  if defined(RPY_PLATFORM_WINDOWS)
+#    if RPY_BUILDING_LIBRARY
+#      define RPY_TEMPLATE_EXTERN
+#      define RPY_EXPORT_TEMPLATE
+#      define RPY_EXPORT_INSTANTIATION RPY_EXPORT
+#    else
+#      define RPY_TEMPLATE_EXTERN
+#      define RPY_EXPORT_TEMPLATE RPY_EXPORT
+#      define RPY_EXPORT_INSTANTIATION
+#    endif
+#  else
+#    define RPY_TEMPLATE_EXTERN extern
 #    define RPY_EXPORT_TEMPLATE RPY_EXPORT
 #    define RPY_EXPORT_INSTANTIATION
 #  endif
-#else
-#  define RPY_TEMPLATE_EXTERN extern
-#  define RPY_EXPORT_TEMPLATE RPY_EXPORT
-#  define RPY_EXPORT_INSTANTIATION
-#endif
 
 // Sanitizer supports
-#ifdef RPY_CLANG
-#  define RPY_NO_UBSAN __attribute__((no_sanitize("undefined")))
-#else
-#  define RPY_NO_UBSAN
-#endif
+#  if defined(RPY_USING_UBSAN)
+#    define RPY_NO_UBSAN __attribute__((no_sanitize("undefined")))
+#  else
+#    define RPY_NO_UBSAN
+#  endif
 
-#if RPY_HAS_FEATURE(address_sanitizer)
-#  define RPY_NO_ASAN __attribute__((no_sanitize("address")))
-#else
-#  define RPY_NO_ASAN
-#endif
+#  if RPY_HAS_FEATURE(address_sanitizer)
+#    define RPY_NO_ASAN __attribute__((no_sanitize("address")))
+#  else
+#    define RPY_NO_ASAN
+#  endif
 
 // Warning and error control
-#if defined(RPY_MSVC)
-#  define RPY_WARNING_PUSH RPY_PRAGMA(warning(push))
-#  define RPY_WARNING_POP RPY_PRAGMA(warning(pop))
-#elif defined(RPY_GCC)
-#  define RPY_WARNING_PUSH RPY_PRAGMA(GCC diagnostic push)
-#  define RPY_WARNING_POP RPY_PRAGMA(GCC diagnostic pop)
-#elif defined(RPY_CLANG)
-#  define RPY_WARNING_PUSH RPY_PRAGMA(clang diagnostic push)
-#  define RPY_WARNING_POP RPY_PRAGMA(clang diagnostic pop)
-#else
-#  define RPY_WARNING_PUSH
-#  define RPY_WARNING_POP
-#endif
+#  if defined(RPY_COMPILER_MSVC)
+#    define RPY_WARNING_PUSH RPY_PRAGMA(warning(push))
+#    define RPY_WARNING_POP RPY_PRAGMA(warning(pop))
+#  elif defined(RPY_COMPILER_GCC)
+#    define RPY_WARNING_PUSH RPY_PRAGMA(GCC diagnostic push)
+#    define RPY_WARNING_POP RPY_PRAGMA(GCC diagnostic pop)
+#  elif defined(RPY_COMPILER_CLANG)
+#    define RPY_WARNING_PUSH RPY_PRAGMA(clang diagnostic push)
+#    define RPY_WARNING_POP RPY_PRAGMA(clang diagnostic pop)
+#  else
+#    define RPY_WARNING_PUSH
+#    define RPY_WARNING_POP
+#  endif
 
-#ifdef RPY_MSVC
-#  define RPY_MSVC_DISABLE_WARNING(ARG) RPY_PRAGMA(warning(disable : ARG))
-#else
-#  define RPY_MSVC_DISABLE_WARNING(ARG)
-#endif
+#  ifdef RPY_COMPILER_MSVC
+#    define RPY_MSVC_DISABLE_WARNING(ARG) RPY_PRAGMA(warning(disable : ARG))
+#  else
+#    define RPY_MSVC_DISABLE_WARNING(ARG)
+#  endif
 
-#ifdef RPY_GCC
-#  define RPY_GCC_DISABLE_WARNING(ARG)                                         \
-      RPY_PRAGMA(GCC diagnostic ignored RPY_STRINGIFY(ARG))
-#else
-#  define RPY_GCC_DISABLE_WARNING(ARG)
-#endif
+#  ifdef RPY_COMPILER_GCC
+#    define RPY_GCC_DISABLE_WARNING(ARG)                                       \
+        RPY_PRAGMA(GCC diagnostic ignored RPY_STRINGIFY(ARG))
+#  else
+#    define RPY_GCC_DISABLE_WARNING(ARG)
+#  endif
 
-#ifdef RPY_CLANG
-#  define RPY_CLANG_DISABLE_WARNING(ARG)                                       \
-      RPY_PRAGMA(clang diagnostic ignored RPY_STRINGIFY(ARG))
-#else
-#  define RPY_CLANG_DISABLE_WARNING(ARG)
-#endif
+#  ifdef RPY_COMPILER_CLANG
+#    define RPY_CLANG_DISABLE_WARNING(ARG)                                     \
+        RPY_PRAGMA(clang diagnostic ignored RPY_STRINGIFY(ARG))
+#  else
+#    define RPY_CLANG_DISABLE_WARNING(ARG)
+#  endif
 
-#if defined(RPY_GCC)
-#  define RPY_FUNC_NAME __PRETTY_FUNCTION__
-#elif defined(RPY_CLANG)
-#  define RPY_FUNC_NAME __builtin_FUNCTION()
-#elif defined(RPY_MSVC)
-#  define RPY_FUNC_NAME __FUNCTION__
-#else
-#  define RPY_FUNC_NAME static_cast<const char*>(0)
-#endif
+#  if defined(RPY_COMPILER_GCC)
+#    define RPY_FUNC_NAME __PRETTY_FUNCTION__
+#  elif defined(RPY_COMPILER_CLANG)
+#    define RPY_FUNC_NAME __builtin_FUNCTION()
+#  elif defined(RPY_COMPILER_MSVC)
+#    define RPY_FUNC_NAME __FUNCTION__
+#  else
+#    define RPY_FUNC_NAME static_cast<const char*>(0)
+#  endif
 
-#if defined(RPY_GCC)
-#  define RPY_FILE_NAME __FILE__
-#elif defined(RPY_CLANG)
-#  define RPY_FILE_NAME __FILE__
-#elif defined(RPY_MSVC)
-#  define RPY_FILE_NAME __FILE__
-#else
-#  define RPY_FILE_NAME __FILE__
-#endif
-
+#  if defined(RPY_COMPILER_GCC)
+#    define RPY_FILE_NAME __FILE__
+#  elif defined(RPY_COMPILER_CLANG)
+#    define RPY_FILE_NAME __FILE__
+#  elif defined(RPY_COMPILER_MSVC)
+#    define RPY_FILE_NAME __FILE__
+#  else
+#    define RPY_FILE_NAME __FILE__
+#  endif
 
 #endif// ROUGHPY_CORE_MACROS_H
