6d917adf51e6d8f3a3ce7885f2be228aa6a1a5bb
Sam Morley <admin@inakleinbottle.com>
2024-11-19 14:40:20 +0000

    Add range-v3 library to project
    Included range-v3 as a dependency in `vcpkg.json` and added the required configuration in `CMakeLists.txt` files. Created a new header file, `ranges.h`, to make use of range-v3 functionalities in the core module.

---
+++ core/include/roughpy/core/ranges.h
@@ -0,0 +1,17 @@
+//
+// Created by sam on 19/11/24.
+//
+
+#ifndef ROUGHPY_CORE_RANGES_H
+#define ROUGHPY_CORE_RANGES_H
+
+#include <range/v3/all.hpp>
+
+namespace rpy {
+
+namespace ranges = ::ranges;
+namespace views = ::ranges::views;
+
+}
+
+#endif //ROUGHPY_CORE_RANGES_H
