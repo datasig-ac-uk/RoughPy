16ae75430d7bc6ae67188cde0f4e4183d40264cb
Sam Morley <admin@inakleinbottle.com>
2024-11-22 11:51:59 +0000

    Add device categories and identifiers enums
    Introduced `DeviceCategory` and `DeviceIdType` enums to classify devices and identification methods. Updated the `DeviceHandle` class with additional documentation and added functions for event and queue management.

--- platform/include/roughpy/device/device_handle.h
+++ platform/include/roughpy/device/device_handle.h
@@ -28,8 +28,42 @@ class Type;
 
 namespace device {
 
-class Memory;
+enum class DeviceCategory : int32_t
+{
+    CPU = 0,
+    GPU = 1,
+    FPGA = 2,
+    DSP = 3,
+    AIP = 4,
+    Other = 5
+};
 
+enum class DeviceIdType : int32_t
+{
+    None = 0,
+    VendorID = 1,
+    UUID = 2,
+    PCI = 3
+};
+
+
+// Forward declarations of types that the device handle needs to interact with
+class Memory;
+class Queue;
+class Event;
+
+/**
+ * @brief Represents a handle to a device, which abstracts the interaction with
+ * hardware or virtual devices.
+ *
+ * This class provides methods to open, close, read from, write to, and manage
+ * the state of a device. It encapsulates the details of device communication
+ * and ensures safe and controlled access to the device.
+ *
+ * Typical usage involves creating an instance of DeviceHandle, opening a
+ * connection to the device, performing read/write operations, and finally
+ * closing the connection.
+ */
 class ROUGHPY_PLATFORM_EXPORT DeviceHandle : public mem::PolymorphicRefCounted
 {
 
@@ -41,15 +75,21 @@ public:
     virtual bool is_host() const noexcept;
     virtual bool supports_type(const generics::Type& type) const noexcept = 0;
 
+    // Memory management
     RPY_NO_DISCARD virtual Rc<Memory> allocate_memory(
             const generics::Type& type,
             size_t size,
             size_t alignment
     ) const = 0;
-
     virtual void copy_memory(Memory& dst, const Memory& src) const = 0;
-
     virtual void destroy_memory(Memory& memory) const = 0;
+
+    // Event management
+    virtual Rc<Event> new_event() const;
+
+    // Queue management
+    virtual Rc<Queue> new_queue() const;
+
 
     static Rc<const DeviceHandle> host() noexcept;
 };
