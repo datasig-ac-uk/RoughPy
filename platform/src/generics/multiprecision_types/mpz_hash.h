ad326ca60c84928139f4384fc6e38843f3f4c8e2
Sam Morley <admin@inakleinbottle.com>
2024-11-25 20:40:08 +0000

    Update hash calculation in mpz_hash.h
    Changed hash initialization to use the mp_size_t of the integer. This ensures a more unique starting hash value and potentially improves collision resistance in hash computations.

--- platform/src/generics/multiprecision_types/mpz_hash.h
+++ platform/src/generics/multiprecision_types/mpz_hash.h
@@ -17,7 +17,7 @@ inline hash_t mpz_hash(mpz_srcptr integer) noexcept
     const size_t nlimbs = mpz_size(integer);
     const auto* limbs = mpz_limbs_read(integer);
 
-    hash_t result = 0;
+    hash_t result = Hash<mp_size_t>{}(integer->_mp_size);
     for (size_t i = 0; i < nlimbs; ++i) {
         hash_combine(result, limbs[i]);
     }
