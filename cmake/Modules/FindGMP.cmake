94973771c7b0f37b434a04d899abb5f420580a42
Sam Morley <admin@inakleinbottle.com>
2024-11-26 15:17:51 +0000

    Enhance GMP find module with imported target support
    Updated the FindGMP.cmake module to use pkg_check_modules with IMPORTED_TARGET, ensuring the creation of the GMP::GMP target when the package is found. This streamlined the handling of GMP library properties and reduced the redundancy in code, making it easier to manage and maintain.

--- cmake/Modules/FindGMP.cmake
+++ cmake/Modules/FindGMP.cmake
@@ -9,15 +9,17 @@ include(SelectLibraryConfigurations)
 find_package(PkgConfig QUIET)
 
 if (PkgConfig_FOUND)
-    pkg_check_modules(PC_GMP QUIET gmp)
+    pkg_check_modules(PC_GMP QUIET gmp IMPORTED_TARGET)
 endif ()
 
-if (GMP_PKG_FOUND)
+if (PC_GMP_FOUND)
     set(GMP_INCLUDE_DIR "${PC_GMP_INCLUDE_DIRS}")
     set(GMP_LIBRARY_DIRS "${PC_GMP_LIBRARY_DIRS}")
     set(GMP_LIBRARIES "${PC_GMP_LIBRARIES}")
     set(GMP_COMPILE_OPTIONS ${PC_GMP_CFLAGS} ${PC_GMP_CFLAGS_OTHER})
     set(GMP_LINK_OPTIONS ${PC_GMP_LDFLAGS} ${PC_GMP_LDFLAGS_OTHER})
+
+    add_library(GMP::GMP ALIAS PkgConfig::PC_GMP)
 else()
 
     set(_gmp_lib_names gmp libgmp gmp-10 libgmp-10 mpir libmpir)
@@ -52,60 +54,55 @@ else()
 
 
     select_library_configurations(GMP)
-
-endif ()
-
-
-
-
-find_package_handle_standard_args(GMP
-        FOUND_VAR GMP_FOUND
-        REQUIRED_VARS
+    find_package_handle_standard_args(GMP
+            FOUND_VAR GMP_FOUND
+            REQUIRED_VARS
             GMP_LIBRARY GMP_INCLUDE_DIR)
 
-mark_as_advanced(
-        GMP_INCLUDE_DIR
-        GMP_LIBRARIES
-        GMP_LIBRARY_DIRS
-        GMP_COMPILE_OPTIONS
-        GMP_LINK_OPTIONS
-)
-
-
-if (GMP_FOUND AND NOT TARGET GMP::GMP)
-    add_library(GMP::GMP UNKNOWN IMPORTED GLOBAL)
-    set_target_properties(GMP::GMP PROPERTIES
-            IMPORTED_LOCATION "${GMP_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}"
-            IMPORTED_IMPLIB "${GMP_LIBRARY}"
+    mark_as_advanced(
+            GMP_INCLUDE_DIR
+            GMP_LIBRARIES
+            GMP_LIBRARY_DIRS
+            GMP_COMPILE_OPTIONS
+            GMP_LINK_OPTIONS
     )
-#    if (GMP_COMPILE_OPTIONS)
-#        set_target_properties(GMP::GMP PROPERTIES
-#                INTERFACE_COMPILE_OPTIONS "${GMP_COMPILE_OPTIONS}"
-#        )
-#    endif()
-#    if(GMP_LINK_OPTIONS)
-#        set_target_properties(GMP::GMP PROPERTIES
-#                INTERFACE_LINK_OPTIONS "${GMP_LINK_OPTIONS}"
-#        )
-#    endif()
-#
-#    if (GMP_LIBRARY_RELEASE)
-#        set_property(TARGET GMP::GMP APPEND PROPERTY
-#            IMPORTED_CONFIGURATIONS RELEASE
-#        )
-#        set_target_properties(GMP::GMP PROPERTIES
-#                IMPORTED_LOCATION_RELEASE "${GMP_LIBRARY_RELEASE}"
-#        )
-#    endif()
-#    if (GMP_LIBRARY_DEBUG)
-#        set_property(TARGET GMP::GMP APPEND PROPERTY
-#                IMPORTED_CONFIGURATIONS DEBUG
-#        )
-#        set_target_properties(GMP::GMP PROPERTIES
-#                IMPORTED_LOCATION_DEBUG "${GMP_LIBRARY_DEBUG}"
-#        )
-#    endif()
+
+    if (GMP_FOUND AND NOT TARGET GMP::GMP)
+        add_library(GMP::GMP UNKNOWN IMPORTED GLOBAL)
+        set_target_properties(GMP::GMP PROPERTIES
+                IMPORTED_LOCATION "${GMP_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${GMP_INCLUDE_DIR}"
+                IMPORTED_IMPLIB "${GMP_LIBRARY}"
+        )
+        #    if (GMP_COMPILE_OPTIONS)
+        #        set_target_properties(GMP::GMP PROPERTIES
+        #                INTERFACE_COMPILE_OPTIONS "${GMP_COMPILE_OPTIONS}"
+        #        )
+        #    endif()
+        #    if(GMP_LINK_OPTIONS)
+        #        set_target_properties(GMP::GMP PROPERTIES
+        #                INTERFACE_LINK_OPTIONS "${GMP_LINK_OPTIONS}"
+        #        )
+        #    endif()
+        #
+        #    if (GMP_LIBRARY_RELEASE)
+        #        set_property(TARGET GMP::GMP APPEND PROPERTY
+        #            IMPORTED_CONFIGURATIONS RELEASE
+        #        )
+        #        set_target_properties(GMP::GMP PROPERTIES
+        #                IMPORTED_LOCATION_RELEASE "${GMP_LIBRARY_RELEASE}"
+        #        )
+        #    endif()
+        #    if (GMP_LIBRARY_DEBUG)
+        #        set_property(TARGET GMP::GMP APPEND PROPERTY
+        #                IMPORTED_CONFIGURATIONS DEBUG
+        #        )
+        #        set_target_properties(GMP::GMP PROPERTIES
+        #                IMPORTED_LOCATION_DEBUG "${GMP_LIBRARY_DEBUG}"
+        #        )
+        #    endif()
 
 
-endif()
+    endif()
+endif ()
+
