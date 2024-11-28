26356b203cc662a4283e24cfa06c1ce8a164cb56
Sam Morley <admin@inakleinbottle.com>
2024-11-28 10:53:26 +0000

    Refactor copy function and add separate move functionality
    Renamed `copy_or_move` to `copy_or_fill` and added a new `move` function to handle object movement more clearly. Updated logic to support uninitialized data handling in both `copy_or_fill` and `move` functions using `std::uninitialized_*` methods for safe memory operations.

--- platform/src/generics/polynomial_type/polynomial_type.cpp
+++ platform/src/generics/polynomial_type/polynomial_type.cpp
@@ -81,23 +81,45 @@ void PolynomialType::free_object(void* ptr) const
 }
 
 
-void PolynomialType::copy_or_move(void* dst,
+void PolynomialType::copy_or_fill(void* dst,
                                   const void* src,
                                   size_t count,
-                                  bool move) const
+                                  bool uninit) const
 {
     RPY_DBG_ASSERT_NE(dst, nullptr);
     auto* dst_poly = static_cast<Polynomial*>(dst);
     const auto* src_poly = static_cast<const Polynomial*>(src);
 
     if (src_poly == nullptr) {
-        for (size_t i = 0; i < count; ++i) { dst_poly[i] = Polynomial(); }
-    } else if (move) {
-        auto* modifiable_src = const_cast<Polynomial*>(src_poly);
-        for (size_t i = 0; i < count; ++i) {
-            dst_poly[i] = std::move(modifiable_src[i]);
+        if (uninit) {
+            std::uninitialized_default_construct_n(dst_poly, count);
+        } else {
+            std::fill_n(dst_poly, count, Polynomial());
         }
-    } else { std::copy_n(src_poly, count, dst_poly); }
+    } else {
+        if (uninit) {
+            std::uninitialized_copy_n(src_poly, count, dst_poly);
+        } else {
+            std::copy_n(src_poly, count, dst_poly);
+        }
+    }
+}
+
+void PolynomialType::move(void* dst, void* src, size_t count, bool uninit) const
+{
+    if (RPY_UNLIKELY(count == 0)) { return; }
+
+    RPY_DBG_ASSERT_NE(dst, nullptr);
+    RPY_DBG_ASSERT_NE(src, nullptr);
+
+    auto* dst_poly = static_cast<Polynomial*>(dst);
+    auto* src_poly = static_cast<const Polynomial*>(src);
+
+    if (uninit) {
+        std::uninitialized_move_n(src_poly, count, dst_poly);
+    } else {
+        std::copy_n(std::make_move_iterator(src_poly), count, dst_poly);
+    }
 }
 
 void PolynomialType::destroy_range(void* data, size_t count) const
--- platform/src/generics/polynomial_type/polynomial_type.h
+++ platform/src/generics/polynomial_type/polynomial_type.h
@@ -47,10 +47,12 @@ protected:
 public:
     bool parse_from_string(void* data, string_view str) const noexcept override;
 
-    void copy_or_move(void* dst,
+    void copy_or_fill(void* dst,
         const void* src,
         size_t count,
-        bool move) const override;
+        bool uninit) const override;
+
+    void move(void *dst, void *src, size_t count, bool uninit) const override;
 
     void destroy_range(void* data, size_t count) const override;
 
