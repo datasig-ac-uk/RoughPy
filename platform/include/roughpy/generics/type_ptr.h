273b608dfd0293ecd8be409c627df1da7ab55ee7
Sam Morley <admin@inakleinbottle.com>
2024-11-29 13:22:09 +0000

    Add BuiltinTypesList to type_ptr.h
    Include a new metafunction BuiltinTypesList in type_ptr.h to define a list of common primitive types. This enhancement provides better type management and organization within the generics module. Also, added an include directive for meta.h to support the new metafunction.

--- platform/include/roughpy/generics/type_ptr.h
+++ platform/include/roughpy/generics/type_ptr.h
@@ -6,6 +6,7 @@
 #define ROUGHPY_GENERICS_TYPE_PTR_H
 
 #include <roughpy/core/smart_ptr.h>
+#include "roughpy/core/meta.h"
 
 namespace rpy::generics {
 
@@ -16,6 +17,21 @@ using TypePtr = Rc<const Type>;
 
 template <typename T>
 inline constexpr string_view type_id_of;
+
+
+using BuiltinTypesList = meta::TypeList<
+    int8_t,
+    uint8_t,
+    int16_t,
+    uint16_t,
+    int32_t,
+    uint32_t,
+    int64_t,
+    uint64_t,
+    float,
+    double
+    >;
+
 
 
 }
