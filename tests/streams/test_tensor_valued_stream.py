7b345cac56589e4c4e0d0067fe9aa5b2dc748359
Sam Morley <admin@inakleinbottle.com>
2024-12-11 11:34:40 +0000

    Refactor test formatting for tensor-valued stream.
    Updated indentation and line breaks to improve code readability and maintain consistent styling. No functional changes were made to the test logic or behavior.

--- tests/streams/test_tensor_valued_stream.py
+++ tests/streams/test_tensor_valued_stream.py
@@ -6,14 +6,14 @@ import roughpy as rp
 @pytest.fixture
 def simple_tv_stream():
     ctx = rp.get_context(4, 2, rp.DPReal)
-    return rp.TensorValuedStream(increment_stream=rp.BrownianStream.with_generator("pcg64", ctx=ctx),
-                                       initial_value=rp.FreeTensor([1.0], ctx=ctx),
-                                       domain=rp.RealInterval(0., 1.))
-
-
+    return rp.TensorValuedStream(
+        increment_stream=rp.BrownianStream.with_generator("pcg64", ctx=ctx),
+        initial_value=rp.FreeTensor([1.0], ctx=ctx),
+        domain=rp.RealInterval(0., 1.))
 
 
 def test_tv_stream_initial_value(simple_tv_stream):
-    assert simple_tv_stream.initial_value() == rp.FreeTensor([1.], ctx=rp.get_context(4, 2, rp.DPreal))
-
-
+    assert simple_tv_stream.initial_value() == rp.FreeTensor([1.],
+                                                             ctx=rp.get_context(
+                                                                 4, 2,
+                                                                 rp.DPReal))
