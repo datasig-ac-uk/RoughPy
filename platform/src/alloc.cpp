aa779a5d1afb539d744bb37d9172a0e50ce4c9e7
Sam Morley <admin@inakleinbottle.com>
2024-12-17 16:40:11 +0000

    Refactor memory allocation to use Boost pool allocator.
    Replaced standard polymorphic memory resource with Boost's `pool` for small object allocation. This improves alignment handling and simplifies the custom allocator logic while maintaining proper deallocation behavior. Removed obsolete commented-out code for clarity.

--- platform/src/alloc.cpp
+++ platform/src/alloc.cpp
@@ -5,8 +5,8 @@
 #include "roughpy/platform/alloc.h"
 
 #include <new>
-#include <memory_resource>
 
+#include <boost/pool/singleton_pool.hpp>
 #include <boost/align/aligned_alloc.hpp>
 #include <roughpy/core/traits.h>
 
@@ -25,61 +25,108 @@ void rpy::mem::aligned_free(void* ptr, size_t size) noexcept
     boost::alignment::aligned_free(ptr);
 }
 
+//
+// namespace {
+//
+// class PageAlignedMemoryResource : public std::pmr::memory_resource
+// {
+//
+// protected:
+//     void* do_allocate(size_t bytes, size_t alignment) override
+//     {
+//         ignore_unused(alignment);
+//         void* ptr = mem::aligned_alloc(small_chunk_size, bytes);
+//         if (!ptr) { throw std::bad_alloc(); }
+//         return ptr;
+//     }
+//
+//     void do_deallocate(void* p, size_t bytes, size_t alignment) override
+//     {
+//         ignore_unused(alignment);
+//         return mem::aligned_free(p, bytes);
+//     }
+//
+//     bool do_is_equal(const std::pmr::memory_resource& other
+//     ) const noexcept override
+//     {
+//         return this == &other;
+//     }
+//
+// public:
+//     static PageAlignedMemoryResource* get() noexcept
+//     {
+//         static PageAlignedMemoryResource instance;
+//         return &instance;
+//     }
+// };
+//
+// std::pmr::synchronized_pool_resource* get_pool() noexcept
+// {
+//     static std::pmr::synchronized_pool_resource pool (
+//         std::pmr::pool_options{ small_blocks_per_chunk, small_block_size },
+//         PageAlignedMemoryResource::get());
+//     return &pool;
+// }
+// }
+
 
 namespace {
 
-class PageAlignedMemoryResource : public std::pmr::memory_resource
+
+struct MyAlignedAlloc
 {
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
 
-protected:
-    void* do_allocate(size_t bytes, size_t alignment) override
+    static char* malloc BOOST_PREVENT_MACRO_SUBSTITUTION(const size_type size) noexcept
     {
-        ignore_unused(alignment);
-        void* ptr = mem::aligned_alloc(small_chunk_size, bytes);
-        if (!ptr) { throw std::bad_alloc(); }
-        return ptr;
+        return static_cast<char*>(mem::aligned_alloc(small_chunk_size, size));
     }
 
-    void do_deallocate(void* p, size_t bytes, size_t alignment) override
+    static void free BOOST_PREVENT_MACRO_SUBSTITUTION(char* const block) noexcept
     {
-        ignore_unused(alignment);
-        return mem::aligned_free(p, bytes);
+        mem::aligned_free(block, small_chunk_size);
     }
 
-    bool do_is_equal(const std::pmr::memory_resource& other
-    ) const noexcept override
-    {
-        return this == &other;
-    }
-
-public:
-    static PageAlignedMemoryResource* get() noexcept
-    {
-        static PageAlignedMemoryResource instance;
-        return &instance;
-    }
 };
 
-std::pmr::synchronized_pool_resource* get_pool() noexcept
+boost::pool<MyAlignedAlloc>* get_pool() noexcept
 {
-    static std::pmr::synchronized_pool_resource pool (
-        std::pmr::pool_options{ small_blocks_per_chunk, small_block_size },
-        PageAlignedMemoryResource::get());
+    static boost::pool<MyAlignedAlloc> pool {small_block_size, small_blocks_per_chunk};
     return &pool;
 }
+
+
 }
+
 
 void* rpy::mem::small_object_alloc(size_t size)
 {
-    return get_pool()->allocate(size);
+    static constexpr size_t max_alignment = alignof(std::max_align_t);
+    // return get_pool()->allocate(size);
+    if (size > small_block_size) {
+        return aligned_alloc(max_alignment, size);
+    }
+    return get_pool()->malloc();
 }
+
 void rpy::mem::small_object_free(void* ptr, size_t size)
 {
-    get_pool()->deallocate(ptr, size);
+    if (size > small_block_size) {
+        aligned_free(ptr, size);
+    } else {
+        get_pool()->free(static_cast<char*>(ptr));
+    }
+
+    // get_pool()->deallocate(ptr, size);
 }
-void* SmallObjectBase::operator new(size_t size) {
+
+void* SmallObjectBase::operator new(size_t size)
+{
     return small_object_alloc(size);
 }
-void SmallObjectBase::operator delete(void* object, size_t size) {
+
+void SmallObjectBase::operator delete(void* object, size_t size)
+{
     small_object_free(object, size);
 }
