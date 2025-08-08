ee92f74eb9cb4ebdccad55ecc07da7d8c2077f02
Sam Morley <admin@inakleinbottle.com>
2025-08-08 14:11:10 +0100

    Update `conanfile.py` to include `CMakeToolchain` generator and comment out `generate` method

--- conanfile.py
+++ conanfile.py
@@ -7,15 +7,15 @@ from conan.tools.cmake import cmake_layout, CMakeToolchain
 class ConanApplication(ConanFile):
     package_type = "application"
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeDeps"
+    generators = "CMakeToolchain", "CMakeDeps"
 
     def layout(self):
         cmake_layout(self)
 
-    def generate(self):
-        tc = CMakeToolchain(self)
-        tc.user_presets_path = False
-        tc.generate()
+    # def generate(self):
+        # tc = CMakeToolchain(self)
+        # tc.user_presets_path = False
+        # tc.generate()
 
     def requirements(self):
         requirements = self.conan_data.get('requirements', [])
