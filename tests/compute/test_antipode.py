c70fa1ab480d43b0cb1b518b740193de7db873f9
Sam Morley <admin@inakleinbottle.com>
2025-08-13 14:14:14 +0100

    Add idempotence property test for `antipode` in free tensor computations

--- tests/compute/test_antipode.py
+++ tests/compute/test_antipode.py
@@ -1,4 +1,4 @@
-
+from dis import deoptmap
 
 import numpy as np
 import pytest
@@ -10,6 +10,11 @@ from numpy.testing import assert_array_almost_equal
 
 
 def test_antipode_on_signature():
+    """
+    The antipode of a group-like free tensor (like a signature) is the
+    inverse of said tensor. Thus the product of antipode(x) and x should
+    be the identity.
+    """
 
     rng = default_rng(1234)
 
@@ -33,4 +38,24 @@ def test_antipode_on_signature():
 
     assert_array_almost_equal(product.data, expected)
 
+
+def test_antipode_idempotent():
+    """
+    The antipode is an idempotent operation:
+        if x is a free tensor then antipode(antipode(x)) == x
+    This is a test that this property holds.
+    """
+
+    rng = default_rng(12345)
+
+    width = 4
+    depth = 3
+
+    basis = compute.TensorBasis(width, depth)
+    x = compute.FreeTensor(rng.normal(size=(basis.size(),)), basis)
+
+    ax = compute.antipode(x)
+    aax = compute.antipode(ax)
+
+    assert_array_almost_equal(x.data, aax.data)
 
