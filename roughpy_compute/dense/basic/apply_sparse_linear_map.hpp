52cda0d8ba4c441bbb506c7a61b82bff6b7a7afe
Sam Morley <admin@inakleinbottle.com>
2025-09-12 16:24:01 +0100

    Refactor `apply_sparse_linear_map` to support generic compressed sparse matrix types
    - Replaced `CompressedRowMatrix` with template-based `CompressedSparseMatrix` for broader compatibility.
    - Improved row and column traversal logic to handle different compressed dimensions.
    - Simplified and modularized code for maintainability and clarity.

--- roughpy_compute/dense/basic/apply_sparse_linear_map.hpp
+++ roughpy_compute/dense/basic/apply_sparse_linear_map.hpp
@@ -7,21 +7,41 @@
 namespace rpy::compute::basic {
 inline namespace v1 {
 
-template <typename Scalar, typename OutIter, typename ArgIter>
+template <typename OutIter, typename CompressedSparseMatrix, typename ArgIter>
 [[gnu::always_inline]] inline
-void apply_sparse_linear_map(DenseVectorFragment<OutIter> out, CompressedRowMatrix<Scalar> const& matrix, DenseVectorFragment<ArgIter> arg)
+void apply_sparse_linear_map(DenseVectorFragment<OutIter> out, CompressedSparseMatrix const& matrix, DenseVectorFragment<ArgIter> arg)
 {
-
-    auto const n_rows = matrix.n_rows();
-
-    for (int32_t i=0; i < n_rows; ++i) {
-        auto& coeff = out[i];
-
-        for (auto&& [j, c] : matrix.row(i)) {
-            coeff += c * arg[j];
+    using Index = typename CompressedSparseMatrix::difference_type;
+    if constexpr (CompressedSparseMatrix::compressed_dim == CompressedRow) {
+        const auto n_rows = matrix.rows();
+        for (Index i=0; i<n_rows; ++i) {
+            auto const row = matrix.in_dim(i);
+            auto& coeff = out[i];
+            for (auto&& [j, c] : row) {
+                coeff += c * arg[j];
+            }
+        }
+    } else {
+        const auto cols = matrix.cols();
+        for (Index j=0; j<cols; ++j) {
+            const auto col = matrix.in_dim(j);
+            for (auto&& [i, c] : col) {
+                out[i] += c * arg[j];
+            }
         }
 
     }
+
+    // auto const n_rows = matrix.n_rows();
+    //
+    // for (int32_t i=0; i < n_rows; ++i) {
+    //     auto& coeff = out[i];
+    //
+    //     for (auto&& [j, c] : matrix.row(i)) {
+    //         coeff += c * arg[j];
+    //     }
+    //
+    // }
 }
 
 } // version namespace
