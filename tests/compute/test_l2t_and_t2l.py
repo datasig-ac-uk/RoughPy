d901cdc3bd56f463e7fb148916d3b23409b29868
Sam Morley <admin@inakleinbottle.com>
2025-09-30 15:27:40 +0100

    start tests for l2t and t2l

---
+++ tests/compute/test_l2t_and_t2l.py
@@ -0,0 +1,26 @@
+
+from roughpy import compute as rpc
+
+import pytest
+import numpy as np
+from numpy.testing import assert_array_equal, assert_array_almost_equal
+
+
+
+
+def test_l2t_t2l_roundtrip():
+    lie_basis = rpc.LieBasis(4, 4)
+    tensor_basis = rpc.TensorBasis(lie_basis.width, lie_basis.depth)
+
+    rng = np.random.default_rng()
+
+    x_data = rng.uniform(-1.0, 1.0, (lie_basis.size(),))
+
+    x = rpc.Lie(x_data, lie_basis)
+
+    tensor_x = rpc.lie_to_tensor(x, tensor_basis)
+
+    y = rpc.tensor_to_lie(tensor_x, lie_basis)
+
+    assert_array_equal(x.data, y.data)
+
