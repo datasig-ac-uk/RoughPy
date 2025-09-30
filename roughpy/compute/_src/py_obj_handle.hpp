c860fbec39e73c973c9f2a58a3b4c3f01db10a42
Sam Morley <admin@inakleinbottle.com>
2025-09-30 17:00:58 +0100

    Refactor `PyObjHandle` to use `ptr_` for consistency and clarity
    - Renamed `ptr` to `ptr_` across `PyObjHandle` to align with naming conventions.
    - Updated all member access and methods accordingly for improved readability.

--- roughpy/compute/_src/py_obj_handle.hpp
+++ roughpy/compute/_src/py_obj_handle.hpp
@@ -7,68 +7,68 @@ namespace rpy::compute {
 
 class PyObjHandle
 {
-    PyObject* ptr = nullptr;
+    PyObject* ptr_ = nullptr;
 
 public:
 
     PyObjHandle() = default;
 
-    PyObjHandle(PyObject* ptr, bool incref=true) : ptr(ptr)
+    PyObjHandle(PyObject* ptr, bool incref=true) : ptr_(ptr)
     {
         if (incref) {
-            Py_INCREF(ptr);
+            Py_INCREF(ptr_);
         }
     }
 
     ~PyObjHandle()
     {
-        Py_XDECREF(ptr);
+        Py_XDECREF(ptr_);
     }
 
-    PyObjHandle(PyObjHandle&& other) noexcept : ptr(other.ptr)
+    PyObjHandle(PyObjHandle&& other) noexcept : ptr_(other.ptr_)
     {
-        other.ptr = nullptr;
+        other.ptr_ = nullptr;
     }
 
     PyObjHandle& operator=(PyObjHandle&& other) noexcept
     {
-        Py_XSETREF(ptr, other.ptr);
-        other.ptr = nullptr;
+        Py_XSETREF(ptr_, other.ptr_);
+        other.ptr_ = nullptr;
         return *this;
     }
 
     void reset(PyObject* new_obj, bool incref=true)
     {
-        if (incref) { Py_INCREF(ptr); }
-        Py_XSETREF(ptr, new_obj);
+        if (incref) { Py_INCREF(ptr_); }
+        Py_XSETREF(ptr_, new_obj);
     }
 
     // Must be followed by inc_ref if the object should be preserved
-    constexpr PyObject*& obj() { return ptr; }
+    constexpr PyObject*& obj() { return ptr_; }
     void drop() noexcept
     {
-        Py_XDECREF(ptr);
-        ptr = nullptr;
+        Py_XDECREF(ptr_);
+        ptr_ = nullptr;
     }
 
-    void inc_ref() noexcept { Py_INCREF(ptr); }
+    void inc_ref() noexcept { Py_INCREF(ptr_); }
 
     PyObject* release() noexcept
     {
-        auto tmp = ptr;
-        ptr = nullptr;
+        auto tmp = ptr_;
+        ptr_ = nullptr;
         return tmp;
     }
 
     explicit constexpr operator bool() const noexcept
     {
-        return ptr != nullptr;
+        return ptr_ != nullptr;
     }
 
     PyObjHandle& operator=(PyObject* obj) noexcept
     {
         Py_INCREF(obj);
-        Py_XSETREF(ptr, obj);
+        Py_XSETREF(ptr_, obj);
         // Py_XDECREF(ptr);
         // ptr = obj;
         // Py_INCREF(ptr);
