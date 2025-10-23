f7a1123c6b5e114fe9b43400fa2fe860eae152b1
Sam Morley <admin@inakleinbottle.com>
2025-10-23 14:40:57 +0100

    Implement the fnv1a hash function

---
+++ roughpy/compute/_src/fnv1a_hash.h
@@ -0,0 +1,40 @@
+#ifndef ROUGHPY_COMPUTE__SRC_FNV1A_HASH_H
+#define ROUGHPY_COMPUTE__SRC_FNV1A_HASH_H
+
+
+#include <stdint.h>
+#include <stddef.h>
+
+/*
+ * This is an implementation of the FNV-1a hash function which essentially
+ * follows the implementation here: https://github.com/lcn2/fnv. This is the
+ * same hash function that is used in NumPy (and is referenced is implemented
+ * in the Python source code).
+ */
+
+#define FNV1A_OFFSET_BASIS_64 0xCBF29CE484222325
+#define FNV1A_PRIME_64 0x00000100000001B3
+#define FNV1A_OFFSET_BASIS_32 0x811C9DC5
+#define FNV1A_PRIME_32 0x01000193
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t
+fnv1a_hash_string32(uint32_t state, const char* str);
+uint64_t
+fnv1a_hash_string64(uint64_t state, const char* str);
+
+uint32_t
+fnv1a_hash_bytes32(uint32_t state, const unsigned char* bytes, size_t len);
+uint64_t
+fnv1a_hash_bytes64(uint64_t state, const unsigned char* bytes, size_t len);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif// ROUGHPY_COMPUTE__SRC_FNV1A_HASH_H
