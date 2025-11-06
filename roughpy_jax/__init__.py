1a1649756eea04de6e49cca90288d74c1d0c5021
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2025-11-06 15:52:57 +0000

    tidy: use dataclass not pytree (#220)
    - See jax.tree_util.register_dataclass docs for further info; uses optimised C++ dataclass builtin under the hood.
    - Also removes code duplication in custom flatten/unflatten code.

--- roughpy_jax/__init__.py
+++ roughpy_jax/__init__.py
@@ -1,6 +1,7 @@
 import ctypes
+from dataclasses import dataclass
+
 import numpy as np
-from typing import NamedTuple
 
 try:
     import jax
@@ -43,44 +44,36 @@ class TensorBasis(_rpy_compute_internals.TensorBasis):
     pass
 
 
-@jax.tree_util.register_pytree_node_class
-class DenseFreeTensor(NamedTuple):
+def _tensor_dataclass(cls):
+    """
+    Combined decorator for roughpy_jax tensor objects
+
+    Registers dataclass and JAX data class with dynamic data and static basis
+    """
+    cls = dataclass(cls)
+    return jax.tree_util.register_dataclass(
+        cls,
+        data_fields=["data"],
+        meta_fields=["basis"]
+    )
+
+
+@_tensor_dataclass
+class DenseFreeTensor:
     """
     Dense free tensor class built from basis and associated ndarray of data.
     """
     data: jnp.ndarray
     basis: TensorBasis
 
-    def tree_flatten(self):
-        # Flatten dynamic data with static basis
-        return (self.data,), self.basis
 
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        # Reconstruct from static and dynamic data
-        (data,) = children
-        basis = aux_data
-        return cls(data, basis)
-
-
-@jax.tree_util.register_pytree_node_class
-class DenseShuffleTensor(NamedTuple):
+@_tensor_dataclass
+class DenseShuffleTensor:
     """
     Dense shuffle tensor class built from basis and associated ndarray of data.
     """
     data: jnp.ndarray
     basis: TensorBasis
-
-    def tree_flatten(self):
-        # Flatten dynamic data with static basis
-        return (self.data,), self.basis
-
-    @classmethod
-    def tree_unflatten(cls, aux_data, children):
-        # Reconstruct from static and dynamic data
-        (data,) = children
-        basis = aux_data
-        return cls(data, basis)
 
 
 """
@@ -88,6 +81,7 @@ Tensor aliases. Tensors are assumed to be dense without prefix
 """
 FreeTensor = DenseFreeTensor
 ShuffleTensor = DenseShuffleTensor
+
 
 def _check_basis_compat(first_basis: TensorBasis, *other_bases: TensorBasis):
     for i, basis in enumerate(other_bases):
