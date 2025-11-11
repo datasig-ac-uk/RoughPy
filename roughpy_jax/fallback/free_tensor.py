7236cd5b2fd59a29456f60ff355fb62a9a276df3
Sam Morley <admin@inakleinbottle.com>
2025-11-11 12:03:16 +0000

    Added fallback for the adjoint of left free tensor multiplication operator.

--- roughpy_jax/fallback/free_tensor.py
+++ roughpy_jax/fallback/free_tensor.py
@@ -166,3 +166,69 @@ def antipode_fallback(out_data: jnp.ndarray, arg_data: jnp.ndarray, basis: Tenso
         return val.at[db[i]:db[i+1]].set(sign*jnp.transpose(level_data).ravel())
 
     return jax.lax.fori_loop(0, basis.depth, transpose_level, out_data, unroll=True)
+
+
+
+@jax.vmap
+@partial(jax.jit, static_argnums=(3, 4, 5, 6, 7))
+def ft_left_mul_adjoint_fallback(out_data: jnp.ndarray,
+                                 op_data: jnp.ndarray,
+                                 arg_data: jnp.ndarray,
+                                 basis: TensorBasis,
+                                 op_max_deg: np.int32,
+                                 arg_max_deg: np.int32,
+                                 op_min_deg: np.int32 = 0,
+                                 arg_min_deg: np.int32 = 0
+                                 ) -> jnp.ndarray:
+    """
+    Fallback implementation of the adjoint of the left free-tensor multiplication operator.
+
+    This is the adjoint of the left multiplication operator L_A: T -> T given by L_A(B) = A*B
+    where * denotes the free tensor product. This is technically a map between shuffle tensors,
+    but it is occasionally useful to apply this to free tensors too.
+
+    This implementation is not intended to be used directly.
+
+    :param out_data: data for the output (shuffle) tensor
+    :param op_data: data for the operator (free) tensor
+    :param arg_data:  data for the argument (shuffle) tensor
+    :param basis: basis for the output tensor
+    :param op_max_deg: maximum degree for the operator tensor
+    :param arg_max_deg: maximum degree for the argument tensor
+    :param op_min_deg: optional minimum degree for the operator tensor (default 0)
+    :param arg_min_deg: optional minimum degree for the argument tensor (default 0)
+    :return:
+    """
+    db = basis.degree_begin
+
+    out_min_deg = arg_min_deg + op_min_deg
+    out_max_deg = min(basis.depth, arg_max_deg + op_max_deg)
+
+    def dsize(degree):
+        return db[degree+1] - db[degree]
+
+    def inner_fn(op_idx, val, *, op_deg, out_deg):
+        arg_deg = op_deg + out_deg
+        out_size = dsize(out_deg)
+        offset = db[arg_deg] + op_idx * out_size
+        op_val = op_data[db[op_deg] + op_idx];
+
+        arg_level = arg_data[offset: offset + out_size]
+        return val.at[db[out_deg]:db[out_deg+1]].add(op_val * arg_level)
+
+    def out_deg_loop(out_deg, val, *, arg_deg):
+        op_deg = arg_deg - out_deg
+
+        fn = partial(inner_fn, op_deg = op_deg, out_deg = out_deg)
+        return jax.lax.fori_loop(0, dsize(op_deg), fn, val)
+
+    def arg_deg_loop(d, val):
+        arg_deg = arg_max_deg - d
+
+        odeg_start = max(arg_deg - op_max_deg, out_min_deg)
+        odeg_end = min(arg_deg - op_min_deg, out_max_deg)
+
+        return jax.lax.fori_loop(odeg_start, odeg_end, partial(out_deg_loop, arg_deg=arg_deg), val)
+
+    return jax.lax.fori_loop(arg_min_deg, arg_max_deg, arg_deg_loop, out_data)
+
