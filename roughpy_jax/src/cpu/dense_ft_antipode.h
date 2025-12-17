dbee050491fe050f61cd2d1438213880b2a1e3cc
Jack Leland <jack.leland@dtc.ox.ac.uk>
2025-12-17 16:36:38 +0000

    typo: Remove commented out namespaces

--- roughpy_jax/src/cpu/dense_ft_antipode.h
+++ roughpy_jax/src/cpu/dense_ft_antipode.h
@@ -3,14 +3,6 @@
 
 #include <xla/ffi/api/c_api.h>
 
-// namespace rpy {
-// namespace jax {
-// namespace cpu {
-
 XLA_FFI_Error* cpu_dense_ft_antipode(XLA_FFI_CallFrame*);
-
-// }  // namespace cpu
-// }  // namespace jax
-// }  // namespace rpy
 
 #endif  // ROUGHPY_JAX_CPU_DENSE_FT_ANTIPODE_H
