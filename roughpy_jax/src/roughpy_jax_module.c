a077f0872dcc3b823f05e72d59899f40229517cd
Sam Morley <admin@inakleinbottle.com>
2026-01-07 13:41:03 +0000

    add mul functions to exported functions dict

--- roughpy_jax/src/roughpy_jax_module.c
+++ roughpy_jax/src/roughpy_jax_module.c
@@ -47,6 +47,9 @@ static int make_jax_function_dict(PyObject* module) {
     if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_ft_fma) < 0) {
          goto finish;
     }
+    if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_ft_mul) < 0) {
+        goto finish;
+    }
     if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_ft_fmexp) < 0) {
          goto finish;
     }
@@ -55,6 +58,9 @@ static int make_jax_function_dict(PyObject* module) {
     }
     if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_st_fma) < 0) {
          goto finish;
+    }
+    if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_st_mul) < 0) {
+        goto finish;
     }
     if (RPJ_ADD_FN_CAPSULE(dict, cpu_dense_ft_adj_lmul) < 0) {
         goto finish;
