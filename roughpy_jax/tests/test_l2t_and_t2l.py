4add0a652f265ec10c2ca5ad91664d26ca615fff
Alex Allmont <alex.allmont@dtc.ox.ac.uk>
2026-01-19 12:05:47 +0000

    tidy: lt2 and t2l tests use conftest.py (#270)

--- roughpy_jax/tests/test_l2t_and_t2l.py
+++ roughpy_jax/tests/test_l2t_and_t2l.py
@@ -1,63 +1,44 @@
 import jax.numpy as jnp
-import numpy as np
-import pytest
 import roughpy_jax as rpj
 
-from rpy_test_common import array_dtypes
 
-
-# FIXME replace with new batch methods in conftest.py
-batch_dims = [(), (10,), (10, 5)]
-
-@pytest.mark.parametrize("jnp_dtype", array_dtypes)
-@pytest.mark.parametrize("batch_dims", batch_dims)
-def test_l2t_t2l_roundtrip(jnp_dtype, batch_dims):
-    rng = np.random.default_rng(1234)
-
+def test_l2t_t2l_roundtrip(rpy_dtype, rpy_batch):
     lie_basis = rpj.LieBasis(4, 4)
     tensor_basis = rpj.TensorBasis(lie_basis.width, lie_basis.depth)
 
-    x_data = rng.uniform(-1.0, 1.0, (*batch_dims, lie_basis.size())).astype(jnp_dtype)
+    x_data = rpy_batch.rng_uniform(-1, 1, lie_basis.size(), rpy_dtype)
     x = rpj.Lie(x_data, lie_basis)
 
     tensor_x = rpj.lie_to_tensor(x, tensor_basis)
     y = rpj.tensor_to_lie(tensor_x, lie_basis)
 
-    assert y.data.dtype == x.data.dtype == jnp_dtype
+    assert y.data.dtype == x.data.dtype == rpy_dtype
     assert jnp.allclose(x.data, y.data, atol=1e-7)
 
 
-@pytest.mark.parametrize("jnp_dtype", array_dtypes)
-@pytest.mark.parametrize("batch_dims", batch_dims)
-def test_l2t_scaled_t2l_roundtrip(jnp_dtype, batch_dims):
-    rng = np.random.default_rng(1234)
-
+def test_l2t_scaled_t2l_roundtrip(rpy_dtype, rpy_batch):
     lie_basis = rpj.LieBasis(4, 4)
     tensor_basis = rpj.TensorBasis(lie_basis.width, lie_basis.depth)
 
-    x_data = rng.uniform(-1.0, 1.0, (*batch_dims, lie_basis.size())).astype(jnp_dtype)
+    x_data = rpy_batch.rng_uniform(-1, 1, lie_basis.size(), rpy_dtype)
     x = rpj.Lie(x_data, lie_basis)
 
     tensor_x = rpj.lie_to_tensor(x, tensor_basis, scale_factor=0.5)
     y = rpj.tensor_to_lie(tensor_x, lie_basis)
 
-    assert y.data.dtype == x.data.dtype == jnp_dtype
+    assert y.data.dtype == x.data.dtype == rpy_dtype
     assert jnp.allclose(0.5 * x.data, y.data, atol=1e-7)
 
 
-@pytest.mark.parametrize("jnp_dtype", array_dtypes)
-@pytest.mark.parametrize("batch_dims", batch_dims)
-def test_l2t_t2l_scaled_roundtrip(jnp_dtype, batch_dims):
-    rng = np.random.default_rng(1234)
-
+def test_l2t_t2l_scaled_roundtrip(rpy_dtype, rpy_batch):
     lie_basis = rpj.LieBasis(4, 4)
     tensor_basis = rpj.TensorBasis(lie_basis.width, lie_basis.depth)
 
-    x_data = rng.uniform(-1.0, 1.0, (*batch_dims, lie_basis.size())).astype(jnp_dtype)
+    x_data = rpy_batch.rng_uniform(-1, 1, lie_basis.size(), rpy_dtype)
     x = rpj.Lie(x_data, lie_basis)
 
     tensor_x = rpj.lie_to_tensor(x, tensor_basis)
     y = rpj.tensor_to_lie(tensor_x, lie_basis, scale_factor=0.5)
 
-    assert y.data.dtype == x.data.dtype == jnp_dtype
+    assert y.data.dtype == x.data.dtype == rpy_dtype
     assert jnp.allclose(0.5 * x.data, y.data, atol=1e-7)
