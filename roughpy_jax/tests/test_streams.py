6987f30cc53c7d5489a9748e4e6a48d791736498
Jack Leland <jack.leland@dtc.ox.ac.uk>
2026-02-17 11:56:50 +0000

    feat: Add stub test for stream class

---
+++ roughpy_jax/tests/test_streams.py
@@ -0,0 +1,11 @@
+
+
+def test_piecewise_abelian_stream():
+    """Test the PiecewiseAbelianStream class.""" 
+    # Create a piecewise abelian path
+    # Give it an interval [t0, t1, t2, ..., tn]
+    # Corresponding Lies [L1, L2, ..., Ln]
+    # Calculate the log signature over the interval [t0, t1] and make sure it 
+    # equals L1
+    
+    assert False, "Test not implemented yet"
