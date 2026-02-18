29c7ef7988abe0a43518c1eb576e9338078bee53
Jack Leland <jack.leland@dtc.ox.ac.uk>
2026-02-18 16:08:01 +0000

    rf: Add identity testing and refactor to reuse setup

--- roughpy_jax/tests/streams/test_piecewise_abelian_stream.py
+++ roughpy_jax/tests/streams/test_piecewise_abelian_stream.py
@@ -5,40 +5,47 @@ from roughpy_jax.streams import PiecewiseAbelianStream
 from roughpy_jax.intervals import RealInterval, IntervalType, Partition
 
 
-def test_piecewise_abelian_stream(rpj_batch, rpj_dtype):
-    """Test the PiecewiseAbelianStream class.""" 
-    # Create a piecewise abelian path
-    # Give it an interval [t0, t1, t2, ..., tn]
-    # Corresponding Lies [L1, L2, ..., Ln]
-    # Calculate the log signature over the interval [t0, t1] and make sure it 
-    # equals L1
+class TestPiecewiseAbelianStream:
     
-    # Create a simple piecewise abelian stream with two intervals
-    # [0, 1] and [1, 2] with corresponding Lie elements L1 and L2
-    interval = RealInterval(0.0, 2.0, IntervalType.ClOpen)
-    query_interval = RealInterval(0.0, 1.0, IntervalType.ClOpen)
-    partition = Partition([0.0, 1.0, 2.0], IntervalType.ClOpen)
-    
-    # Make some Lie elements for the stream (we can just use random data for this test)
-    lie_basis = rpj.LieBasis(2, 2)
-    tensor_basis = rpj.TensorBasis(lie_basis.width, lie_basis.depth)
+    def setup(self, rpj_batch, rpj_dtype):
+        # Create a simple piecewise abelian stream with two intervals
+        # [0, 1] and [1, 2] with corresponding Lie elements L1 and L2
+        self.interval = RealInterval(0.0, 2.0, IntervalType.ClOpen)
+        self.query_interval = RealInterval(0.0, 1.0, IntervalType.ClOpen)
+        self.partition = Partition([0.0, 1.0, 2.0], IntervalType.ClOpen)
+        
+        # Make some Lie elements for the stream (we can just use random data for this test)
+        self.lie_basis = rpj.LieBasis(2, 2)
+        self.tensor_basis = rpj.TensorBasis(self.lie_basis.width, self.lie_basis.depth)
 
-    l1_data = rpj_batch.rng_uniform(-1, 1, lie_basis.size(), rpj_dtype)
-    l1 = rpj.Lie(l1_data, lie_basis)
-    l2_data = rpj_batch.rng_uniform(-1, 1, lie_basis.size(), rpj_dtype)
-    l2 = rpj.Lie(l2_data, lie_basis)
+        self.l1_data = rpj_batch.rng_uniform(-1, 1, self.lie_basis.size(), rpj_dtype)
+        self.l1 = rpj.Lie(self.l1_data, self.lie_basis)
+        self.l2_data = rpj_batch.rng_uniform(-1, 1, self.lie_basis.size(), rpj_dtype)
+        self.l2 = rpj.Lie(self.l2_data, self.lie_basis)
+        
+        # Create the piecewise abelian stream
+        self.stream = PiecewiseAbelianStream(
+            _data=(self.l1, self.l2),
+            _partition=self.partition,
+            _lie_basis=self.lie_basis,
+            _group_basis=rpj.TensorBasis(self.lie_basis.width, self.lie_basis.depth)
+        )
     
-    # Create the piecewise abelian stream
-    stream = PiecewiseAbelianStream(
-        _data=(l1, l2),
-        _partition=partition,
-        _lie_basis=lie_basis,
-        _group_basis=rpj.TensorBasis(lie_basis.width, lie_basis.depth)
-    )
-    
-    # Compute log signature over [0, 1]
-    log_sig = stream.log_signature(query_interval)
-    
-    # Check that it equals L1 (which is l1 in this case)
-    assert log_sig == l1
-    
+    def test_log_signature(self, rpj_batch, rpj_dtype):
+        """Test the PiecewiseAbelianStream class.""" 
+        self.setup(rpj_batch, rpj_dtype)
+        
+        # Compute log signature over [0, 1]
+        log_sig = self.stream.log_signature(self.query_interval)
+        
+        # Check that it equals L1 (which is l1 in this case)
+        assert log_sig == self.l1
+        
+    def test_get_identity_batch(self, rpj_batch, rpj_dtype):
+        """Test that the identity element has the correct shape and properties."""
+        self.setup(rpj_batch, rpj_dtype)
+        
+        identity = self.stream._get_identity(dtype=self.l1.data.dtype)
+        
+        assert identity.data.shape[-1] == self.tensor_basis.size()
+        assert identity.data.shape == (*rpj_batch.shape, self.tensor_basis.size())
